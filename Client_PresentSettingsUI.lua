4cc7630b3430365f2fde5d0e6a7e7d81c4ece90d
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-13 11:51:14 +0200

    Update Client_PresentSettingsUI.lua
    fixed Client_PresentSettingsUI

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -1,10 +1,10 @@
 
 function Client_PresentSettingsUI(rootParent)
 --	UI.CreateLabel(rootParent).SetText("AI's can play reinforcement cards: " .. Mod.Settings.CanPlayReinforcement);
-	UI.CreateLabel(rootParent).SetText("AI's can play emergency blockade cards: ", Mod.Settings.CanPlayEMB,false,true, "");
-	UI.CreateLabel(rootParent).SetText("AI's can play diplomacy cards: ", Mod.Settings.CanPlayDiplomacy,false,true, "");
-	UI.CreateLabel(rootParent).SetText("AI's can play sanctions cards: ", Mod.Settings.CanPlaySanctions,false,true, "");
-	UI.CreateLabel(rootParent).SetText("AI's can play blockade cards: ", Mod.Settings.CanPlayBlockade,false,true, "");
-	UI.CreateLabel(rootParent).SetText("AI's can play bomb cards: ", Mod.Settings.CanPlayBomb,false,true, "");
+	UI.CreateLabel(rootParent).SetText("AI's can play emergency blockade cards: " .. tostring(Mod.Settings.CanPlayEMB));
+	UI.CreateLabel(rootParent).SetText("AI's can play diplomacy cards: " .. tostring(Mod.Settings.CanPlayDiplomacy));
+	UI.CreateLabel(rootParent).SetText("AI's can play sanctions cards: " .. tostring(Mod.Settings.CanPlaySanctions));
+	UI.CreateLabel(rootParent).SetText("AI's can play blockade cards: " .. tostring(Mod.Settings.CanPlayBlockade));
+	UI.CreateLabel(rootParent).SetText("AI's can play bomb cards: " .. tostring(Mod.Settings.CanPlayBomb));
 end
 
