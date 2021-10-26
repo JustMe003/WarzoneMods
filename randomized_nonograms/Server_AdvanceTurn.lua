1bdcd2f44821f1ecd4205470ce942c3dddebb03c
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 01:50:48 +0200

    Update Server_AdvanceTurn.lua

--- randomized_nonograms/Server_AdvanceTurn.lua
+++ randomized_nonograms/Server_AdvanceTurn.lua
@@ -2,9 +2,9 @@ function Server_AdvanceTurn_End(Game, addNeworder)
 	local publicGameData = Mod.PublicGameData;
 	bonuses = publicGameData.Bonuses
 	for bonusID, list in pairs(bonuses) do
-		if player_has_bonus(Game, list) then
+		if player_has_bonus(game, list) then
 			if Mod.Settings.LocalDeployments == true then
-				local_deployments(Game, list);
+				local_deployments(game, list);
 			else
 				print(tostring(Mod.Settings.LocalDeployments));
 			end
@@ -12,10 +12,10 @@ function Server_AdvanceTurn_End(Game, addNeworder)
 	end
 end
 
-function player_has_bonus(Game, list_of_terr)
+function player_has_bonus(game, list_of_terr)
 	local playerID = 0;
 	for _, terrID in pairs(list_of_terr) do
-		local terr = game.ServerGame.LatestTurnStanding.Territories[2]
+		local terr = game.ServerGame.LatestTurnStanding.Territories[terrID]
 		if terr.OwnerPlayerID ~= PlayerID.Neutral and playerID == 0 then
 			playerID = game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID;
 		elseif game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID ~= playerID or game.ServerGame.LatestTurnStanding.Territories[terrID].OwnerPlayerID == PlayerID.Neutral then
@@ -25,7 +25,7 @@ function player_has_bonus(Game, list_of_terr)
 	return true;
 end
 
-function local_deployments (Game, list_of_terr)
+function local_deployments (game, list_of_terr)
 	for _, terrID in pairs(list_of_terr) do
 		local terr = game.ServerGame.LatestTurnStanding.Territories[terrID];
 		terrMod = WL.TerritoryModification.Create(terr.ID);
