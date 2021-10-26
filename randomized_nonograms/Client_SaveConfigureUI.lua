84d54fb407f24da8630eb5fdadc59308fbd8c2db
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-26 18:29:28 +0200

    Update Client_SaveConfigureUI.lua

--- randomized_nonograms/Client_SaveConfigureUI.lua
+++ randomized_nonograms/Client_SaveConfigureUI.lua
@@ -4,5 +4,5 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.NonogramWidth = setWidth.GetValue();
 	Mod.Settings.NonogramHeigth = setHeigth.GetValue();
 	Mod.Settings.NonogramDensity = setDensity.GetValue();
-	Mod.Settings.LocalDeployments = LocalDeployments.GetValue();
+	Mod.Settings.LocalDeployments = LocalDeployments.GetIsChecked();
 end
