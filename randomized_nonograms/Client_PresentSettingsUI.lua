42e4613957fb144c2629f31afeae28156604f9e4
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 17:07:36 +0200

    Update Client_PresentSettingsUI.lua

--- randomized_nonograms/Client_PresentSettingsUI.lua
+++ randomized_nonograms/Client_PresentSettingsUI.lua
@@ -1,5 +1,8 @@
 
 function Client_PresentSettingsUI(rootParent)
-	
+	UI.CreateLabel(rootParent).SetText("Heigth of nonogram: " .. Mod.Settings.NonogramHeigth);
+	UI.CreateLabel(rootParent).SetText("Width of nonogram: " .. Mod.Settings.NonogramWidth);
+	UI.CreateLabel(rootParent).SetText("Density of nonogram: " .. Mod.Settings.NonogramDensity);
+	UI.CreateLabel(rootParent).SetText("Custom local deployments: " .. tostring(Mod.Settings.LocalDeployments));
 end
 
