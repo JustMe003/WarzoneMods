42bdb24e3cf5e355a0f91c93c2c22c72cb0cf4ad
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 02:00:21 +0200

    Update Server_AdvanceTurn.lua

--- randomized_nonograms/Server_AdvanceTurn.lua
+++ randomized_nonograms/Server_AdvanceTurn.lua
@@ -5,7 +5,7 @@ function Server_AdvanceTurn_End(Game, addNeworder)
 		if player_has_bonus(Game, list) then
 			print("true");
 			if Mod.Settings.LocalDeployments == true then
-				local_deployments(Game, list);
+				local_deployments(Game, addNeworder, list);
 			else
 				print(tostring(Mod.Settings.LocalDeployments));
 			end
@@ -26,10 +26,11 @@ function player_has_bonus(game, list_of_terr)
 	return true;
 end
 
-function local_deployments (game, list_of_terr)
+function local_deployments (game, addNeworder, list_of_terr)
 	for _, terrID in pairs(list_of_terr) do
 		local terr = game.ServerGame.LatestTurnStanding.Territories[terrID];
 		terrMod = WL.TerritoryModification.Create(terr.ID);
 		terrMod.SetArmiesTo = terr.NumArmies.NumArmies + 1;
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "deployed", {}, {terrMod}));
 	end
 end
