f38d4d68ba543c2a00cadfc8d0123dac319996e6
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 15:25:33 +0200

    Update Server_AdvanceTurn.lua

--- randomized_nonograms/Server_AdvanceTurn.lua
+++ randomized_nonograms/Server_AdvanceTurn.lua
@@ -1,6 +1,7 @@
 function Server_AdvanceTurn_End(Game, addNewOrder)
 	local publicGameData = Mod.PublicGameData;
 	bonuses = publicGameData.Bonuses;
+	print(bonuses);
 	local player_income = initiate_player_income(Game);
 	for _, list in pairs(bonuses) do
 		if player_has_bonus(Game, list) then
