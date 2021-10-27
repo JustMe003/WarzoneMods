5a8fd4e3c5fa9655063eb47c0939221cfab59c84
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 15:41:21 +0200

    Update Server_Created.lua

--- randomized_nonograms/Server_Created.lua
+++ randomized_nonograms/Server_Created.lua
@@ -47,11 +47,11 @@ function createNonogram(width, heigth, density)
 				length = length + 1;
 			elseif nonogramData[(j*20) + i] == 0 and length ~= 0 then
 				overrideBonuses[bonusID] = length
-				territoriesInBonus[bonusID] = getTerritories((j*20) + i - (length * 20), (j*20) + i + 20, 20)
+				territoriesInBonus[bonusID] = getTerritories((j*20) + i - 20, (j*20) + i + ((length-1)*20), 20)
 				length = 0
 				bonusID = bonusID + 1
 			end
-			print(nonogramData[(j*20) + i], i, j, (j*20) + i, (j*20) + i - (length * 20), (j*20) + i + 20)
+			print(nonogramData[(j*20) + i], i, j, (j*20) + i, (j*20) + i - 20, (j*20) + i + ((length-1)*20))
 		end
 		if length ~= 0 then
 			overrideBonuses[bonusID] = length
