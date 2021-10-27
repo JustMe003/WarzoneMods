3c3521d0de60082b535ba8e2e370944a720a0d98
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 16:52:42 +0200

    Update Server_AdvanceTurn.lua

--- randomized_nonograms/Server_AdvanceTurn.lua
+++ randomized_nonograms/Server_AdvanceTurn.lua
@@ -2,10 +2,11 @@ function Server_AdvanceTurn_End(Game, addNewOrder)
 	local publicGameData = Mod.PublicGameData;
 	bonuses = publicGameData.Bonuses;
 	local player_income = initiate_player_income(Game);
+	local modifiedTerritories = {};
 	for bonusID, list in pairs(bonuses) do
 		if player_has_bonus(Game, list) then
 			if Mod.Settings.LocalDeployments == true then
-				local_deployments(Game, addNewOrder, list);
+				modifiedTerritories = local_deployments(Game, addNewOrder, list, modifiedTerritories);
 			else
 				playerID = get_player(Game, list);
 				player_income[playerID] = player_income[playerID] + #list;
@@ -42,14 +43,27 @@ function player_has_bonus(game, list_of_terr)
 	return true;
 end
 
-function local_deployments (game, addNewOrder, list_of_terr)
+function local_deployments (game, addNewOrder, list_of_terr, modifiedTerritories)
 	for _, terrID in pairs(list_of_terr) do
 		local terr = game.ServerGame.LatestTurnStanding.Territories[terrID];
 		terrMod = WL.TerritoryModification.Create(terr.ID);
 		terrMod.SetOwnerOpt = terr.OwnerPlayerID;
-		terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies + 1;
+		if isModified(terrID, modifiedTerritories) then
+			terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies + 2;
+		else
+			terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies + 1;
+		end
 		addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"added armies",{},{terrMod}));
+		table.insert(modifiedTerritories, terrID);
 	end
+	return modifiedTerritories;
+end
+
+function isModified(terrID, modifiedTerritories)
+	for _, id in pairs(modifiedTerritories) do
+		if terrID == id then return true; end
+	end
+	return false;
 end
 
 function grant_income(game, addNewOrder, player_income)
