aaed00c01060eddab2614b71d02d9a295c5af3ce
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-10-27 11:50:59 +0200

    Update Server_AdvanceTurn.lua

--- randomized_nonograms/Server_AdvanceTurn.lua
+++ randomized_nonograms/Server_AdvanceTurn.lua
@@ -1,16 +1,18 @@
-function Server_AdvanceTurn_End(Game, addNeworder)
+function Server_AdvanceTurn_End(Game, addNewOrder)
 	local publicGameData = Mod.PublicGameData;
-	bonuses = publicGameData.Bonuses
-	for bonusID, list in pairs(bonuses) do
-		if player_has_bonus(Game, list) then
-			print("true");
+	bonuses = publicGameData.Bonuses;
+	local player_income = {};
+	for _, list in pairs(bonuses) do
+		player_has_bonus, playerID = player_has_bonus(Game, list);
+		if player_has_bonus then
 			if Mod.Settings.LocalDeployments == true then
-				local_deployments(Game, addNeworder, list);
+				local_deployments(Game, addNewOrder, list);
 			else
-				print(tostring(Mod.Settings.LocalDeployments));
+				player_income[playerID] = player_income[playerID] + table.getn(list);
 			end
 		end
 	end
+	grant_income(Game, addNewOrder, player_income);
 end
 
 function player_has_bonus(game, list_of_terr)
@@ -23,7 +25,7 @@ function player_has_bonus(game, list_of_terr)
 			return false;
 		end
 	end
-	return true;
+	return true, playerID;
 end
 
 function local_deployments (game, addNewOrder, list_of_terr)
@@ -34,5 +36,13 @@ function local_deployments (game, addNewOrder, list_of_terr)
 			terrMod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies + 1;
 			addNewOrder(WL.GameOrderEvent.Create(terr.OwnerPlayerID,"added armies",{},{terrMod}));
 	end
-	print("added armies???");
+end
+
+function grant_income(Game, addNewOrder, player_income)
+	for _,playerID in pairs(game.ServerGame.Game.PlayingPlayers)do
+		local moneyforplayer = {};
+		moneyforplayer[playerID.ID] = {};
+		moneyforplayer[playerID.ID][WL.ResourceType.Gold] = player_income[playerID.ID] + Game.ServerGame.LatestTurnStanding.NumResources(playerID.ID,WL.ResourceType.Gold);
+		addNewOrder(WL.GameOrderEvent.Create(playerID.ID, "Recieved " .. player_income[playerID.ID] .. " gold from bonuses", {}, {},moneyforplayer));
+	end
 end
