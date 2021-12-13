bbb251fb2ad86dce4033e56ef5d3004d6cff53b5
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-12-13 17:11:08 +0100

    Update UI.lua

--- randomized_nonograms/UI.lua
+++ randomized_nonograms/UI.lua
@@ -10,12 +10,12 @@ function init()
 	verts = {};
 	labels = {};
 	buttons = {};
-	colors = {};
 	texts = {};
 	numbers = {};
 end
 
 function initColors()
+	local colors = {};
 	colors.TextColor = "#AAAAAA"; colors.ErrorColor = "#FF2222"; colors.TrueColor = "#33AA33"; colors.FalseColor = "#AA3333"; colors.NumberColor = "#3333AA"; colors.WarningNumberColor = "#DD1111"; colors.Lime = "#88DD00"; colors.Blue = "#0000FF"; colors.Purple = "#800080"; colors.Orange = "#ffa500"; colors.DarkGray = "#A9A9A9"; colors.HotPink = "#FF69B4"; colors.SeaGreen = "#93E9BE"; colors.Teal = "#008080"; colors.DarkMagenta = "#8b008b"; colors.Yellow = "#FFFF00"; colors.Ivory = "#FFFFF0"; colors.ElectricPurple = "#bf00ff"; colors.DeepPink = "#FF1493"; colors.Aqua = "#00FFFF"; colors.DarkGreen = "#006400"; colors.Red = "#FF0000"; colors.Green = "#00FF00"; colors.SaddleBrown = "#8b4513"; colors.OrangeRed = "#FF5349"; colors.LightBlue = "#ADD8E6"; colors.Orchid = "#DA70D6"; colors.Brown = "#964B00"; colors.CopperRose = "#996666"; colors.Tan = "#D2B48C"; colors.TyrianPurple = "#630330"; colors.MardiGras = "#880085"; colors.RoyalBlue = "#4169e1"; colors.WildStrawberry = "#ff43a4";
 	return colors;
 end
