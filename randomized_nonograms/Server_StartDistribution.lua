eaec5cd3b67c374a4f57140bcde275ea8ee5b017
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-12-14 12:34:45 +0100

    Update Server_StartDistribution.lua

--- randomized_nonograms/Server_StartDistribution.lua
+++ randomized_nonograms/Server_StartDistribution.lua
@@ -13,7 +13,7 @@ function Server_StartDistribution(game, standing)
 		end
 		-- Mod.PublicGameData.Bonuses has as index the bonus ID's, and as value another table containing an array of territory ID's
 		-- Note that the bonuses are custom made
-		data = Mod.PublicGameData
+		data = Mod.PublicGameData;
 		list = {};
 		for bonusID, listOfTerr in pairs(Mod.PublicGameData.Bonuses) do
 			-- All the territories are either assigned to 0 or 2 bonuses
@@ -32,7 +32,7 @@ function Server_StartDistribution(game, standing)
 			else break; end
 		end
 		print(list)
-		data.List = list
+		data[List] = list
 		Mod.PublicGameData.List = data;
 		print(Mod.PublicGameData.List);
 		terr = standing.Territories[400];
