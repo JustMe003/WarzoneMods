59be4618db01ecdcbc5f3fc66e9794d31629fdd9
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2021-12-23 11:30:12 +0100

    Update Server_AdvanceTurn.lua

--- Improved Limited Multiattacks/Server_AdvanceTurn.lua
+++ Improved Limited Multiattacks/Server_AdvanceTurn.lua
@@ -155,7 +155,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 					terrFrom = game.ServerGame.LatestTurnStanding.Territories[order.From];
 					terrTo = game.ServerGame.LatestTurnStanding.Territories[order.To];
 					for i, v in pairs(modifications) do print(i, v) end
-					modifications[1].SetArmiesTo = max(terrFrom.NumArmies.NumArmies - order.NumArmies.NumArmies, 0);
+					modifications[1].SetArmiesTo = math.max(terrFrom.NumArmies.NumArmies - order.NumArmies.NumArmies, 0);
 					modifications[2].SetArmiesTo = terrTo.NumArmies.NumArmies + order.NumArmies.NumArmies;
 					addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, "multi moves", {}, modifications));
 				else
