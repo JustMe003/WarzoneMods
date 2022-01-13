a48da1505dc86ff00a3552252547f60a3da03e5a
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-01-13 12:16:43 +0100

    Update Client_PresentMenuUI.lua

--- Local_Deployment_Helper/Client_PresentMenuUI.lua
+++ Local_Deployment_Helper/Client_PresentMenuUI.lua
@@ -159,10 +159,10 @@ function AddOrdersConfirmes()
 			if (order.proxyType == "GameOrderAttackTransfer") and addTransfers.GetIsChecked() == true then
 				if (Game.Us.ID == standing.Territories[order.From].OwnerPlayerID) then --from us 
 					if (Game.Us.ID == standing.Territories[order.To].OwnerPlayerID) then -- to us
-						if setToPercentage.GetIsChecked() == false then
-							newOrder = WL.GameOrderAttackTransfer.Create(Game.Us.ID, order.From, order.To,3, order.ByPercent, order.NumArmies, false);
-						else
+						if setToPercentage.GetIsChecked() and Game.Settings.AllowPercentageAttacks then
 							newOrder = WL.GameOrderAttackTransfer.Create(Game.Us.ID, order.From, order.To,3, true, WL.Armies.Create(100, {}), false);
+						else
+							newOrder = WL.GameOrderAttackTransfer.Create(Game.Us.ID, order.From, order.To,3, order.ByPercent, order.NumArmies, false);
 						end
 						table.insert(orderTabel, newOrder);
 					end
