660cc97cdbc223b45f6c49c03051a8d6437a93ad
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-01-18 13:08:25 +0100

    Update Server_Created.lua

--- Extended Hybrid Distribution/Server_Created.lua
+++ Extended Hybrid Distribution/Server_Created.lua
@@ -1,5 +1,5 @@
 function Server_Created(game, settings)
 	settings.AutomaticTerritoryDistribution = false;
 	settings.LimitDistributionTerritories = math.max(settings.LimitDistributionTerritories, 1)
-	print(settings.FogLevel);
+	if settings.FogLevel >= 4 then settings.FogLevel = 2; end
 end
