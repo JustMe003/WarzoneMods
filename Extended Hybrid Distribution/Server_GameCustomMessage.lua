5ea3b46bd554e2080758880322475f8c2083eddf
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-01-18 13:44:14 +0100

    Update Server_GameCustomMessage.lua

--- Extended Hybrid Distribution/Server_GameCustomMessage.lua
+++ Extended Hybrid Distribution/Server_GameCustomMessage.lua
@@ -1,6 +1,7 @@
 function Server_GameCustomMessage(game, playerID, payload, setReturn)
 	local playerData = Mod.PlayerGameData[playerID];
 	print(playerData)
+	if playerData == nil then playerData = {}; end
 	if payload.Message == "HasSeenPlayMessage" then
 		playerData.HasSeenPlayMessage = true;
 	elseif payload.Message == "LastTurnSinceMessage" then
