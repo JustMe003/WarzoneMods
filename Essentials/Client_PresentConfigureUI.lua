550a296efbb6f1d94399e2af932cd275f5dd4d4f
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-01-27 11:38:50 +0100

    updated Essentials

--- Essentials/Client_PresentConfigureUI.lua
+++ Essentials/Client_PresentConfigureUI.lua
@@ -64,8 +64,7 @@ function pageUp()
 	if page + 1 <= #pages then
 		page = page + 1;
 		pages[page]();
-	end
-	if page + 1 > #pages then
+	elseif page == #pages then
 		page = 1;
 		pages[page]();
 	end
@@ -75,8 +74,7 @@ function pageDown()
 	if page - 1 >= 1 then
 		page = page - 1
 		pages[page]();
-	end
-	if page - 1 < 1 then
+	elseif page == 1 then
 		page = #pages;
 		pages[page]();
 	end
@@ -88,6 +86,7 @@ function pageOne()
 		if getCurrentWindow() ~= win then
 			destroyWindow(getCurrentWindow());
 			restoreWindow(win);
+			updateText("pageNumber", "1 / " .. #pages);
 		end
 	else
 		destroyWindow(getCurrentWindow());
@@ -95,14 +94,16 @@ function pageOne()
 		local vert = newVerticalGroup("vert", "root");
 		showButtons(vert);
 		newLabel("DescriptionLabel", vert, "Check all the mods the game will use. Currently " .. modsChecked .. " mods checked", "Orange Red");
+		newLabel("MoreInfoInExtras", vert, "For more information about the mods their colors please go to [Extras]", "Orange");
 		for _, str in pairs(getSubArray(mods, 1, 10)) do
 			local line = newHorizontalGroup(str .. "Hor", vert)
 			newCheckbox(str, line, " ", modsInit[str], true, function() updateLabel(str); end);
-			newLabel(str .. "Label", line, str);
+			newLabel(str .. "Label", line, str, getColorFromStatus(str));
 		end
 		local line = newHorizontalGroup("buttons", vert);
-		local but1 = newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
-		local but2 = newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
+		newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
+		newLabel("pageNumber", line, "1 / " .. #pages, "Orchid") 
+		newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
 	end
 	allObjects = getAllObjects();
 end
@@ -113,6 +114,7 @@ function pageTwo()
 		if getCurrentWindow() ~= win then
 			destroyWindow(getCurrentWindow());
 			restoreWindow(win);
+			updateText("pageNumber", "2 / " .. #pages);
 		end
 	else
 		destroyWindow(getCurrentWindow());
@@ -120,14 +122,16 @@ function pageTwo()
 		local vert = newVerticalGroup("vert", "root");
 		showButtons(vert);
 		newLabel("DescriptionLabel", vert, "Check all the mods the game will use. Currently " .. modsChecked .. " mods checked", "Orange Red");
+		newLabel("MoreInfoInExtras", vert, "For more information about the mods their colors please go to [Extras]", "Orange");
 		for _, str in pairs(getSubArray(mods, 11, 20)) do
 			local line = newHorizontalGroup(str .. "Hor", vert);
 			newCheckbox(str, line, " ", modsInit[str], true, function() updateLabel(str); end);
-			newLabel(str .. "Label", line, str);
+			newLabel(str .. "Label", line, str, getColorFromStatus(str));
 		end
 		local line = newHorizontalGroup("buttons", vert);
-		local but1 = newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
-		local but2 = newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
+		newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
+		newLabel("pageNumber", line, "2 / " .. #pages, "Orchid") 		
+		newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
 	end
 	allObjects = getAllObjects();
 end
@@ -138,6 +142,7 @@ function pageThree()
 		if getCurrentWindow() ~= win then
 			destroyWindow(getCurrentWindow());
 			restoreWindow(win);
+			updateText("pageNumber", "3 / " .. #pages);
 		end
 	else
 		destroyWindow(getCurrentWindow());
@@ -145,14 +150,16 @@ function pageThree()
 		local vert = newVerticalGroup("vert", "root");
 		showButtons(vert);
 		newLabel("DescriptionLabel", vert, "Check all the mods the game will use. Currently " .. modsChecked .. " mods checked", "Orange Red");
+		newLabel("MoreInfoInExtras", vert, "For more information about the mods their colors please go to [Extras]", "Orange");
 		for _, str in pairs(getSubArray(mods, 21, 30)) do
 			local line = newHorizontalGroup(str .. "Hor", vert);
 			newCheckbox(str, line, " ", modsInit[str], true, function() updateLabel(str); end);
-			newLabel(str .. "Label", line, str);
+			newLabel(str .. "Label", line, str, getColorFromStatus(str));
 		end
 		local line = newHorizontalGroup("buttons", vert);
-		local but1 = newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
-		local but2 = newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
+		newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
+		newLabel("pageNumber", line, "3 / " .. #pages, "Orchid") 
+		newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
 	end
 	allObjects = getAllObjects();
 end
@@ -163,6 +170,7 @@ function pageFour()
 		if getCurrentWindow() ~= win then
 			destroyWindow(getCurrentWindow());
 			restoreWindow(win);
+			updateText("pageNumber", "4 / " .. #pages);
 		end
 	else
 		destroyWindow(getCurrentWindow());
@@ -170,14 +178,16 @@ function pageFour()
 		local vert = newVerticalGroup("vert", "root");
 		showButtons(vert);
 		newLabel("DescriptionLabel", vert, "Check all the mods the game will use. Currently " .. modsChecked .. " mods checked", "Orange Red");
+		newLabel("MoreInfoInExtras", vert, "For more information about the mods their colors please go to [Extras]", "Orange");
 		for _, str in pairs(getSubArray(mods, 31, 40)) do
 			local line = newHorizontalGroup(str .. "Hor", vert);
 			newCheckbox(str, line, " ", modsInit[str], true, function() updateLabel(str); end);
-			newLabel(str .. "Label", line, str);
+			newLabel(str .. "Label", line, str, getColorFromStatus(str));
 		end
 		local line = newHorizontalGroup("buttons", vert);
-		local but1 = newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
-		local but2 = newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
+		newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
+		newLabel("pageNumber", line, "4 / " .. #pages, "Orchid") 
+		newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
 	end
 	allObjects = getAllObjects();
 end
@@ -188,6 +198,7 @@ function pageFive()
 		if getCurrentWindow() ~= win then
 			destroyWindow(getCurrentWindow());
 			restoreWindow(win);
+			updateText("pageNumber", "5 / " .. #pages);
 		end
 	else
 		destroyWindow(getCurrentWindow());
@@ -195,14 +206,16 @@ function pageFive()
 		local vert = newVerticalGroup("vert", "root");
 		showButtons(vert);
 		newLabel("DescriptionLabel", vert, "Check all the mods the game will use. Currently " .. modsChecked .. " mods checked", "Orange Red");
+		newLabel("MoreInfoInExtras", vert, "For more information about the mods their colors please go to [Extras]", "Orange");
 		for _, str in pairs(getSubArray(mods, 41, #mods)) do
 			local line = newHorizontalGroup(str .. "Hor", vert);
 			newCheckbox(str, line, " ", modsInit[str], true, function() updateLabel(str); end);
-			newLabel(str .. "Label", line, str);
+			newLabel(str .. "Label", line, str, getColorFromStatus(str));
 		end
 		local line = newHorizontalGroup("buttons", vert);
-		local but1 = newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
-		local but2 = newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
+		newButton("PageDown", line, "Page down", pageDown, "Hot Pink")
+		newLabel("pageNumber", line, "5 / " .. #pages, "Orchid") 
+		newButton("PageUp", line, "Page up", pageUp, "Hot Pink");
 	end
 	allObjects = getAllObjects();
 end
@@ -221,6 +234,15 @@ function showExtras()
 		showButtons(vert);
 		newLabel("credits", vert, "This UI.lua file was a lot of work and probably still will throw errors. If you're missing anything or having trouble using it please message me", "Light Blue")
 		newLabel("credits2", vert, "Note that this mod will add the mod list to the description. This way every player has the ability to know which mods are being used without reading the settings", "Royal Blue");
+		newLabel("modColorsStart", vert, "You might have noticed the mods in the window [Settings] are all coloured. These colors indicate in what state the mod is:", "Cyan")
+		newLabel("modColorExampleTrusted", vert, "(Mod Name)   Trusted", "Lime");
+		newLabel("modColorTrusted", vert, "This color (Lime) indicates the mod is trusted. This means that this mod is generally free of bugs and (almost) never crashes. These type of mods can be included in tournaments", "Cyan");
+		newLabel("modColorExampleStandard", vert, "(Mod Name)   Standard", "Orange");
+		newLabel("modColorStandard", vert, "This color (Orange) indicates the mod has the state 'standard'. This is some sort of state between trusted and experimental and generally means this mod is very unlikely to crash your game", "Cyan");
+		newLabel("modColorExampleExperimental", vert, "(Mod Name)   Experimental", "Orange Red");
+		newLabel("modColorExperimental", vert, "This is the lowest state a mod can be in and be public, named 'experimental'. These mods are mostly (relatively) new and / or are rarely used. In order to make these mods 'standard' they have to be used in a lot of games without the game crashing which takes time", "Cyan");
+		newLabel("modColorExamplePicked", vert, "(Mod Name)   Picked", "#3333FF");
+		newLabel("modColorPicked", vert, "This color indicates only that you have checked its checkbox", "Cyan");
 	end
 	allObjects = getAllObjects();
 end
@@ -228,7 +250,7 @@ end
 function updateLabel(name)
 	if getIsChecked(name) then
 		modsChecked = modsChecked + 1;
-		updateColor(name .. "Label", "#33FF3");
+		updateColor(name .. "Label", "#3333FF");
 	else
 		modsChecked = modsChecked - 1;
 		updateColor(name .. "Label", getColorFromStatus(name));
@@ -264,6 +286,12 @@ end
 
 
 function getColorFromStatus(mod)
-	local t = getStatus();
-	for i, v in pairs(t) do print(i, v); end
+	local status = getStatus(mod);
+	if status == "trusted" then
+		return "Lime";
+	elseif status == "standard" then
+		return "Orange";
+	else
+		return "Orange Red";
+	end
 end
--- Essentials/utilities.lua
+++ Essentials/utilities.lua
@@ -1,3 +1,15 @@
 function getMods()
 	return {"Buy Neutral", "Diplomacy", "Gift Armies", "Gift Gold", "Picks Swap", "Randomized Bonuses", "Randomized Wastelands", "Advanced Diplo Mod V4", "AIs don't deploy", "Buy Cards", "Commerce Plus", "Custom Card Package", "Diplomacy 2", "Late Airlifts", "Limited Attacks", "Limited Multiattacks", "Neutral Moves", "Safe Start", "Swap Territories", "Take Turns", "AI's don't play cards", "Better don't fail an attack", "Bonus Airlift", "BonusValue QuickOverrider", "Connected Commander", "Deployment Limit", "Don't lose a territory", "Extended Randomized Bonuses", "Extended Winning Conditions", "Forts", "Gift Armies 2", "Hybrid Distribution", "Hybrid Distribution 2", "Infromations for spectaters", "King of the Hills", "Lotto Mod -> Instant Random Winner", "More_Distributions", "One Way Connections", "Random Starting Cities", "Runtime Wastelands", "Spawnbarriers", "Special units are medics", "Stack Limit", "Transport Only Airlift"}
 end
+
+function getStatus(mod)
+	local index;
+	for i, v in pairs(getMods()) do if mod == v then index = i; end end
+	if index <= 7 then 
+		return "trusted"; 
+	elseif index <= 20 then 
+		return "standard"; 
+	else 
+		return "experimental"; 
+	end
+end
