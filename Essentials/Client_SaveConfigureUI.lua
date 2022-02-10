24c595a7b717b822a0f3f53a4a624f64780e5f16
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-02-10 17:19:09 +0100

    Update Client_SaveConfigureUI.lua

--- Essentials/Client_SaveConfigureUI.lua
+++ Essentials/Client_SaveConfigureUI.lua
@@ -1,7 +1,6 @@
 function Client_SaveConfigureUI(alert)
 	local t = {};
 	for _, mod in pairs(modList) do
-		print(mod);
 		if objectExists(mod) then
 			if getIsChecked(mod) then
 				t[mod] = true;
