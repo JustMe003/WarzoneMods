a31b26b0b8541917558817aa0587f81d7ec5cdcd
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-02-28 18:08:07 +0100

    Added Silent Surrender

---
+++ Silent Surrender/Server_AdvanceTurn.lua
@@ -0,0 +1,16 @@
+function Server_AdvanceTurn_Start(game, addNewOrder)
+	for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
+		print(player.BootedDuration);
+		if player.HumanTurnedIntoAI and player.BootedDuration == nil then
+			local modifications = {};
+			for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+				if terr.OwnerPlayerID == player.ID then
+					local mod = WL.TerritoryModification.Create(terr.ID);
+					mod.SetOwnerOpt = WL.PlayerID.Neutral;
+					table.insert(modifications, mod);
+				end
+			end
+			addNewOrder(WL.GameOrderEvent.Create(player.ID, game.ServerGame.Game.PlayingPlayers[player.ID].DisplayName(nil, false) .. " silently surrendered", nil, modifications));
+		end
+	end
+end
