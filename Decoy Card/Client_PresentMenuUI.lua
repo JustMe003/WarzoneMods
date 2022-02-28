6d51a040897153d1ed2851a2f897ba1f5005c095
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-02-28 16:32:09 +0100

    Update Client_PresentMenuUI.lua

--- Decoy Card/Client_PresentMenuUI.lua
+++ Decoy Card/Client_PresentMenuUI.lua
@@ -95,7 +95,7 @@ function createCard(terrDetails)
 		local vert = newVerticalGroup("vert", "root");
 		newLabel("terrDetailsName", vert, "You have chosen " .. terrDetails.Name .. "\nSpecify how many armies will be visible on the territory", "Light Blue")
 		local numberOfArmies = newNumberField("numberOfArmies", vert, 0, 100, 20);
-		newButton("playCard", vert, "Play Decoy Card", function() playCard(terrDetails, math.max(numberOfArmies, 0); end, "Lime");
+		newButton("playCard", vert, "Play Decoy Card", function() playCard(terrDetails, math.max(numberOfArmies, 0)); end, "Lime");
 	end
 end
 
