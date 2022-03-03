95bb599a56b2e4e7b8cbd5d3480044e98c3ee6e7
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-03 12:21:52 +0100

    Update Client_PresentMenuUI.lua

--- Silent Surrender/Client_PresentMenuUI.lua
+++ Silent Surrender/Client_PresentMenuUI.lua
@@ -1,5 +1,6 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	if Mod.PublicGameData.Prints == nil then return; end
 	for _, v in pairs(Mod.PublicGameData.Prints) do
 		UI.CreateLabel(vert).SetText(tostring(v));
 	end
