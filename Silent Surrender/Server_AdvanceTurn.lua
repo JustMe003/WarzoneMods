57767342fa9b1b9c68cbd1a3b5237385878edeaf
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-03 12:21:00 +0100

    Update Server_AdvanceTurn.lua

--- Silent Surrender/Server_AdvanceTurn.lua
+++ Silent Surrender/Server_AdvanceTurn.lua
@@ -1,6 +1,16 @@
 function Server_AdvanceTurn_Start(game, addNewOrder)
+	local data = Mod.PublicGameData;
+	local prints = {};
+	for _, v in pairs(game.ServerGame.PendingStateTransitions) do
+		table.insert(prints, v.NewState);
+		table.insert(prints, v.PlayerID);
+		table.insert(prints, v.TakingOverForAI);
+		table.insert(prints, v.TurningIntoAI);
+	end
 	for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
-		print(player.BootedDuration);
+		table.insert(prints, player.BootedDuration);
+		table.insert(prints, player.Surrendered);
+		table.insert(prints, player.HumanTurnedIntoAI);
 		if player.HumanTurnedIntoAI and player.Surrendered then
 			local modifications = {};
 			for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
@@ -13,4 +23,6 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
 			addNewOrder(WL.GameOrderEvent.Create(player.ID, game.ServerGame.Game.PlayingPlayers[player.ID].DisplayName(nil, false) .. " silently surrendered", nil, modifications));
 		end
 	end
+	data.Prints = prints;
+	Mod.PublicGameData = data;
 end
