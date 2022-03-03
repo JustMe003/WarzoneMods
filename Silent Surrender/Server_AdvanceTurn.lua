aa61a84a9578db68acf52e0ada76f5923d4eefe3
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-03 12:26:17 +0100

    Update Server_AdvanceTurn.lua

--- Silent Surrender/Server_AdvanceTurn.lua
+++ Silent Surrender/Server_AdvanceTurn.lua
@@ -2,15 +2,15 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
 	local data = Mod.PublicGameData;
 	local prints = {};
 	for _, v in pairs(game.ServerGame.PendingStateTransitions) do
-		table.insert(prints, "new state " .. v.NewState);
-		table.insert(prints, "playerID " .. v.PlayerID);
-		table.insert(prints, "taking over AI " .. v.TakingOverForAI);
-		table.insert(prints, "turning into AI " .. v.TurningIntoAI);
+		table.insert(prints, "new state " .. tostring(v.NewState));
+		table.insert(prints, "playerID " .. tostring(v.PlayerID));
+		table.insert(prints, "taking over AI " .. tostring(v.TakingOverForAI));
+		table.insert(prints, "turning into AI " .. tostring(v.TurningIntoAI));
 	end
 	for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
 		table.insert(prints, "booted ion " .. tostring(player.BootedDuration));
-		table.insert(prints, "surrendered " .. player.Surrendered);
-		table.insert(prints, "human turned into AI " .. player.HumanTurnedIntoAI);
+		table.insert(prints, "surrendered " .. tostring(player.Surrendered));
+		table.insert(prints, "human turned into AI " .. tostring(player.HumanTurnedIntoAI));
 		if player.HumanTurnedIntoAI and player.Surrendered then
 			local modifications = {};
 			for _, terr in pairs(game.ServerGame.LatestTurnStanding.Territories) do
