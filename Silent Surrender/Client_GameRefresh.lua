bc099319db05f1d6f901fc5109e9a5d9dbf9bf93
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-06 18:18:32 +0100

    Update Client_GameRefresh.lua

--- Silent Surrender/Client_GameRefresh.lua
+++ Silent Surrender/Client_GameRefresh.lua
@@ -1,8 +1,7 @@
 function Client_GameRefresh(game)
 	if game.Us == nil then return; end
+	
 	if not game.Us.HumanTurnedIntoAI then
-		for i, v in pairs(WL.GamePlayerState) do
-			print(i, v);
-		end
+		
 	end
 end
