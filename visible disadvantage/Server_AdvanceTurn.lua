81c9f9802dbe07ec6c0eaf7d4d39f4c76dece7f6
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-06 18:31:07 +0100

    Update Server_AdvanceTurn.lua

--- visible disadvantage/Server_AdvanceTurn.lua
+++ visible disadvantage/Server_AdvanceTurn.lua
@@ -10,7 +10,7 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 			for _, player in pairs(game.Game.PlayingPlayers) do
 				if player.Income(0, game.ServerGame.LatestTurnStanding, true, false).Total == mostIncome then
 					for _, player2 in pairs(game.Game.PlayingPlayers) do
-						if player.ID ~= player2.ID then
+						if player.ID ~= player2.ID and not sameTeam(player, player2) then
 							local instance = WL.NoParameterCardInstance.Create(WL.CardID.Spy);
 							addNewOrder(WL.GameOrderReceiveCard.Create(player2.ID, {instance}));
 							addNewOrder(WL.GameOrderPlayCardSpy.Create(instance.ID, player2.ID, player.ID));
@@ -20,4 +20,9 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 			end
 		end
 	end
+end
+
+function sameTeam(player, player2)
+	if player.Team == -1 then return false; end
+	return player.Team == player2.Team;
 end
