ff352498c0bc6dc786f2a578b91f325dadbc2fd6
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-18 09:35:48 +0100

    Update Server_Created.lua

--- visible disadvantage/Server_Created.lua
+++ visible disadvantage/Server_Created.lua
@@ -3,5 +3,8 @@ function Server_Created(game, settings)
 	if settings.Cards[WL.CardID.Spy] == nil then
 		cards[WL.CardID.Spy] = WL.CardGameSpy.Create(1, 0, 0, 0, 1, false);
 	end
+	for i, v in pairs(game.Settings.Cards) do
+		print(i, v);
+	end
 	settings.Cards = cards;
 end
