976e222dbeb731186a631c52b17e5beacbd4c740
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-27 16:01:15 +0200

    Update Client_PresentConfigureUI.lua

--- custom scenario structures/Client_PresentConfigureUI.lua
+++ custom scenario structures/Client_PresentConfigureUI.lua
@@ -1,6 +1,7 @@
 require("UI");
 function Client_PresentConfigureUI(rootParent)
 	init(rootParent);
+	print("hi")
 	local vert = newVerticalGroup("vert", "root");
 	newLabel("desc", vert, "Paste the string you copied from singleplayer");
 	local text = Mod.Settings.Data;
