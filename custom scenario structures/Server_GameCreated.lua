cabb6703b834661a640393a2526d2be09ab7e885
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-03-28 12:07:32 +0200

    Update Server_GameCreated.lua

--- custom scenario structures/Server_GameCreated.lua
+++ custom scenario structures/Server_GameCreated.lua
@@ -1,4 +1,5 @@
 function Server_GameCreated(game, settings)
+	print(game.Settings.MultiPlayer, Mod.Settings.Testing);
 	if not game.Settings.MultiPlayer and not Mod.Settings.Testing then
 		settings.FogLevel = 3;
 	end
