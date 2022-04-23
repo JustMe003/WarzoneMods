67851fa5946d40d13cce11300c95ed8b6e1844dc
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-04-23 10:45:00 +0200

    Update Client_PresentSettingsUI.lua

--- Capture The Flag/Client_PresentSettingsUI.lua
+++ Capture The Flag/Client_PresentSettingsUI.lua
@@ -1,4 +1,7 @@
-
+require("UI");
 function Client_PresentSettingsUI(rootParent)
-	
+	init(rootParent);
+	window("showSettings");
+	local vert = newHorizontalGroup("vert", "root");
+	newLabel("intro", vert, "This are the settings for this game", "Lime");
 end
