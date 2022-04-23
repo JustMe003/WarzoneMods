3501887a1fd62bf0d39f037251e701bf2bc8ff3d
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-04-23 10:53:18 +0200

    Update Client_PresentSettingsUI.lua

--- Capture The Flag/Client_PresentSettingsUI.lua
+++ Capture The Flag/Client_PresentSettingsUI.lua
@@ -2,6 +2,9 @@ require("UI");
 function Client_PresentSettingsUI(rootParent)
 	init(rootParent);
 	window("showSettings");
-	local vert = newHorizontalGroup("vert", "root");
+	local vert = newVerticalroup("vert", "root");
 	newLabel("intro", vert, "This are the settings for this game", "Lime");
+	local line = newHorizontalGroup("line1", vert);
+	newLabel("nFlags", line, "Number of flags each team starts with \n(note that this value might not be the true value)", "Lime"); 
+	newLabel("nFlagsInt", line, Mod.Settings.NumberOfFlags, "Royal Blue");
 end
