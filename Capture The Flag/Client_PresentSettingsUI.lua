1344e70499eae131aa805ce868b008029d76015a
JustMe003 <51970740+JustMe003@users.noreply.github.com>
2022-04-23 11:19:02 +0200

    Update Client_PresentSettingsUI.lua

--- Capture The Flag/Client_PresentSettingsUI.lua
+++ Capture The Flag/Client_PresentSettingsUI.lua
@@ -7,7 +7,10 @@ function Client_PresentSettingsUI(rootParent)
 	local line = newHorizontalGroup("line1", vert);
 	newLabel("nFlags", line, "Number of flags each team starts with: ", "Lime"); 
 	newLabel("nFlagsInt", line, Mod.Settings.FlagsPerTeam, "Royal Blue");
+	line = newHorizontalGroup("line2", vert);
 	newLabel("nFlags", line, "Teams are eliminated after losing X flags: ", "Lime"); 
 	newLabel("nFlagsInt", line, Mod.Settings.NFlagsForLose, "Royal Blue");
-
+	line = newHorizontalGroup("line3", vert);
+	newLabel("nFlags", line, "Players get X extra income on a flag capture: ", "Lime"); 
+	newLabel("nFlagsInt", line, Mod.Settings.IncomeBoost, "Royal Blue");
 end
