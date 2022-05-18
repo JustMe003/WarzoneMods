72df9bdb404e35901eae4cecbdb2f4176782ae27
JustMe003 <kingoflol20033@gmail.com>
2022-05-18 11:39:46 +0200

    Update Client_PresentMenuUI.lua

--- Artillery Strike/Client_PresentMenuUI.lua
+++ Artillery Strike/Client_PresentMenuUI.lua
@@ -6,6 +6,11 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close
 	setMaxSize(500, 400);
 	setSize = setMaxSize;
 	if game.Us == nil then return; end
+	if game.Game.TurnNumber < 0 then
+		UI.Alert("This mod cannot be used in the distribution turn");
+		close();
+		return;
+	end
 	showMenu();
 end
 
