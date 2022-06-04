488827b0a40956cf6bc06b2b78dc8c71f74a3637
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-06-04 09:21:37 +0200

    Update Server_AdvanceTurn.lua

--- Bypass Negative Sanction Limit/Server_AdvanceTurn.lua
+++ Bypass Negative Sanction Limit/Server_AdvanceTurn.lua
@@ -1,3 +1,19 @@
 function Server_AdvanceTurn_End(game, addNewOrder)
-
+	if game.Settings.Cards == nil then return; end
+	if game.Settings.Cards[WL.CardID.Sanctions] == nil then return; end
+	if game.Settings.Cards[WL.CardID.Sanctions].Percentage >= 0 then return; end
+	if game.ServerGame.LatestTurnStanding.ActiveCards == nil then return; end
+	local t = {};
+	for pid, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
+		t[pid] = 0
+	end
+	for _, ac in pairs(game.ServerGame.LatestTurnStanding.ActiveCards) do
+		if ac.Card.CardID == WL.CardID.Sanction then
+			t[ac.Card.SanctionedPlayerID] = t[ac.Card.SanctionedPlayerID] + 1;
+		end
+	end
+	for pid, v in pairs(t) do
+		local income = game.ServerGame.Game.PlayingPlayers[pid].Income.Total;
+		local mod = WL.IncomeMod.Create(pid, math.min(1000000, income * math.pow(game.Settings.Cards[WL.CardID.Sanctions].Percentage, v)) - 10000, "Bypass negative sanctions limit");
+	end
 end
