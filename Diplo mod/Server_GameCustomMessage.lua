637683d9713624349bc1502ed9548857f2eb4d0c
JustMe003 <kingoflol20033@gmail.com>
2022-06-10 18:48:33 +0200

    Update Server_GameCustomMessage.lua

--- Diplo mod/Server_GameCustomMessage.lua
+++ Diplo mod/Server_GameCustomMessage.lua
@@ -89,8 +89,8 @@ function fiveMinuteAlert(game, playerID, payload, setReturn)
 	local playerData = Mod.PlayerGameData;
 	if playerData[playerID] == nil then playerData[playerID] = {}; end
 	playerData[playerID].LastMessage = payload.NewTime;
-	playerData[playerID].NumberOfNotifications = count(playerData.Notifications, function(t) return #t; end);
 	playerData[playerID].Notifications = resetPlayerNotifications(playerData[playerID].Notifications);
+	playerData[playerID].NumberOfNotifications = count(playerData.Notifications, function(t) return #t; end);
 	Mod.PlayerGameData = playerData;
 end
 
