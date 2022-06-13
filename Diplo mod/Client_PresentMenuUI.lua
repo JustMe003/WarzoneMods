87e26205535e72ae6c1486a2b8f7a5d81d4cae59
JustMe003 <kingoflol20033@gmail.com>
2022-06-13 15:57:02 +0200

    Update Client_PresentMenuUI.lua

--- Diplo mod/Client_PresentMenuUI.lua
+++ Diplo mod/Client_PresentMenuUI.lua
@@ -258,7 +258,7 @@ function showFactionChat()
 	for i = #Mod.PublicGameData.Factions[Mod.PublicGameData.PlayerInFaction[game.Us.ID]].FactionChat, 1, -1 do
 		local message = Mod.PublicGameData.Factions[Mod.PublicGameData.PlayerInFaction[game.Us.ID]].FactionChat[i];
 		local line = newHorizontalGroup("line" .. i, vert);
-		newLabel(win .. "player" .. i, line, game.Game.Players[message.Player].DisplayName(nil, false) .. ": ", game.Game.Players[message.Player].Color.HtmlColor, 200, 20, 1, 1);
+		newLabel(win .. "player" .. i, line, game.Game.Players[message.Player].DisplayName(nil, false) .. ": ", game.Game.Players[message.Player].Color.HtmlColor, 200, -1, 1, 1);
 		newLabel(win .. "text" .. i, line, message.Text, game.Game.Players[message.Player].Color.HtmlColor);
 	end
 end
