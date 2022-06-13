d137897a19309fe66279ec49f1f6160cbab02693
JustMe003 <kingoflol20033@gmail.com>
2022-06-13 22:59:19 +0200

    Update Client_PresentConfigureUI.lua

--- Diplo mod/Client_PresentConfigureUI.lua
+++ Diplo mod/Client_PresentConfigureUI.lua
@@ -25,7 +25,7 @@ function showMain()
 	newButton(win .. "AddSlotConfig", line, "Add slot", pickSlot, "Aqua");
 	newLabel(win .. "EmptyAfterAddFaction", vert, " ");
 	for i, _ in pairs(config.Factions) do
-		newButton(win .. i, vert, i, function() showFactionConfig(faction); end);
+		newButton(win .. i, vert, i, function() showFactionConfig(i); end);
 	end
 end
 
@@ -75,14 +75,14 @@ function showFactionConfig(faction)
 	newLabel(win .. "FactionName", vert, faction .. " (configuration)\n");
 	local line = newHorizontalGroup(win .. "line", vert);
 	newLabel(win .. "FactionLeaderString", line, "Faction leader: ");
-	newButton(win .. "SetFactionLeader", line, getFactionLeader(faction), function() PromptForFactionLeader(faction) end, "Yellow");
+	newButton(win .. "SetFactionLeader", line, getFactionLeader(faction), function() PickForFactionLeader(faction) end, "Yellow");
 	newLabel(win .. "EmptyAfterFactionLeader", vert, "\nFaction members:");
 	for i, v in pairs(config.Factions[faction].FactionMembers) do
 		newLabel(win .. i .. "Slot", vert, getSlotName(v));
 	end
 	local line = newHorizontalGroup(win .. "line2", vert);
-	newButton(win .. "AddSlots", line, "Add slot", function() promptForAddingSlot(faction); end, "Green");
-	newButton(win .. "RemoveSlots", line, "Remove slot", function() promptForRemovingSlot(faction); end, "Red");
+	newButton(win .. "AddSlots", line, "Add slot", function() PickToAddSlot(faction); end, "Green");
+	newButton(win .. "RemoveSlots", line, "Remove slot", function() PickToRemoveSlot(faction); end, "Red");
 	newLabel(win .. "EmptyAfterSlotsConfig", vert, " ");
 	newButton(win .. "ShowFactionRelation", vert, "Relation configuration", function() showFactionRelationConfig(faction); end, "Aqua");
 end
@@ -110,7 +110,7 @@ function showFactionRelationConfig(faction)
 	end
 end
 
-function promptForAddingSlot(faction)
+function PickToAddSlot(faction)
 	local payload = {};
 	for i = 0, 49 do
 		if config.SlotInFaction[i] == nil and config.Factions[faction].FactionLeader ~= i then
@@ -126,7 +126,7 @@ function addSlotToFaction(faction, slot)
 	showFactionConfig(faction);
 end
 
-function promptForRemovingSlot(faction)
+function PickToRemoveSlot(faction)
 local payload = {};
 	for i, v in pairs(config.Factions[faction].FactionMembers) do
 		if v ~= config.Factions[faction].FactionLeader then
@@ -142,11 +142,11 @@ function removeSlotFromFaction(faction, index, slot)
 	showFactionConfig(faction);
 end
 
-function PromptForFactionLeader(faction)
+function PickForFactionLeader(faction)
 	local payload = {};
 	for i = 0, 49 do
 		if config.SlotInFaction[i] == nil then
-			table.insert(payload, {text=getSlotName(i), selected=function() config.Factions[faction].FactionLeader = i; table.insert(config.Factions[faction].FactionMembers, i); showFactionConfig(faction); end});
+			table.insert(payload, {text=getSlotName(i), selected=function() config.SlotInFaction[i] = faction; config.Factions[faction].FactionLeader = i; table.insert(config.Factions[faction].FactionMembers, i); showFactionConfig(faction); end});
 		end
 	end
 	UI.PromptFromList("Which slot will be Faction leader?", payload);
