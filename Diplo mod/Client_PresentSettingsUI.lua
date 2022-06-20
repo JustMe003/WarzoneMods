af7214930c143ffec1089a51348cb8fa63141378
JustMe003 <kingoflol20033@gmail.com>
2022-06-21 01:03:32 +0200

    Update Client_PresentSettingsUI.lua

--- Diplo mod/Client_PresentSettingsUI.lua
+++ Diplo mod/Client_PresentSettingsUI.lua
@@ -4,11 +4,11 @@ function Client_PresentSettingsUI(rootParent)
 	
 	local win = "main";
 	local vert = newVerticalGroup("vert", "root");
-	newLabel(win, vert, "VisibleHistory: " .. tostring(Mod.Settings.GlobalSettingsVisibleHistory));
-	newLabel(win, vert, "AICanDeclareOnPlayer: " .. tostring(Mod.Settings.GlobalSettingsAICanDeclareOnPlayer));
-	newLabel(win, vert, "AICanDeclareOnAI: " .. tostring(Mod.Settings.GlobalSettingsAICanDeclareOnAI));
-	newLabel(win, vert, "ApproveFactionJoins: " .. tostring(Mod.Settings.GlobalSettingsApproveFactionJoins));
-	newLabel(win, vert, "LockPreSetFactions: " .. tostring(Mod.Settings.GlobalSettingsLockPreSetFactions));
-	newLabel(win, vert, "FairFactions: " .. tostring(Mod.Settings.GlobalSettingsFairFactions));
-	newLabel(win, vert, "FairFactionsModifier: " .. Mod.Settings.GlobalSettingsFairFactionsModifier)
+	newLabel(win, vert, "VisibleHistory: " .. tostring(Mod.Settings.GlobalSettings,.VisibleHistory));
+	newLabel(win, vert, "AICanDeclareOnPlayer: " .. tostring(Mod.Settings.GlobalSettings,.AICanDeclareOnPlayer));
+	newLabel(win, vert, "AICanDeclareOnAI: " .. tostring(Mod.Settings.GlobalSettings,.AICanDeclareOnAI));
+	newLabel(win, vert, "ApproveFactionJoins: " .. tostring(Mod.Settings.GlobalSettings,.ApproveFactionJoins));
+	newLabel(win, vert, "LockPreSetFactions: " .. tostring(Mod.Settings.GlobalSettings,.LockPreSetFactions));
+	newLabel(win, vert, "FairFactions: " .. tostring(Mod.Settings.GlobalSettings,.FairFactions));
+	newLabel(win, vert, "FairFactionsModifier: " .. Mod.Settings.GlobalSettings,.FairFactionsModifier)
 end
