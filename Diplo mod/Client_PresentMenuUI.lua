0183bf351a74305a6e3a5f19961d8769f190012f
JustMe003 <kingoflol20033@gmail.com>
2022-06-21 14:10:23 +0200

    Update Client_PresentMenuUI.lua

--- Diplo mod/Client_PresentMenuUI.lua
+++ Diplo mod/Client_PresentMenuUI.lua
@@ -5,7 +5,7 @@ function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close
 	if game.Us == nil then UI.Alert("You cannot use this mod since you're not playing in this game"); close(); return; end
 	if game.Us.State ~= WL.GamePlayerState.Playing then UI.Alert("You cannot use this mod anymore since you're not playing anymore"); close(); return; end
 	if game.Game.TurnNumber < 1 then UI.Alert("This mod can only be used after the distribution turn"); close(); return; end
-	if calledFromGameRefresh ~= nil then
+	if calledFromGameRefresh ~= nil and func ~= nil then
 		func();
 		func = nil;
 	end
