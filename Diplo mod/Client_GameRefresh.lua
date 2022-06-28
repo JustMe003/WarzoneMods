4427b6f5d4f533e1b4d47714f2f6dbeead8387ff
JustMe003 <kingoflol20033@gmail.com>
2022-06-28 12:02:06 +0200

    Update Client_GameRefresh.lua

--- Diplo mod/Client_GameRefresh.lua
+++ Diplo mod/Client_GameRefresh.lua
@@ -216,7 +216,7 @@ function dateIsEarlier(date1, date2)
 	for _, v in pairs(list) do
 		print(v);
 		if date1[v] ~= date2[v] then
-			print(date1[v] .. "\t" .. date2[v]);
+			print(tostring(date1[v]) .. "\t" .. tostring(date2[v]));
 			if date1[v] < date2[v] then
 				return true;
 			else
