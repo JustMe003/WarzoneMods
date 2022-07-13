a3ba2513786697359dd306f367469c7ff90f5dce
JustMe003 <kingoflol20033@gmail.com>
2022-07-13 17:36:40 +0200

    Update Server_AdvanceTurn.lua

--- Improved Limited Multiattacks/Server_AdvanceTurn.lua
+++ Improved Limited Multiattacks/Server_AdvanceTurn.lua
@@ -1,24 +1,30 @@
 function Server_AdvanceTurn_Start (game,addNewOrder)
-	data = Mod.PublicGameData;
+	map = {};
+	for i, _ in pairs(game.ServerGame.LatestTurnStanding.Territories) do
+		map[i] = Mod.Settings.MaxAttacks;
+	end
 end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if order.proxyType == "GameOrderAttackTransfer" then
-		if result.IsAttack then
-			
-		else
-			local numArmies = 0;
-			if not order.ByPercent then
-				numArmies = order.NumArmies.NumArmies;
-			else
-				numArmies = round(game.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies.NumArmies / 100 * order.NumArmies.NumArmies);
+		if not result.IsAttack then
+			if map[order.From] > 0 then
+				local numArmies = 0;
+				if not order.ByPercent then
+					numArmies = order.NumArmies.NumArmies;
+				else
+					numArmies = round(game.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies.NumArmies / 100 * order.NumArmies.NumArmies);
+				end
+				result.ActualArmies = WL.Armies.Create(numArmies, {});
+				map[order.To] = math.min(map[order.To], map[order.From] - 1);
 			end
-			result.ActualArmies = WL.Armies.Create(numArmies, {});
+		elseif result.IsSuccessful then
+			map[order.To] = Mod.Settings.MaxAttacks;
 		end
 	end
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-	Mod.PublicGameData = data;
+	
 end
 
 function round(n)
