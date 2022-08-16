5ce9a80453c1a1b0f5e20e57a1605f7daa442480
JustMe003 <kingoflol20033@gmail.com>
2022-08-16 20:42:07 +0200

    Update Client_PresentSettingsUI.lua

--- Improved Limited Multiattacks/Client_PresentSettingsUI.lua
+++ Improved Limited Multiattacks/Client_PresentSettingsUI.lua
@@ -3,6 +3,6 @@ function Client_PresentSettingsUI(rootParent)
 	local horz = UI.CreateHorizontalLayoutGroup(rootParent);
 	if(Mod.Settings.MaxAttacks ~= 0)then
 		UI.CreateLabel(horz).SetText(Mod.Settings.MaxAttacks .. ' transfer range');
-		UI.CreateButton(horz).SetText('?').SetColor('#4FC5FF').SetOnClick(function() UI.Alert('This setting tells you, what the attack range of the multiattack is. Please note, that you are able to enter orders further then the range, but those will not be executed'); end);
+		UI.CreateButton(horz).SetText('?').SetColor('#00B5FF').SetOnClick(function() UI.Alert('This setting tells you, what the attack range of the multiattack is. Please note, that you are able to enter orders further then the range, but those will not be executed'); end);
 	end
 end
