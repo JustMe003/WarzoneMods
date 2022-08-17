c6549c120e912bd119364be5d73356d0affb7163
JustMe003 <kingoflol20033@gmail.com>
2022-08-17 20:11:11 +0200

    Update Server_AdvanceTurn.lua

--- Improved Limited Multiattacks/Server_AdvanceTurn.lua
+++ Improved Limited Multiattacks/Server_AdvanceTurn.lua
@@ -7,17 +7,17 @@ end
 function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
 	if order.proxyType == "GameOrderAttackTransfer" then
 		if not result.IsAttack then
-			if map[order.From] > 0 then
+			if map[order.From] > 0 and game.ServerGame.LatestTurnStanding.Territories[order.From].OwnerPlayerID == order.PlayerID then
 				local numArmies = 0;
 				if not order.ByPercent then
 					numArmies = order.NumArmies.NumArmies;
 				else
 					numArmies = round(game.ServerGame.LatestTurnStanding.Territories[order.From].NumArmies.NumArmies / 100 * order.NumArmies.NumArmies);
 				end
-				result.ActualArmies = WL.Armies.Create(numArmies, {});
+				result.ActualArmies = WL.Armies.Create(numArmies, result.ActualArmies.SpecialUnits);
 				map[order.To] = math.min(map[order.From] - 1, map[order.To]);
 			else
-				result.ActualArmies = WL.Armies.Create(0, {});
+				result.ActualArmies = WL.Armies.Create(0, result.ActualArmies.SpecialUnits);
 			end
 		elseif result.IsSuccessful then
 			map[order.To] = Mod.Settings.MaxAttacks;
