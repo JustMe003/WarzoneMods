2f08c4ae2975c96565d19ae1197ef928cdd40506
JustMe003 <kingoflol20033@gmail.com>
2022-08-30 11:54:28 +0200

    Update Client_PresentMenuUI.lua

--- Advanced Card Distribution/Client_PresentMenuUI.lua
+++ Advanced Card Distribution/Client_PresentMenuUI.lua
@@ -17,17 +17,14 @@ function showMenu()
 	
 	newLabel(win .. "label", vert, "These players have a modified card distribution");
 	local hasButton = {};
-	for i, v in pairs(WL.GamePlayerState) do
-		print(i, v);
-	end
 	for _, p in pairs(game.Game.Players) do
 		if p.State ~= WL.GamePlayerState.RemovedByHost and p.State ~= WL.GamePlayerState.Declined and p.Slot ~= nil then
-			newButton(win .. p.ID .. "button", vert, p.DisplayName(nil, false) .. " (Slot " .. getSlotName(p.Slot) .. ")", function() showSlotSettings(p.Slot); end, p.Color.HtmlColor);
+			newButton(win .. p.ID .. "button", vert, p.DisplayName(nil, false) .. " (Slot " .. getSlotName(p.Slot) .. ")", function() showSlotSettings(p); end, p.Color.HtmlColor);
 		end
 	end
 end
 
-function showSlotSettings(slot)
+function showSlotSettings(p)
 	local win = "showSlotSettings";
 	if windowExists(win) then
 		resetWindow(win);
@@ -36,27 +33,32 @@ function showSlotSettings(slot)
 	window(win)
 	local vert = newVerticalGroup("vert", "root");
 	
+	newLabel(win .. "explanation1", vert, "(Green numbers are standard Warzone settings, the red numbers are modifications from this mod", "Lime");
+	newLabel(win .. "explanation1", vert, p.DisplayName(nil, false) .. " will start with the following card pieces\n", "Lime");
+	
 	for card, cardGame in pairs(game.Settings.Cards) do
 		local line = newHorizontalGroup(win .. "lineS" .. card, vert);
 		newLabel(win .. card .. "cardS", line, readableString(getCardName(card)) .. ": ", "Royal Blue");
 		newLabel(win .. card .. "initialPieces", line, cardGame.InitialPieces, "Green");
-		if Mod.Settings.CardPiecesFromStart[slot][card] ~= nil then
-			if Mod.Settings.CardPiecesFromStart[slot][card] > 0 then
+		if Mod.Settings.CardPiecesFromStart[p.Slot][card] ~= nil then
+			if Mod.Settings.CardPiecesFromStart[p.Slot][card] > 0 then
 				newLabel(win .. card .. "equation", line, "+ ", "Royal Blue");
 			end
-			newLabel(win .. card .. "S", line, Mod.Settings.CardPiecesFromStart[slot][card], "Orange Red");
+			newLabel(win .. card .. "S", line, Mod.Settings.CardPiecesFromStart[p.Slot][card], "Orange Red");
 		end
 	end
+	
+	newLabel(win .. "explanation2", vert, p.DisplayName(nil, false) .. " will receive the following card pieces each turn: \n", "Lime");
 	
 	for card, cardGame in pairs(game.Settings.Cards) do
 		local line = newHorizontalGroup(win .. "lineT" .. card, vert);
 		newLabel(win .. card .. "cardT", line, readableString(getCardName(card)) .. ": ", "Royal Blue");
 		newLabel(win .. card .. "MinimumPiecesPerTurn", line, cardGame.MinimumPiecesPerTurn, "Green");
-		if Mod.Settings.CardPiecesEachTurn[slot][card] ~= nil then
-			if Mod.Settings.CardPiecesEachTurn[slot][card] > 0 then
+		if Mod.Settings.CardPiecesEachTurn[p.Slot][card] ~= nil then
+			if Mod.Settings.CardPiecesEachTurn[p.Slot][card] > 0 then
 				newLabel(win .. card .. "equation", line, "+ ", "Royal Blue");
 			end
-			newLabel(win .. card .. "T", line, Mod.Settings.CardPiecesEachTurn[slot][card], "Orange Red");
+			newLabel(win .. card .. "T", line, Mod.Settings.CardPiecesEachTurn[p.Slot][card], "Orange Red");
 		end
 	end
 end
