3efee37ce2a827a75fe5706b24cba4760d1250af
JustMe003 <kingoflol20033@gmail.com>
2022-08-30 10:24:01 +0200

    Create Client_PresentMenuUI.lua

---
+++ Advanced Card Distribution/Client_PresentMenuUI.lua
@@ -0,0 +1,30 @@
+require("UI");
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close)
+	init(rootParent);
+	game = Game;
+	
+	showMenu();
+end
+
+function showMenu()
+	local win = "showMenu";
+	if windowExists(win) then
+		resetWindow(win);
+	end
+	destoyWindow(getCurrentWindow());
+	window(win)
+	local vert = newVerticalGroup("vert", "root");
+	
+	for i = 0, 49 do
+		getPlayerSlot(i);
+	end
+end
+
+function getPlayerSlot(n)
+	for _, p in pairs(game.Game.Players) do
+		if p.Slot = n then 
+			print(p.Slot, p.State);
+			p.DisplayName(nil, false); 
+		end
+	end
+end
