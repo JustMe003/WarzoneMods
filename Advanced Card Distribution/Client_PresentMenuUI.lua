aad3bb389d836e756855abb3993e6ee79c6076c2
JustMe003 <kingoflol20033@gmail.com>
2022-08-31 12:35:28 +0200

    Update Client_PresentMenuUI.lua

--- Advanced Card Distribution/Client_PresentMenuUI.lua
+++ Advanced Card Distribution/Client_PresentMenuUI.lua
@@ -2,9 +2,7 @@ require("UI");
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close)
 	init(rootParent);
 	game = Game;
-	
-	for i, v in pairs(WL.ReinforcementCardMode) do print(i, v); end
-	
+		
 	setMaxSize(400, 500);
 	showMenu();
 end
