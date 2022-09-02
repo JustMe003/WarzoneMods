a91c8f62628c88d944f4de4019de01d03496cd18
JustMe003 <kingoflol20033@gmail.com>
2022-09-02 12:23:58 +0200

    Update Client_PresentSettingsUI.lua

--- Late Wastelands/Client_PresentSettingsUI.lua
+++ Late Wastelands/Client_PresentSettingsUI.lua
@@ -1,7 +1,7 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	if Mod.Settings.IsCustomScenario then
-		UI.CreateLabel(vert).SetText("If this game uses a custom scenario then this are the wastelands settings:\nNumber of wastelands: " .. Mod.Settings.NumOfWastelands .. "\nSize of wastelands: " .. Mod.Settings.WastelandSize);
+		UI.CreateLabel(vert).SetText("If this game uses a custom scenario then this are the wastelands settings\n\nNumber of wastelands: " .. Mod.Settings.NumOfWastelands .. "\nSize of wastelands: " .. Mod.Settings.WastelandSize);
 	else
 		UI.CreateLabel(vert).SetText("This mod uses the normal, Warzone wastelands settings.");
 	end
