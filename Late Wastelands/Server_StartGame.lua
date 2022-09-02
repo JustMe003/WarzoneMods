84155064454eba44f9283e4d2b82d5638569e41a
JustMe003 <kingoflol20033@gmail.com>
2022-09-02 12:29:00 +0200

    Update Server_StartGame.lua

--- Late Wastelands/Server_StartGame.lua
+++ Late Wastelands/Server_StartGame.lua
@@ -1,19 +1,21 @@
 function Server_StartGame(game, standing)
-	if game.Settings.AutomaticTerritoryDistribution then return; end
+	local numOfWastelands = game.Settings.NumberOfWastelands;
+	local wastelandSize = game.Settings.WastelandSize;
+	print(numOfWastelands, wastelandSize);
+	if game.Settings.CustomScenario ~= nil and Mod.Settings.IsCustomScenario then numOfWastelands = Mod.Settings.NumOfWastelands; wastelandSize = Mod.Settings.WastelandSize;
+	elseif game.Settings.AutomaticTerritoryDistribution then return; end
 	
 	local terrList = {};
 	for _, terr in pairs(standing.Territories) do
 		if terr.IsNeutral then
 			table.insert(terrList, terr.ID);
 		end
-	end
-
-
+	end	
 	
-	for i = 1, math.min(game.Settings.NumberOfWastelands, #terrList) do
+	for i = 1, math.min(numOfWastelands, #terrList) do
 		local rand = math.random(#terrList);
 		local terr = standing.Territories[terrList[rand]];
-		terr.NumArmies = WL.Armies.Create(game.Settings.WastelandSize, {});
+		terr.NumArmies = WL.Armies.Create(wastelandSize, {});
 		standing.Territories[terrList[rand]] = terr;
 		table.remove(terrList, rand);
 	end
