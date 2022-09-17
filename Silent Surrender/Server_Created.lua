420a18ea0482ece8f830800971fca3ce764e7991
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-09-17 09:33:00 +0200

    Update Server_Created.lua

--- Silent Surrender/Server_Created.lua
+++ Silent Surrender/Server_Created.lua
@@ -1,3 +1,7 @@
+require("JSON");
 function Server_Created(game, settings)
-	settings.SurrenderedPlayersTurnIntoAIs = true;
+	-- settings.SurrenderedPlayersTurnIntoAIs = true;
+        local t = {193, "didheb", 8};
+        local json = getJSON(t);
+        print(json);
 end
