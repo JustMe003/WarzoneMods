79f31417315047c6b910857157a6fd377c84e2ec
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-09-17 12:36:47 +0200

    Create Server_StartGame.lua

---
+++ Dreadfully Hypothermia 4/Server_StartGame.lua
@@ -0,0 +1,9 @@
+Server_StartGame(game, standing)
+    local t = {};
+    for _, v in pairs(standing.Territories) do
+        if not v.IsNeutral and t[v.OwnerPlayerID] == nil then
+            standing.Territories[v.ID].NumArmies = WL.Armies.Create(standing.Territories[v.ID].NumArmies.NumArmies, {WL.Boss3.Create(v.OwnerPlayerID)})
+            t[v.OwnerPlayerID] = true;
+        end
+    end
+end
