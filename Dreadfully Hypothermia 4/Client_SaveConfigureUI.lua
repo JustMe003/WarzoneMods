8b6d0fc9ae7f86ac0e11850878a3d3a2d5d07be7
JustMe003 <kingoflol20033@gmail.com>
2022-09-18 00:23:57 +0200

    Delete Client_SaveConfigureUI.lua

--- Dreadfully Hypothermia 4/Client_SaveConfigureUI.lua
+++
@@ -1,3 +0,0 @@
-function Client_SaveConfigureUI(alert)
-	UI.Alert("WARNING\nYou've included the Dreadful Hypothermia 4 mod in your game. This mod will bring CHAOS, and CHAOS only. Use at your own risk (read mod description for more information).");
-end
