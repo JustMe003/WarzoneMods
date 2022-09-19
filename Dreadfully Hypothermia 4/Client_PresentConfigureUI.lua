db38df7364d5364dfe6516b2dcc2f72c2c2717f4
JustMe003 <kingoflol20033@gmail.com>
2022-09-19 21:25:06 +0200

    Create Client_PresentConfigureUI.lua

---
+++ Dreadfully Hypothermia 4/Client_PresentConfigureUI.lua
@@ -0,0 +1,5 @@
+function Client_PresentConfigureUI(rootParent)
+    if Mod.Settings.IsIncluded ~= nil then
+        UI.Alert("[!] WARNING [!]\n\nThis mod is meant for chaos. If you don't want chaos don't add this mod...");
+    end
+end
