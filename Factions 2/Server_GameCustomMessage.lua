7dedb571141c4d3183ef95038e6f22e0d5a844dd
JustMe003 <kingoflol20033@gmail.com>
2022-09-19 21:51:41 +0200

    Update Server_GameCustomMessage.lua

--- Factions 2/Server_GameCustomMessage.lua
+++ Factions 2/Server_GameCustomMessage.lua
@@ -353,6 +353,7 @@ function declareFactionWar(game, playerID, payload, setReturn)
 						if #data.PlayerInFaction[playerMember] > 1 and #data.PlayerInFaction[opponentMember] > 1 then
 							for i, f in pairs(data.PlayerInFaction[playerMember]) do
 								for j, f2 in pairs(data.PlayerInFaction[opponentMember]) do
+									print(f, f2);
 									if f == f2 and payload.PlayerInFaction ~= f and payload.OpponentFaction ~= f then
 										if data.Factions[f].FactionLeader ~= playerMember then
 											kickPlayer(game, playerID, {Faction=f, Index=i, Player=playerMember}, setReturn);
