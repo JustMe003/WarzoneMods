5d5c28717bedf6fd540df7665689ca8c9c873999
JustMe003 <kingoflol20033@gmail.com>
2022-10-07 18:21:59 +0200

    .

--- Factions 2/Client_SaveConfigureUI.lua
+++ Factions 2/Client_SaveConfigureUI.lua
@@ -6,7 +6,6 @@ function Client_SaveConfigureUI(alert)
 	globalSettings.FairFactions = getIsChecked(FairFactions);
 	globalSettings.ApproveFactionJoins = getIsChecked(ApproveFactionJoins);
 	globalSettings.LockPreSetFactions = getIsChecked(LockPreSetFactions);
-	globalSettings.PlaySpyOnFactionMembers = getIsChecked(PlaySpyOnFactionMembers);
 	if objectsID[FairFactionsModifier] ~= nil then
 		globalSettings.FairFactionsModifier = math.min(math.max(getValue(FairFactionsModifier), 0), 1);
 	else
