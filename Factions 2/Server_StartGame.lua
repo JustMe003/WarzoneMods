954258133a80095f93dd2c2ac8204d0812ce76e5
JustMe003 <kingoflol20033@gmail.com>
2022-10-07 18:37:32 +0200

    .

--- Factions 2/Server_StartGame.lua
+++ Factions 2/Server_StartGame.lua
@@ -22,7 +22,7 @@ function Server_StartGame(game, standing)
 		for _, p in pairs(game.ServerGame.Game.PlayingPlayers) do
 			count = count + p.Income(0, game.ServerGame.TurnZeroStanding, true, true).Total;
 			if not p.IsAI then
-				playerData[p.ID] = {};		-- Initialization notifications system
+				playerData[p.ID] = {};
 				playerData[p.ID].LastMessage = game.ServerGame.Game.ServerTime;
 				playerData[p.ID].Notifications = setPlayerNotifications();
 				playerData[p.ID].NumberOfNotifications = 0;
@@ -44,16 +44,13 @@ function Server_StartGame(game, standing)
 					relations[i][p.ID] = "InPeace";
 				end
 			end
-			isInFaction[p.ID] = Mod.Settings.Configuration.SlotInFaction[p.Slot] ~= nil;
-			playerInFaction[p.ID] = {};
+			isInFaction[p.ID] = Mod.Settings.Configuration.SlotInFaction[p.Slot] ~= nil
+			playerInFaction[p.ID] = Mod.Settings.Configuration.SlotInFaction[p.Slot];
 			if isInFaction[p.ID] then
-				playerInFaction[p.ID] = Mod.Settings.Configuration.SlotInFaction[p.Slot];
-				for _, faction in pairs(playerInFaction[p.ID]) do
-					table.insert(data.Factions[faction].FactionMembers, p.ID);
-					if data.Factions[faction].FactionLeader == p.Slot and not hasFactionLeader[faction] then
-						data.Factions[faction].FactionLeader = p.ID;
-						hasFactionLeader[faction] = true;
-					end
+				table.insert(data.Factions[playerInFaction[p.ID]].FactionMembers, p.ID);
+				if data.Factions[playerInFaction[p.ID]].FactionLeader == p.Slot and not hasFactionLeader[playerInFaction[p.ID]] then
+					data.Factions[playerInFaction[p.ID]].FactionLeader = p.ID;
+					hasFactionLeader[playerInFaction[p.ID]] = true;
 				end
 			end
 		end
@@ -71,7 +68,6 @@ function Server_StartGame(game, standing)
 			count = count + p.Income(0, game.ServerGame.TurnZeroStanding, true, true).Total;
 			relations[i] = {};
 			isInFaction[i] = false;
-			playerInFaction[p.ID] = {};
 			for k, _ in pairs(game.ServerGame.Game.PlayingPlayers) do
 				relations[i][k] = "InPeace";
 			end
@@ -85,12 +81,17 @@ function Server_StartGame(game, standing)
 			end
 		end
 	end
+	-- for i, v in pairs(relations) do
+		-- for k, j in pairs(v) do
+			-- print(i, k, j);
+		-- end
+	-- end
 	data.TotalIncomeOfAllPlayers = count;
 	data.Relations = relations;
 	data.IsInFaction = isInFaction;
 	data.PlayerInFaction = playerInFaction;
 	data.Events = {};
-	data.VersionNumber = 6;
+	data.VersionNumber = 5;
 	Mod.PlayerGameData = playerData;
 	Mod.PublicGameData = data;
 end
