4b358d1d31fa848c7dbd448db49f91e808a67fd6
JustMe003 <kingoflol20033@gmail.com>
2022-10-11 23:48:37 +0200

    Update Server_StartGame.lua

--- Dynamic Bonuses/Server_StartGame.lua
+++ Dynamic Bonuses/Server_StartGame.lua
@@ -2,7 +2,7 @@ function Server_StartGame(game, standing)
 	local data = Mod.PublicGameData;
 	local t = {};
 	for terrID, _ in pairs(standing.Territories) do
-		t[terrID] = 0;
+		t[terrID] = Mod.Settings.MinMultiplier;
 	end
 	data.WellBeingMultiplier = t;
 	Mod.PublicGameData = data;
