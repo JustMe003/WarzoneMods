6a388d3dad9a0f0a8e2988cfba81d9d7e4aea005
JustMe003 <kingoflol20033@gmail.com>
2022-10-12 00:41:52 +0200

    Update Client_PresentSettingsUI.lua

--- Dynamic Bonuses/Client_PresentSettingsUI.lua
+++ Dynamic Bonuses/Client_PresentSettingsUI.lua
@@ -1,4 +1,7 @@
 function Client_PresentSettingsUI(rootParent)
-	
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+    UI.CreateLabel(vert).SetText("Minimum multiplier: " .. Mod.Settings.MinMultiplier).SetColor("#8EBE57");
+    UI.CreateLabel(vert).SetText("Maximum multiplier: " .. Mod.Settings.MaxMultiplier).SetColor("#8EBE57");
+    UI.CreateLabel(vert).SetText("Multiplier increment each turn: " .. Mod.Settings.LevelMultiplierIncrement).SetColor("#8EBE57");
 end
 
