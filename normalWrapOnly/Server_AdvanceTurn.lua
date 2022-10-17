5855616924bedb9fee2a40cf68de6f5970083587
JustMe003 <kingoflol20033@gmail.com>
2022-10-17 15:51:35 +0200

    Update Server_AdvanceTurn.lua

--- normalWrapOnly/Server_AdvanceTurn.lua
+++ normalWrapOnly/Server_AdvanceTurn.lua
@@ -1,7 +1,7 @@
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
     if order.proxyType == "GameOrderAttackTransfer" then
         if game.Map.Territories[order.From].ConnectedTo[order.To] ~= WL.TerritoryConnectionWrap.Normal then
-            skipThisOrder(WL.ModOrderControl.SkipOrder);
+            skipThisOrder(WL.ModOrderControl.Skip);
         end
     end
 end
