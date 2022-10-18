2812a6374f0884492f513038add5ffca73d60e01
JustMe003 <kingoflol20033@gmail.com>
2022-10-18 16:26:49 +0200

    Update Server_GameCustomMessage.lua

--- normalWrapOnly/Server_GameCustomMessage.lua
+++ normalWrapOnly/Server_GameCustomMessage.lua
@@ -2,8 +2,5 @@ require("Dialog");
 function Server_GameCustomMessage(game, playerID, payload, setReturn)
     pd = Mod.PlayerGameData[playerID];
     getFunction(payload.Type)(game, playerID, payload, setReturn);
-    for i, v in pairs(pd) do
-        print(i, v);
-    end
     Mod.PlayerGameData[playerID] = pd;
 end
