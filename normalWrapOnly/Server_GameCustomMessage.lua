61567ab1d90071713f843e80c4094a79f878706f
JustMe003 <kingoflol20033@gmail.com>
2022-10-18 16:45:58 +0200

    Update Server_GameCustomMessage.lua

--- normalWrapOnly/Server_GameCustomMessage.lua
+++ normalWrapOnly/Server_GameCustomMessage.lua
@@ -7,7 +7,15 @@ function Server_GameCustomMessage(game, playerID, payload, setReturn)
         Mod.PlayerGameData[playerID] = pd;
         return;
     end
+
+    local functions = {showMessages=setPlayerNotifications()};
+
     pd = Mod.PlayerGameData[playerID];
-    getFunction(payload.Type)(game, playerID, payload, setReturn);
+    functions[payload.Type](game, playerID, payload, setReturn);
     Mod.PlayerGameData[playerID] = pd;
+end
+
+function setPlayerNotifications()
+    if pd == nil then pd = {}; end
+    pd.Notifications_JAD = payload.Value;
 end
