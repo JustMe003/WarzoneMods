8795f8510ea5eb83b7a67b708338bc90efed5caa
JustMe003 <kingoflol20033@gmail.com>
2022-10-18 16:39:19 +0200

    Update Client_GameRefresh.lua

--- normalWrapOnly/Client_GameRefresh.lua
+++ normalWrapOnly/Client_GameRefresh.lua
@@ -1,10 +1,10 @@
 require("Dialog");
 require("Client_PresentMenuUI")
 function Client_GameRefresh(game)
+    print(playerWantsNotifications());
     if not hasSeenIntroductionMessage() then
         showIntroductionDialog(game, ""); return;
     end
-    print(playerWantsNotifications());
     if playerWantsNotifications() then
         game.CreateDialog(Client_PresentMenuUI);
     end
