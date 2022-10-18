d5de8173c4f38c426dbaa87d483b55686b256acd
JustMe003 <kingoflol20033@gmail.com>
2022-10-18 16:19:13 +0200

    Update Client_PresentMenuUI.lua

--- normalWrapOnly/Client_PresentMenuUI.lua
+++ normalWrapOnly/Client_PresentMenuUI.lua
@@ -13,10 +13,15 @@ end
 function showOrderList()
     DestroyWindow();
     SetWindow("showOrderList");
+    local cnt = 0;
     for i, order in pairs(Game.Orders) do
         if order.proxyType == "GameOrderAttackTransfer"  and Game.Map.Territories[order.From].ConnectedTo[order.To].Wrap ~= WL.TerritoryConnectionWrap.Normal then
             CreateButton(vert).SetText("Move " .. order.NumArmies.NumArmies .. " from " .. order.From .. " to " .. order.To).SetColor(colors.OrangeRed).SetOnClick(function() showMoveDetails(order, i); end);
+            cnt = cnt + 1;
         end
+    end
+    if cnt == 0 then
+        CreateLabel(vert).SetText("All orders are valid (for this mod at least)!").SetColor(colors.Lime);
     end
 end
 
@@ -24,7 +29,7 @@ function showMoveDetails(order, i)
     DestroyWindow();
     SetWindow("ShowOrderDetails");
     CreateLabel(vert).SetText("This order will not be processed. Any move that makes your armies move from one side to the other side of the map are skipped").SetColor(colors.OrangeRed);
-    CreateButton(vert).SetText("Remove order").SetColor(colors.Lime).SetOnClick(function() removeOrder(i); end)
+    CreateButton(vert).SetText("Remove order").SetColor(colors.Lime).SetOnClick(function() removeOrder(i); showOrderList() end)
     CreateButton(vert).SetText("Return").SetColor(colors.Orange).SetOnClick(showOrderList);
 end
 
