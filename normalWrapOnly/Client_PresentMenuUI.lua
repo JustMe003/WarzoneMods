63a01b8554fc3d082d85115af2761381b0a23a2a
JustMe003 <kingoflol20033@gmail.com>
2022-10-25 11:17:31 +0200

    Update Client_PresentMenuUI.lua

--- normalWrapOnly/Client_PresentMenuUI.lua
+++ normalWrapOnly/Client_PresentMenuUI.lua
@@ -20,6 +20,18 @@ function showOrderList()
         if order.proxyType == "GameOrderAttackTransfer"  and Game.Map.Territories[order.From].ConnectedTo[order.To].Wrap ~= WL.TerritoryConnectionWrap.Normal then
             CreateButton(vert).SetText("Move " .. order.NumArmies.NumArmies .. " from " .. Game.Map.Territories[order.From].Name .. " to " .. Game.Map.Territories[order.To].Name).SetColor(colors.OrangeRed).SetOnClick(function() showMoveDetails(order, i); end);
             cnt = cnt + 1;
+        elseif order.proxyType == "GameOrderPlayCardBomb" then
+            local b = false;
+            for connID, conn in pairs(Game.Map.Territories[order.TargetTerritoryID].ConnectedTo) do
+                if conn.Wrap == WL.TerritoryConnectionWrap.Normal and Game.LatestStanding.Territories[connID].OwnerPlayerID == Game.Us.ID then
+                    b = true;
+                    break;
+                end
+            end
+            if not b then
+                CreateButton(vert).SetText("Bomb " .. Game.Map.Territories[order.TargetTerritoryID].Name).SetColor(colors.OrangeRed).SetOnClick(function() showBombDetails(order, i); end);
+                cnt = cnt + 1;
+            end
         end
     end
     if cnt == 0 then
@@ -27,6 +39,14 @@ function showOrderList()
     end
     CreateEmpty(vert).SetPreferredHeight(10);
     CreateButton(vert).SetText("Change notifications settings").SetColor(colors.Blue).SetOnClick(changeNotifications)
+end
+
+function showBombDetails(order, i)
+    DestroyWindow();
+    SetWindow("ShowBombDetails");
+    CreateLabel(vert).SetText("This order will not be processed. Any move that makes your armies move from one side to the other side of the map are skipped").SetColor(colors.OrangeRed);
+    CreateButton(vert).SetText("Remove order").SetColor(colors.Lime).SetOnClick(function() removeOrder(i); showOrderList(); end)
+    CreateButton(vert).SetText("Return").SetColor(colors.Orange).SetOnClick(showOrderList);
 end
 
 function showMoveDetails(order, i)
