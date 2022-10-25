5ae67e654259178e010b81402809ab716411553e
JustMe003 <kingoflol20033@gmail.com>
2022-10-25 11:05:49 +0200

    Update Server_AdvanceTurn.lua

--- normalWrapOnly/Server_AdvanceTurn.lua
+++ normalWrapOnly/Server_AdvanceTurn.lua
@@ -3,5 +3,16 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
         if game.Map.Territories[order.From].ConnectedTo[order.To].Wrap ~= WL.TerritoryConnectionWrap.Normal then
             skipThisOrder(WL.ModOrderControl.Skip);
         end
+    elseif order.proxyType == "GameOrderPlayCardBomb" then
+        local b = false;
+        for connID, wrap in pairs(game.Map.Territories[order.TargetTerritoryID].ConnectedTo) do
+            if wrap == WL.TerritoryConnectionWrap.Normal and game.ServerGame.LatestTurnStanding.Territories[connID].OwnerPlayerID == order.PlayerID then
+                b = true;
+                break;
+            end
+        end
+        if not b then
+            skipThisOrder(WL.ModOrderControl.Skip);
+        end
     end
 end
