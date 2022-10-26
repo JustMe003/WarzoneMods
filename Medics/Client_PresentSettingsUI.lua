8f0579672b250d33dd3e470808dfc5e4cf2c8258
JustMe003 <kingoflol20033@gmail.com>
2022-10-26 23:47:41 +0200

    Update Client_PresentSettingsUI.lua

--- Medics/Client_PresentSettingsUI.lua
+++ Medics/Client_PresentSettingsUI.lua
@@ -5,10 +5,10 @@ function Client_PresentSettingsUI(rootParent)
     local root = GetRoot();
     local colors = GetColors();
 
-    CreateLabel(root).SetText("The amount of gold to purchase a Medic: \t" .. Mod.Settings.Cost).SetColor(colors.Tan);
-    CreateLabel(root).SetText("The percentage of armies that are recovered by Medics: \t" .. Mod.Settings.Percentage).SetColor(colors.Tan);
-    CreateLabel(root).SetText("The amount of armies a Medic is worth (note that it cannot be damaged, only be killed in one attack): \t" .. Mod.Settings.Health).SetColor(colors.Tan);
-    CreateLabel(root).SetText("The maximum number of Medic units a player can have: \t" .. Mod.Settings.MaxUnits).SetColor(colors.Tan);
-
-
+    CreateLabel(root).SetText("The amount of gold to purchase a Medic: " .. Mod.Settings.Cost).SetColor(colors.Tan);
+    CreateLabel(root).SetText("The percentage of armies that are recovered by Medics: " .. Mod.Settings.Percentage).SetColor(colors.Tan);
+    CreateLabel(root).SetText("The amount of armies a Medic is worth: " .. Mod.Settings.Health).SetColor(colors.Tan);
+    CreateLabel(root).SetText("(note that it cannot be damaged, only be killed in one attack)").SetColor(colors.Orange);
+    CreateLabel(root).SetText("The maximum number of Medic units a player can have: " .. Mod.Settings.MaxUnits).SetColor(colors.Tan);
+    
 end
