f979c2e9c29236ca6b93adb46eb7e0e9f874dbcc
JustMe003 <kingoflol20033@gmail.com>
2022-10-29 17:20:20 +0200

    Update Client_PresentSettingsUI.lua

--- NoSplit Curse/Client_PresentSettingsUI.lua
+++ NoSplit Curse/Client_PresentSettingsUI.lua
@@ -6,6 +6,6 @@ function Client_PresentSettingsUI(rootParent)
     local colors = GetColors();
 
     CreateLabel(root).SetText("The amount of gold to purchase a No-split Curse: " .. Mod.Settings.Cost).SetColor(colors.Tan);
-    CreateLabel(root).SetText("The extra gold added for the next purchase: " .. Mod.Settings.MaxUnits).SetColor(colors.Tan);
+    CreateLabel(root).SetText("The extra gold added for the next purchase: " .. Mod.Settings.Increment).SetColor(colors.Tan);
     
 end
