5bf34ffc5eacc3ce642d3f30af21d79c60f21eeb
JustMe003 <kingoflol20033@gmail.com>
2022-10-31 18:56:55 +0100

    Update Client_PresentSettingsUI.lua

--- Capture The Flag/Client_PresentSettingsUI.lua
+++ Capture The Flag/Client_PresentSettingsUI.lua
@@ -1,4 +1,11 @@
-
+require("UI");
 function Client_PresentSettingsUI(rootParent)
-	
+    init(rootParent);
+
+    local vert = newVerticalGroup("vert", "root");
+
+    newLabel("1", vert, "The number of flags for each player / team: " .. Mod.Settings.FlagsPerTeam, "Tan");
+    newLabel("2", vert, "The number of flags each team / player has to lose to be eliminated: " .. Mod.Settings.NFlagsForLose, "Tan");
+    newLabel("2", vert, "The amount of income you get for controlling a captured flag each: " .. Mod.Settings.IncomeBoost, "Tan");
+    newLabel("2", vert, "The move cooldown for flags: " .. Mod.Settings.Cooldown .. " turns", "Tan");
 end
