6ce7e318b730cd3b4bac12c1cd9a4a9b13aca24c
JustMe003 <kingoflol20033@gmail.com>
2022-11-03 15:23:13 +0100

    Update Client_PresentMenuUI.lua

--- normalWrapOnly/Client_PresentMenuUI.lua
+++ normalWrapOnly/Client_PresentMenuUI.lua
@@ -17,9 +17,11 @@ function showOrderList()
     DestroyWindow();
     SetWindow("showOrderList");
     local cnt = 0;
+    CreateLabel(vert).SetText("Note: This is about your orders in turn " .. Game.Game.TurnNumber .. " only! Sometimes the turn has already advanced when this window is opened\n\nThe following orders are 'illegal'. that is, they will be skipped when your orders are processed. With this mod you cannot use connections that go around the map (top / bottom or left / right). Therefore attacks / transfers and will be skipped. Also bomb cards played using a connection that goes around the map will be skipped").SetColor(colors.Tan);
+    CreateButton(vert).SetText("Refresh").SetColor(colors.Orange).SetOnClick(function() Close(); Game.CreateDialog(Client_PresentMenuUI); end);
     for i, order in pairs(Game.Orders) do
         if order.proxyType == "GameOrderAttackTransfer"  and Game.Map.Territories[order.From].ConnectedTo[order.To].Wrap ~= WL.TerritoryConnectionWrap.Normal then
-            CreateButton(vert).SetText("Move " .. order.NumArmies.NumArmies .. " from " .. Game.Map.Territories[order.From].Name .. " to " .. Game.Map.Territories[order.To].Name).SetColor(colors.OrangeRed).SetOnClick(function() showMoveDetails(order, i); end);
+            CreateButton(vert).SetText("attack / transfer " .. order.NumArmies.NumArmies .. " armies from " .. Game.Map.Territories[order.From].Name .. " to " .. Game.Map.Territories[order.To].Name).SetColor(colors.OrangeRed).SetOnClick(function() showMoveDetails(order, i); end);
             cnt = cnt + 1;
         elseif order.proxyType == "GameOrderPlayCardBomb" then
             local b = false;
@@ -48,7 +50,7 @@ function showBombDetails(order, i)
     local line = CreateHorz(vert);
     CreateLabel(line).SetText("From: ").SetColor(colors.Ivory);
     CreateButton(line).SetText(Game.Map.Territories[order.TargetTerritoryID].Name).SetColor(colors.Orange).SetOnClick(function() if WL.IsVersionOrHigher("5.21") then Game.CreateLocatorCircle(Game.Map.Territories[order.TargetTerritoryID].MiddlePointX, Game.Map.Territories[order.TargetTerritoryID].MiddlePointY); Game.HighlightTerritories({order.TargetTerritoryID}); end end);
-    CreateLabel(vert).SetText("This order will not be processed. Any bomb card that is played at the other side of the map will be skipped").SetColor(colors.OrangeRed);
+    CreateLabel(vert).SetText("This order will be skipped. Any bomb card that is played at the other side of the map will be skipped").SetColor(colors.OrangeRed);
     CreateButton(vert).SetText("Remove order").SetColor(colors.Lime).SetOnClick(function() removeOrder(i); showOrderList(); end)
     CreateButton(vert).SetText("Return").SetColor(colors.Orange).SetOnClick(showOrderList);
 end
@@ -63,7 +65,7 @@ function showMoveDetails(order, i)
     line = CreateHorz(vert);
     CreateLabel(line).SetText("To: ").SetColor(colors.Ivory);
     CreateButton(line).SetText(Game.Map.Territories[order.To].Name).SetColor(colors.Orange).SetOnClick(function() if WL.IsVersionOrHigher("5.21") then Game.CreateLocatorCircle(Game.Map.Territories[order.To].MiddlePointX, Game.Map.Territories[order.To].MiddlePointY); Game.HighlightTerritories({order.To}) end end);
-    CreateLabel(vert).SetText("This order will not be processed. Any move that makes your armies move from one side to the other side of the map are skipped").SetColor(colors.OrangeRed);
+    CreateLabel(vert).SetText("This order will be skipped. Any move that makes your armies move from one side to the other side of the map are skipped").SetColor(colors.OrangeRed);
     CreateButton(vert).SetText("Remove order").SetColor(colors.Lime).SetOnClick(function() removeOrder(i); showOrderList() end)
     CreateButton(vert).SetText("Return").SetColor(colors.Orange).SetOnClick(showOrderList);
 end
