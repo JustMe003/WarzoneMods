bbacf54c38c8ede2fc912641d49a60f65aba2257
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 22:57:59 +0100

    Update Server_GameCustomMessage.lua

--- Factions 2/Server_GameCustomMessage.lua
+++ Factions 2/Server_GameCustomMessage.lua
@@ -1,5 +1,8 @@
-if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
-    require("Server_GameCustomMessage1");
-else
-    require("Server_GameCustomMessage2");
+function Server_GameCustomMessage(game, playerID, payload, setReturn)
+    if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
+        require("Server_GameCustomMessage1");
+    else
+        require("Server_GameCustomMessage2");
+    end
+    Server_GameCustomMessageMain(game, playerID, payload, setReturn);
 end
