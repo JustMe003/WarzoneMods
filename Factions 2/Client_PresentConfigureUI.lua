1bc3b8955e48c6d2e846aa78bd2022d11c5710c9
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 22:43:26 +0100

    Update Client_PresentConfigureUI.lua

--- Factions 2/Client_PresentConfigureUI.lua
+++ Factions 2/Client_PresentConfigureUI.lua
@@ -1,8 +1,22 @@
 function Client_PresentConfigureUI(rootParent)
-	if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
-		require("Client_PresentConfigureUI1");
+	if Mod.Settings.VersionNumber ~= nil then
+		if Mod.Settings.VersionNumber == 1 then
+			require("Client_PresentConfigureUI1");
+		else
+			require("Client_PresentConfigureUI2");
+		end
+		Client_PresentConfigureUIMain(rootParent);
 	else
-		require("Client_PresentConfigureUI2");
+		local t = {};
+		local vert = UI.CreateVerticalLayoutGroup(rootParent);
+		table.insert(t, UI.CreateLabel(vert).SetText("Pick which mod version you want to use"));
+		table.insert(t, UI.CreateButton(vert).SetText("Version: 1.5").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI1"); Version = 1; Client_PresentConfigureUIMain(vert); end));
+		table.insert(t, UI.CreateButton(vert).SetText("Version: 2.3").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI2"); Version = 2; Client_PresentConfigureUIMain(vert); end));
 	end
-	Client_PresentConfigureUIMain(rootParent);
+end
+
+function cleanUpWindow(t)
+	for _, o in pairs(t) do
+		UI.Destroy(o);
+	end
 end
