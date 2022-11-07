c3d00ef5a60ca5677aa46fae073da6c62f7a9c26
JustMe003 <kingoflol20033@gmail.com>
2022-11-08 00:10:31 +0100

    Update Client_PresentSettingsUI.lua

--- Factions 2/Client_PresentSettingsUI.lua
+++ Factions 2/Client_PresentSettingsUI.lua
@@ -1,8 +1,12 @@
 function Client_PresentSettingsUI(rootParent)
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	local line = UI.CreateHorizontalLayoutGroup(vert);
     if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
 		require("Client_PresentSettingsUI1");
+		UI.CreateLabel(line).SetText("Version used: 1.5");
 	else
 		require("Client_PresentSettingsUI2");
+		UI.CreateLabel(line).SetText("Version used: 2.3");
 	end
-    Client_PresentSettingsUIMain(rootParent);
+    Client_PresentSettingsUIMain(vert);
 end
