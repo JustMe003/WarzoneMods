1c386d99cd3a39da2b005881055ca70a892fc4ea
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 22:43:29 +0100

    Update Client_SaveConfigureUI.lua

--- Factions 2/Client_SaveConfigureUI.lua
+++ Factions 2/Client_SaveConfigureUI.lua
@@ -1,4 +1,12 @@
 function Client_SaveConfigureUI(alert)
-    -- code
+    if Version == nil then
+        alert("You must pick a mod version for the Factions mod!");
+        return;
+    end
+    if Version == 1 then
+        require("Client_SaveConfigureUI1");
+    else
+        require("Client_SaveConfigureUI2");
+    end
     Client_SaveConfigureUIMain(alert);
 end
