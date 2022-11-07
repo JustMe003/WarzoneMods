049b0dc6580c65fe328db736d63df31dcc160c97
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 22:56:52 +0100

    Update Server_AdvanceTurn.lua

--- Factions 2/Server_AdvanceTurn.lua
+++ Factions 2/Server_AdvanceTurn.lua
@@ -1,5 +1,8 @@
-if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
-    require("Server_AdvanceTurn1");
-else
-    require("Server_AdvanceTurn2");
+function Server_AdvanceTurn_Start(game, addNewOrder)
+    if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
+        require("Server_AdvanceTurn1");
+    else
+        require("Server_AdvanceTurn2");
+    end
+    Server_AdvanceTurn_StartMain(game, addNewOrder);
 end
