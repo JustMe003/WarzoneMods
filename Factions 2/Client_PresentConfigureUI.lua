ea6cc11faef607ce504284a53b284934d7fce820
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 23:31:43 +0100

    Update Client_PresentConfigureUI.lua

--- Factions 2/Client_PresentConfigureUI.lua
+++ Factions 2/Client_PresentConfigureUI.lua
@@ -8,16 +8,40 @@ function Client_PresentConfigureUI(rootParent)
 		end
 		Client_PresentConfigureUIMain(rootParent);
 	else
-		local t = {};
 		local vert = UI.CreateVerticalLayoutGroup(rootParent);
-		table.insert(t, UI.CreateLabel(vert).SetText("Pick which mod version you want to use"));
-		table.insert(t, UI.CreateButton(vert).SetText("Version: 1.5").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI1"); Version = 1; Client_PresentConfigureUIMain(vert); end));
-		table.insert(t, UI.CreateButton(vert).SetText("Version: 2.3").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI2"); Version = 2; Client_PresentConfigureUIMain(vert); end));
+		showChoices(vert);
 	end
+end
+
+function showChoices(vert)
+	local t = {};
+	table.insert(t, UI.CreateLabel(vert).SetText("Pick which mod version you want to use").SetColor("#FF4700"));
+	table.insert(t, UI.CreateButton(vert).SetText("Version: 1.5").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI1"); Version = 1; Client_PresentConfigureUIMain(vert); end));
+	table.insert(t, UI.CreateButton(vert).SetText("Version: 2.3").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); require("Client_PresentConfigureUI2"); Version = 2; Client_PresentConfigureUIMain(vert); end));
+	table.insert(t, UI.CreateButton(vert).SetText("What's the difference?").SetColor("#FF4700").SetOnClick(function() cleanUpWindow(t); showDifference(vert); end))
 end
 
 function cleanUpWindow(t)
 	for _, o in pairs(t) do
 		UI.Destroy(o);
 	end
+end
+
+function showDifference(vert)
+	local t = {};
+	table.insert(t, UI.CreateButton(vert).SetText("Return").SetColor("#FF7D00").SetOnClick(function() cleanUpWindow(t); showChoices(vert); end));
+	table.insert(t, UI.CreateButton(vert).SetText("Main difference").SetColor("#0000FF").SetOnClick(function() cleanUpWindow(t); showMainDifference(vert); end))
+	table.insert(t, UI.CreateButton(vert).SetText("Full difference list").SetColor("#FF4700").SetOnClick(function() cleanUpWindow(t); showFullDifferenceList(vert); end))
+end
+
+function showMainDifference(vert)
+	local t = {};
+	table.insert(t, UI.CreateButton(vert).SetText("Return").SetColor("#FF7D00").SetOnClick(function() cleanUpWindow(t); showDifference(vert); end));
+	table.insert(t, UI.CreateLabel(vert).SetText("The main difference is a change in a core mechanic, which is the reason why I cannot make it simply a setting.\n\nPrior to Factions 2.0, players were only able to join 1 factions. But, as you can guess, nowadays players can join multiple Factions by using any version above 2.0. This comes with some extra forced rules by the mod to ensure the mod still has control over the diplomacy of the game.\n\nAll you and other players need to know, is that Factions 2.0 (and higher) is the newest version which is actively built on. You can read everything about the forced diplomacy rules in the mod configuration, mod menu or in the mod settings."))
+end
+
+function showFullDifferenceList(vert)
+	local t = {};
+	table.insert(t, UI.CreateButton(vert).SetText("Return").SetColor("#FF7D00").SetOnClick(function() cleanUpWindow(t); showDifference(vert); end));
+	table.insert(t, UI.CreateLabel(vert).SetText("Full changelog (from 1.6 --> 2.0 and higher)\n\n- Players can join multiple Factions instead of the usual 1\n- Diplomacy cards are now played at the end of the turn instead of the start (with some exceptions)\n- Added a new file which contains all the forced diplomacy rules and their explanations\n- Added a button in the mod configuration to read the forced diplomacy rules\n- Added a button in the mod settings to read the forced diplomacy rules\n- Added a button in the mod menu to read the forced diplomacy rules\n- Added a new setting to allow configuration of the normally standard 'playing spy cards on allies'\n- Players can now have pending join request for multiple Factions\n- Players can now read the Faction chat of every Faction they're in\n- Added a new pop up message for when a player opens their game for the first time, to tell them something about the new Factions version\n- Faction names must now be between 2 and 50 characters long\n- With the setting 'Visible History' set to true, players can see the Faction relation of any Faction\n- Modified the text of A LOT of messages..."))
 end
