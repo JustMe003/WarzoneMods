92a454db0dfe1f0f22e18e65365bab17ce5d8a92
JustMe003 <kingoflol20033@gmail.com>
2022-11-07 23:39:39 +0100

    Update Client_PresentConfigureUI2.lua

--- Factions 2/Client_PresentConfigureUI2.lua
+++ Factions 2/Client_PresentConfigureUI2.lua
@@ -39,7 +39,7 @@ function showMain()
 	newButton(win .. "Settings", line, "Settings", showSettings, "Aqua");
 	newButton(win .. "Config", line, "Configuration", showMainConfig, "Lime");
 	newButton(win .. "ForcedRules", line, "Forced rules", function() forcedRulesInit(function() showMain(); end) end, "Royal Blue");
-	newButton(win .. "ChangeVersion", line, "Change version", function() UI.Alert("Unfortunately, you need to remove the mod from your game, and re-open the mod settings page.\n\n[!]Warning[!]\nThis will reset the entire configuration"); end, "Orange Red");
+	newButton(win .. "ChangeVersion", line, "Change version", function() UI.Alert("Unfortunately, to do this you need to remove the mod from your game, and re-open the mod settings page.\n\n[!]Warning[!]\nThis will reset the entire configuration"); end, "Orange Red");
 end
 
 function showSettings()
