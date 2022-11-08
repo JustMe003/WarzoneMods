c5834ab27cfe3c9dc5f3dca4ccd3cb9ba0a50dec
JustMe003 <kingoflol20033@gmail.com>
2022-11-08 14:15:38 +0100

    Update Server_StartGame.lua

--- Factions 2/Server_StartGame.lua
+++ Factions 2/Server_StartGame.lua
@@ -1,5 +1,8 @@
-if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
-    require("Server_StartGame1");
-else 
-    require("Server_StartGame2");
+function Server_StartGame(game, standing)
+    if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
+        require("Server_StartGame1");
+    else 
+        require("Server_StartGame2");
+    end
+    Server_StartGameMain(game, standing);
 end
