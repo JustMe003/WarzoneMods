e6034fc5db3b7c0a4f6c57b43cf252e6050f586b
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-11-12 10:14:18 +0100

    Update Client_PresentMenuUI.lua

--- Testing/Client_PresentMenuUI.lua
+++ Testing/Client_PresentMenuUI.lua
@@ -1,11 +1,12 @@
 -- code
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close)
+    game = Game;
     Game.GetTurn(Game.Game.TurnNumber - 2, callback);
 end
 
 function callback(t)
-    for i, v in pairs (WL.TurnPhase) do
-        print(i, v);
+    for i, v in pairs (game.Orders) do
+        print(i, v.OccursInPhase);
     end
     for i, v in pairs(t.Orders) do
         if v.proxyType == "GameOrderEvent" then
