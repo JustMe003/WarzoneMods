bfce8eaf705d1f724ce10ebd4014497f773875b6
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-11-12 08:51:38 +0100

    Update Client_PresentMenuUI.lua

--- Testing/Client_PresentMenuUI.lua
+++ Testing/Client_PresentMenuUI.lua
@@ -1,1 +1,10 @@
 -- code
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close)
+    Game.GetTurn(Game.Game.TurnNumber - 1, callback);
+end
+
+function callback(t)
+    for i, v in pairs(t) do
+        print(i, v);
+    end
+end
