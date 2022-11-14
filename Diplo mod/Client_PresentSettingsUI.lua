3cdaf0cd4ed899d3ed0a9f0d62147400eab93164
JustMe003 <kingoflol20033@gmail.com>
2022-11-14 21:54:15 +0100

    Update Client_PresentSettingsUI.lua

--- Diplo mod/Client_PresentSettingsUI.lua
+++ Diplo mod/Client_PresentSettingsUI.lua
@@ -3,7 +3,7 @@ function Client_PresentSettingsUI(rootParent)
 	local line = UI.CreateHorizontalLayoutGroup(vert);
     if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
 		require("Client_PresentSettingsUI1");
-		UI.CreateLabel(line).SetText("Version used: 1.5");
+		UI.CreateLabel(line).SetText("Version used: 1.5.2");
 	else
 		require("Client_PresentSettingsUI2");
 		UI.CreateLabel(line).SetText("Version used: 2.3");
