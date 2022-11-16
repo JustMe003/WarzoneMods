605bcc5b50448c72790aff97ef9dfe6fc2ca3bfb
JustMe003 <kingoflol20033@gmail.com>
2022-11-16 22:41:17 +0100

    Update utilities2.lua

--- Diplo mod/utilities2.lua
+++ Diplo mod/utilities2.lua
@@ -144,3 +144,9 @@ function isFactionLeader(p)
 	end
 	return false;
 end
+
+function round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+	return math.floor(num * mult + 0.5) / mult
+  end
+  
