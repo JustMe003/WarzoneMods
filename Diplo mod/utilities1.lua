4cab63bd2c14b15375293953f5ac9f210e84878d
JustMe003 <kingoflol20033@gmail.com>
2022-11-16 22:41:08 +0100

    Update utilities1.lua

--- Diplo mod/utilities1.lua
+++ Diplo mod/utilities1.lua
@@ -115,3 +115,9 @@ function createEvent(m, p, h);
 	end
 	return t;
 end
+
+function round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+	return math.floor(num * mult + 0.5) / mult
+  end
+  
