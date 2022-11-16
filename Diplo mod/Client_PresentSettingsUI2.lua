c03a6f648ffb5b9dacf379833fb29c2b0fb40dda
JustMe003 <kingoflol20033@gmail.com>
2022-11-16 22:44:04 +0100

    Update Client_PresentSettingsUI2.lua

--- Diplo mod/Client_PresentSettingsUI2.lua
+++ Diplo mod/Client_PresentSettingsUI2.lua
@@ -46,7 +46,7 @@ function showMain()
 	end
 	if Mod.Settings.GlobalSettings.FairFactions then
 		line = getLine(vert);
-		newLabel(win .. line, line, "Fair Factions modifier: " .. round(Mod.Settings.GlobalSettings.FairFactionsModifier));
+		newLabel(win .. line, line, "Fair Factions modifier: " .. round(Mod.Settings.GlobalSettings.FairFactionsModifier, 2));
 		newButton(win .. line .. "button", line, "?", function() UI.Alert("This number indicates how much total income a Faction might have compared to the total income of every player in the game. This can prevent the biggest players from joining a Faction and steamroll the rest of the game"); end, "Blue");
 	end
 end
