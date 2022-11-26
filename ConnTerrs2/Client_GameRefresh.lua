73dd8c96e77820e43f67feb7cd593520f0ec31a8
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-11-26 09:24:33 +0100

    Update Client_GameRefresh.lua

--- ConnTerrs2/Client_GameRefresh.lua
+++ ConnTerrs2/Client_GameRefresh.lua
@@ -1,6 +1,6 @@
 function Client_GameRefresh(game)
 	if game.Us == nil then return; end
-    if game.Game.TurnNumber == 1 and Mod.PlayerGameData.HasSeenMessage == nil then
+    if game.Game.TurnNumber == 1 and Mod.PlayerGameData.HasSeenMessage == nil and not Mod.Settings.AutoDistributeUnits then
         require("Client_PresentMenuUI");
         game.CreateDialog(Client_PresentMenuUI);
         game.SendGameCustomMessage("Updating mod...", {}, function(t) end);
