990ab099de6d5a659d587801896a7a308d0ed652
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2022-11-26 09:43:03 +0100

    Update Client_PresentMenuUI.lua

--- ConnTerrs2/Client_PresentMenuUI.lua
+++ ConnTerrs2/Client_PresentMenuUI.lua
@@ -23,7 +23,7 @@ function showMenu()
 			local t = split(order.Payload, "_");
 			if t[1] == "ConnTerrs2" then
 				count = count + 1;
-				CreateButton(GetRoot()).SetText(game.Map.Territories[t[2]].Name).SetColor(colors.Lime).SetOnClick(function() removeOrder(t[2]) DestroyWindow(); SetWindow("ShowInfo"); CreateLabel(GetRoot()).SetText("Click one of your territories to create the order. You can move this dialog out of the way if you need to"); UI.InterceptNextTerritoryClick(validateClick); end);
+				CreateButton(GetRoot()).SetText(game.Map.Territories[t[2]].Name).SetColor(colors.Lime).SetOnClick(function() Order = order; removeOrder(t[2]) DestroyWindow(); SetWindow("ShowInfo"); CreateLabel(GetRoot()).SetText("Click one of your territories to create the order. You can move this dialog out of the way if you need to"); UI.InterceptNextTerritoryClick(validateClick); end);
 			end
 		end
 	end
@@ -35,6 +35,15 @@ end
 
 function validateClick(terrDetails)
 	if terrDetails == nil then return; end
+	if game.LatestStanding.Territories[terrDetails.ID].OwnerPlayeraID ~= game.Us.ID then
+		UI.Alert("you must pick a territory you control");
+		if Order ~= nil then
+			local orders = game.Orders;
+			table.insert(orders, Order);
+			game.Orders = orders;
+		end
+		return;
+	end
 	local orders = game.Orders;
 	table.insert(orders, WL.GameOrderCustom.Create(game.Us.ID, "Place link unit on " .. terrDetails.Name, "ConnTerrs2_" .. terrDetails.ID));
 	game.Orders = orders;
