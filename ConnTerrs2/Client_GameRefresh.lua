00e092e18f21c642b00b994419fbdad02470673c
JustMe003 <kingoflol20033@gmail.com>
2022-11-28 07:43:09 +0100

    Update Client_GameRefresh.lua

--- ConnTerrs2/Client_GameRefresh.lua
+++ ConnTerrs2/Client_GameRefresh.lua
@@ -1,7 +1,8 @@
 function Client_GameRefresh(game)
 	if game.Us == nil then return; end
     if game.Game.TurnNumber == 1 and Mod.PlayerGameData.HasSeenMessage == nil and not Mod.Settings.AutoDistributeUnits then
-        game.CreateDialog(function(rootParent, setMaxSize, setScrollable, Game, close) setMaxSize(100, 160); UI.CreateButton(UI.CreateVerticalLayoutGroup(rootParent)).SetText("Open Dialog").SetColor("#59009D").SetOnClick(function() require("Client_PresentMenuUI"); close(); game.CreateDialog(Client_PresentMenuUI); end); end)
+        require("Client_PresentMenuUI");
+        game.CreateDialog(Client_PresentMenuUI);
         game.SendGameCustomMessage("Updating mod...", {}, function(t) end);
     end
 end
