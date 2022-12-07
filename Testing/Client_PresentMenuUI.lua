9e01fc06c7208c232c5d26b81d912815ebbd5692
JustMe003 <kingoflol20033@gmail.com>
2022-12-08 00:33:27 +0100

    Update Client_PresentMenuUI.lua

--- Testing/Client_PresentMenuUI.lua
+++ Testing/Client_PresentMenuUI.lua
@@ -2,6 +2,14 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close)
     game = Game;
     Game.GetTurn(Game.Game.TurnNumber - 2, callback);
+
+    if Mod.PublicGameData.PendingStateTransitions ~= nil then
+        for i, v in pairs(Mod.PublicGameData.PendingStateTransitions) do
+            for k, v2 in pairs(v) do
+                print(i, k, v2);
+            end
+        end
+    end
 end
 
 function callback(t)
