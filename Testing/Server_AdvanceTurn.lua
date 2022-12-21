f2eb2afbc87f1151042be4f7287e29c55c33d018
JustMe003 <kingoflol20033@gmail.com>
2022-12-21 15:01:31 +0100

    remade Testing (new purpose)

--- Testing/Server_AdvanceTurn.lua
+++ Testing/Server_AdvanceTurn.lua
@@ -1,7 +1,5 @@
 function Server_AdvanceTurn_Start(game, addNewOrder)
-	local d = Mod.PublicGameData;
-    d.PendingStateTransitions = game.ServerGame.PendingStateTransitions;
-    Mod.PublicGameData = d;
+	playCards(game, addNewOrder);
 end
 
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
@@ -9,5 +7,35 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 end
 
 function Server_AdvanceTurn_End(game, addNewOrder)
-	
+	playCards(game, addNewOrder);
+end
+
+function playCards(game, addNewOrder)
+    local data = Mod.PublicGameData;
+    if data.NonPlayingPlayers == nil then data.NonPlayingPlayers = {}; end
+    for _, p in pairs(game.Game.Players) do
+        if playerIsOut(p.State) and not valueInTable(data.NonPlayingPlayers, p.ID) then
+            for p2, _ in pairs(game.Game.PlayingPlayers) do
+                local instance = WL.NoParameterCardInstance.Create(WL.CardID.Spy);
+                addNewOrder(WL.GameOrderReceiveCard.Create(p.ID, {instance}));
+                addNewOrder(WL.GameOrderPlayCardSpy.Create(instance.ID, p.ID, p2));
+            end
+            local instance = WL.NoParameterCardInstance.Create(WL.CardID.Spy);
+            addNewOrder(WL.GameOrderReceiveCard.Create(p.ID, {instance}));
+            addNewOrder(WL.GameOrderPlayCardSpy.Create(instance.ID, p.ID, WL.PlayerID.Neutral));
+            table.insert(data.NonPlayingPlayers, p.ID);
+        end
+    end
+    Mod.PublicGameData = data;
+end
+
+function playerIsOut(state)
+    return state == WL.GamePlayerState.Eliminated or state == WL.GamePlayerState.Booted or state == WL.GamePlayerState.SurrenderAccepted;
+end
+
+function valueInTable(t, v)
+    for _, v2 in pairs(t) do
+        if v == v2 then return true; end
+    end
+    return false;
 end
