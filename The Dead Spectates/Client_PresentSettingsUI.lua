bc619a42e6223c60e465a6683f373723d49e0f75
JustMe003 <kingoflol20033@gmail.com>
2022-12-21 15:27:49 +0100

    Update Client_PresentSettingsUI.lua

--- The Dead Spectates/Client_PresentSettingsUI.lua
+++ The Dead Spectates/Client_PresentSettingsUI.lua
@@ -1,4 +1,9 @@
 function Client_PresentSettingsUI(rootParent)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-    UI.CreateLabel(vert).SetText("Players who got booted, eliminated or surrendered will also spectate the game: " .. tostring(Mod.Settings.IncludeInGamePlayers)).SetColor("#DDDDDD");
+	local line = UI.CreateHorizontalLayoutGroup(rootParent);
+    UI.CreateLabel(line).SetText("Players who got booted, eliminated or surrendered will also spectate the game: ").SetColor("#DDDDDD");
+    if Mod.Settings.IncludeInGamePlayers == nil or Mod.Settings.IncludeInGamePlayers then
+        UI.CreateLabel(line).SetText("True").SetColor("#00FF00");
+    else
+        UI.CreateLabel(line).SetText("False").SetColor("#FF5349");
+    end
 end
