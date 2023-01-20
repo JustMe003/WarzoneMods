2eb674a4974fb1d0dce5344c5410e3312b6d8af4
JustMe003 <kingoflol20033@gmail.com>
2023-01-20 12:07:02 +0100

    Update Server_StartGame.lua

--- Dragons/Server_StartGame.lua
+++ Dragons/Server_StartGame.lua
@@ -2,7 +2,8 @@ function Server_StartGame(game, standing)
 	local s = standing;
     for _, terr in pairs(s) do
         if terr.OwnerPlayerID ~= WL.PlayerID.Neutral then
-            local armies = terr.NumArmies
+            local armies = terr.NumArmies;
+            print(armies.proxyType);
             table.insert(armies.SpecialUnits, getDragon(terr.OwnerPlayerID));
             terr.NumArmies = WL.Armies.Create(armies.NumArmies, armies.SpecialUnits);
         end
