ed0e146ff41d123704841d4364cd52e4da0685ee
JustMe003 <kingoflol20033@gmail.com>
2023-01-20 11:20:16 +0100

    added Dragons mod

---
+++ Dragons/Server_AdvanceTurn.lua
@@ -0,0 +1,35 @@
+function Server_AdvanceTurn_Start(game, addNewOrder)
+	
+end
+
+function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
+	if order.proxyType == "GameOrderAttackTransfer" and orderResult.IsAttack then
+        if #orderResult.ActualArmies.SpecialUnits > 0 then
+            local dragonCount = 0;
+            for _, sp in pairs(orderResult.ActualArmies.SpecialUnits) do
+                if sp.proxyType == "CustomSpecialUnit" then
+                    if sp.Name == "Dragon" then
+                        dragonCount = dragonCount + 1;
+                    end
+                end
+            end
+            if dragonCount > 0 then
+                local mods = {};
+                for connID, _ in pairs(game.Map.Territories[order.To].ConnectedTo) do
+                    if game.ServerGame.LatestStanding.Territories[connID].OwnerPlayerID ~= order.PlayerID then
+                        local mod = WL.TerritoryModification.Create(connID);
+                        mod.AddArmies = -math.min(game.ServerGame.LatestStanding.Territories[connID].NumArmies.NumArmies, dragonCount * 5);
+                        table.insert(mods, mod);
+                    end
+                end
+                local event = WL.GameOrderEvent.Create(order.PlayerID, "Dragon breath", {}, mods);
+                event.JumpToActionSpotOpt = WL.RectangleVM.Create(game.Map.Territories[order.To].MiddlePointX, game.Map.Territories[order.To].MiddlePointY, game.Map.Territories[order.To].MiddlePointX, game.Map.Territories[order.To].MiddlePointY)
+                addNewOrder(event, true);
+            end
+        end
+    end
+end
+
+function Server_AdvanceTurn_End(game, addNewOrder)
+	
+end
---
+++ Dragons/Server_StartGame.lua
@@ -0,0 +1,29 @@
+function Server_StartGame(game, standing)
+	local s = standing;
+    for _, terr in pairs(s) do
+        if terr.OwnerPlayerID ~= WL.PlayerID.Neutral then
+            local armies = terr.NumArmies
+            table.insert(armies.SpecialUnits, getDragon(terr.OwnerPlayerID));
+            terr.NumArmies = WL.Armies.Create(armies.NumArmies, armies.SpecialUnits);
+        end
+    end
+    standing = s;
+end
+
+function getDragon(p)
+    local builder = WL.CustomSpecialUnitBuilder.Create(order.PlayerID);
+    builder.Name = "Dragon";
+    builder.TextOverHeadOpt = "Dragon";
+    builder.IncludeABeforeName = true;
+    builder.ImageFilename = 'dragon.png';
+    builder.AttackPower = 20;
+    builder.DefensePower = 20;
+    builder.Health = 20;
+    builder.CombatOrder = 1362;
+    builder.CanBeGiftedWithGiftCard = true;
+    builder.CanBeTransferredToTeammate = true;
+    builder.CanBeAirliftedToSelf = true;
+    builder.CanBeAirliftedToTeammate = true;
+    builder.IsVisibleToAllPlayers = false;
+    return builder.Build();
+end
