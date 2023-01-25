a0c7d93caff79ef9be41feb4f2e5291e56738fbf
JustMe003 <kingoflol20033@gmail.com>
2023-01-25 13:13:22 +0100

    Update Server_AdvanceTurn.lua

--- Landmines/Server_AdvanceTurn.lua
+++ Landmines/Server_AdvanceTurn.lua
@@ -23,8 +23,9 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
             local mod = WL.TerritoryModification.Create(terrID);
             mod.AddSpecialUnits = {builder.Build()};
 
-            local event = WL.GameOrderEvent.Create(order.PlayerID, "Purchased a Landmine", {}, {mod});
+            local event = WL.GameOrderEvent.Create(order.PlayerID, "Purchased a Landmine", {}, {mod}, {});
             event.JumpToActionSpotOpt = WL.RectangleVM.Create(game.Map.Territories[terrID].MiddlePointX, game.Map.Territories[terrID].MiddlePointY, game.Map.Territories[terrID].MiddlePointX, game.Map.Territories[terrID].MiddlePointY);
+            event.AddResourceOpt = {[WL.ResourceType.Gold] = order.CostOpt[WL.ResourceType.Gold]};
             addNewOrder(event);
             data.LandminesBought[order.PlayerID] = data.LandminesBought[order.PlayerID] + 1;
         end
