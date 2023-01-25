973a064cde169db43d3810631745efc360f57799
JustMe003 <kingoflol20033@gmail.com>
2023-01-25 12:51:48 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- Landmines/Client_PresentCommercePurchaseUI.lua
+++ Landmines/Client_PresentCommercePurchaseUI.lua
@@ -49,7 +49,7 @@ function purchaseLandmine()
     local orders = Game.Orders;
     local index = 0;
     for i, order in pairs(orders) do
-        print(order.OccursInPhase);
+        print(i, order.OccursInPhase);
         if order.OccursInPhase ~= nil and order.OccursInPhase < WL.TurnPhase.Deploys + 1 then
             local index = i;
             break;
