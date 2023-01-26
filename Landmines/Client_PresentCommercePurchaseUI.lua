b23bb5b00c4113a528488b51ce0243165cb9ce3a
JustMe003 <kingoflol20033@gmail.com>
2023-01-26 13:35:08 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- Landmines/Client_PresentCommercePurchaseUI.lua
+++ Landmines/Client_PresentCommercePurchaseUI.lua
@@ -16,14 +16,13 @@ end
 
 function pickTerr(rootParent, setMaxSize, setScrollable, game, close)
     ClosePurchase();
-    if not(UI.IsDestroyed(prevRoot)) and Close ~= nil then
+    if not(UI.IsDestroyed(root)) and Close ~= nil then
         Close();
     end
     Close = close;
     Game = game;
 
     Init(rootParent);
-    prevRoot = rootParent;
     root = GetRoot().SetFlexibleWidth(1);
 
     selected = CreateButton(root).SetText("Pick territory").SetColor(colors.Orange).SetOnClick(selectTerr);
