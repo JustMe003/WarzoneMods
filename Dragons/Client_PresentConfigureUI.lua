581a306110366a5f2b059581faa3097e5a52a20b
JustMe003 <kingoflol20033@gmail.com>
2023-01-26 17:18:37 +0100

    Update Client_PresentConfigureUI.lua

--- Dragons/Client_PresentConfigureUI.lua
+++ Dragons/Client_PresentConfigureUI.lua
@@ -5,7 +5,7 @@ function Client_PresentConfigureUI(rootParent)
     dragons = Mod.Settings.Dragons
     if dragons == nil then dragons = {}; end
 
-    root = GetRoot().SetFlexibleWidth(1).SetPreferredHeight(700);
+    root = GetRoot().SetFlexibleWidth(1).SetPreferredHeight(500);
     colors = GetColors();
     showMain();
 end
@@ -14,6 +14,8 @@ function showMain()
     DestroyWindow(GetCurrentWindow(), true);
     SetWindow("Main");
 
+    root.SetPreferredHeight(500);
+    
     for _, dragon in pairs(dragons) do
         CreateButton(root).SetText(dragon.Name).SetColor(dragon.Color).SetOnClick(function() modifyDragon(dragon); end);
     end
@@ -21,13 +23,15 @@ function showMain()
     CreateEmpty(root).SetPreferredHeight(10);
     
     CreateButton(root).SetText("Add Dragon").SetColor(colors.Lime).SetOnClick(function() table.insert(dragons, initDragon()); modifyDragon(dragons[#dragons]) end).SetInteractable(#dragons < 5);
-
+    
     CreateEmpty(root).SetFlexibleHeight(1);
 end
 
 function modifyDragon(dragon)
     DestroyWindow(GetCurrentWindow(), true);
     SetWindow("modifyDragon");
+    
+    root.SetPreferredHeight(-1);
     
     dragonInputs = {};
     currentDragon = dragon.ID;
@@ -40,7 +44,7 @@ function modifyDragon(dragon)
     CreateLabel(line).SetText("General").SetColor(colors.Tan);
     CreateEmpty(line).SetFlexibleWidth(0.5);
     CreateEmpty(root).SetPreferredHeight(5);
-
+    
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateLabel(line).SetText("Dragon name: ").SetColor(colors.Textcolor);
     dragonInputs.Name = CreateTextInputField(line).SetText(dragon.Name).SetFlexibleWidth(1);
@@ -59,7 +63,7 @@ function modifyDragon(dragon)
     line = CreateHorz(root).SetFlexibleWidth(1);
     dragonInputs.UseHealth = CreateCheckBox(line).SetText(" ").SetIsChecked(dragon.UseHealth);
     CreateLabel(line).SetText("Use dynamic health").SetColor(colors.Textcolor);
-
+    
     local vert = CreateVert(root);
     
     CreateEmpty(root).SetPreferredHeight(10);
@@ -68,7 +72,7 @@ function modifyDragon(dragon)
     CreateLabel(line).SetText("Permissions").SetColor(colors.Tan);
     CreateEmpty(line).SetFlexibleWidth(0.5);
     CreateEmpty(root).SetPreferredHeight(5);
-
+    
     line = CreateHorz(root).SetFlexibleWidth(1);
     dragonInputs.DragonBreathAttack = CreateCheckBox(line).SetText(" ").SetIsChecked(dragon.DragonBreathAttack);
     CreateLabel(line).SetText("Enable Dragon Breath Attack").SetColor(colors.Textcolor);
@@ -88,10 +92,10 @@ function modifyDragon(dragon)
     line = CreateHorz(root).SetFlexibleWidth(1);
     dragonInputs.IncludeABeforeName = CreateCheckBox(line).SetText(" ").SetIsChecked(dragon.IncludeABeforeName);
     CreateLabel(line).SetText("automatically put the word 'A' before the name of this dragon").SetColor(colors.Textcolor);
-
+    
     dragonInputs.UseHealth.SetOnValueChanged(function() saveDragon(dragon, dragonInputs); healthAndDamage(dragon, vert, dragonInputs) end);
     healthAndDamage(dragon, vert, dragonInputs);
-
+    
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateEmpty(line).SetFlexibleWidth(0.45);
     CreateButton(line).SetText("Save and Return").SetColor(colors.Green).SetOnClick(function() saveDragon(dragon, dragonInputs); showMain(); end);
@@ -106,11 +110,11 @@ function healthAndDamage(dragon, vert, inputs)
     AddSubWindow(parent, win);
     DestroyWindow(win, false);
     SetWindow(win);
-
+    
     if dragon.UseHealth then
         CreateLabel(vert).SetText("The initial health of this dragon").SetColor(colors.Textcolor);
         inputs.Health = CreateNumberInputField(vert).SetSliderMinValue(1).SetSliderMaxValue(100).SetValue(dragon.Health);
-
+        
         local line = CreateHorz(vert).SetFlexibleWidth(1);
         inputs.DynamicDefencePower = CreateCheckBox(line).SetText(" ").SetIsChecked(dragon.DynamicDefencePower).SetOnValueChanged(function() saveDragon(dragon, inputs); healthAndDamage(dragon, vert, inputs); end);
         CreateLabel(line).SetText("Use the Dragon's health as defence power")
@@ -119,7 +123,7 @@ function healthAndDamage(dragon, vert, inputs)
             CreateLabel(vert).SetText("The defence power of the Dragon").SetColor(colors.Textcolor);
             inputs.DefensePower = CreateNumberInputField(vert).SetSliderMinValue(1).SetSliderMaxValue(100).SetValue(dragon.DefensePower);
         end
-    
+        
     else
         CreateLabel(vert).SetText("The number of damage points it takes to kill this dragon").SetColor(colors.Textcolor)
         inputs.DamageToKill = CreateNumberInputField(vert).SetSliderMinValue(1).SetSliderMaxValue(50).SetValue(dragon.DamageToKill);
@@ -129,39 +133,43 @@ function healthAndDamage(dragon, vert, inputs)
 
     CreateLabel(vert).SetText("The attack power of the Dragon").SetColor(colors.Textcolor);
     inputs.AttackPower = CreateNumberInputField(vert).SetSliderMinValue(1).SetSliderMaxValue(50).SetValue(dragon.AttackPower);
-
+    
     CreateLabel(vert).SetText("The attack modifier of the Dragon (percentage)").SetColor(colors.Textcolor);
     inputs.AttackPowerPercentage = CreateNumberInputField(vert).SetWholeNumbers(false).SetSliderMinValue(-100).SetSliderMaxValue(100).SetValue(dragon.AttackPowerPercentage)
-
+    
     CreateLabel(vert).SetText("The defence modifier of the Dragon (percentage)").SetColor(colors.Textcolor);
     inputs.DefensePowerPercentage = CreateNumberInputField(vert).SetWholeNumbers(false).SetSliderMinValue(-100).SetSliderMaxValue(100).SetValue(dragon.DefensePowerPercentage)
-
+    
     SetWindow(parent);
 end
 
 function removeDragon(dragon)
     DestroyWindow();
     SetWindow("removeDragon");
-
+    
+    root.SetPreferredHeight(500);
+    
     local line = CreateHorz(root).SetFlexibleWidth(1);
     CreateEmpty(line).SetFlexibleWidth(0.5);
     CreateLabel(line).SetText("Are you sure you want to remove this dragon?").SetColor(colors.Textcolor);
     CreateEmpty(line).SetFlexibleWidth(0.5);
-
+    
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateEmpty(line).SetFlexibleWidth(0.45);
     CreateButton(line).SetText("Yes").SetColor(colors.Green).SetOnClick(function() table.remove(dragons, dragon.ID); showMain(); end);
     CreateEmpty(line).SetFlexibleWidth(0.1);
     CreateButton(line).SetText("No").SetColor(colors.Red).SetOnClick(function() modifyDragon(dragon); end);
     CreateEmpty(line).SetFlexibleWidth(0.45);
-
+    
     CreateEmpty(root).SetFlexibleHeight(1);
 end
 
 function changeColor(dragon)
     DestroyWindow();
     SetWindow("modifyDragon");
-
+    
+    root.SetPreferredHeight(500);
+    
     local c = {Blue=colors.Blue, Green=colors.Green, Red=colors.Red, Yellow=colors.Yellow, White=colors.Ivory};
     CreateLabel(root).SetText("Pick the color you want").SetColor(colors.Textcolor);
     for _, d in pairs(dragons) do
