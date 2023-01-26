3980d09b579671f703289c898c70b67e2ec3974a
JustMe003 <kingoflol20033@gmail.com>
2023-01-26 17:14:39 +0100

    Update Client_PresentConfigureUI.lua

--- Dragons/Client_PresentConfigureUI.lua
+++ Dragons/Client_PresentConfigureUI.lua
@@ -5,7 +5,7 @@ function Client_PresentConfigureUI(rootParent)
     dragons = Mod.Settings.Dragons
     if dragons == nil then dragons = {}; end
 
-    root = GetRoot().SetFlexibleWidth(1).SetPreferredHeight(500);
+    root = GetRoot().SetFlexibleWidth(1).SetPreferredHeight(600);
     colors = GetColors();
     showMain();
 end
