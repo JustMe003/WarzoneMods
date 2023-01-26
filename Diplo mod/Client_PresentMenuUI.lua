c98d33bc0396a85c8e57bb21b884d918360e6b1e
JustMe003 <kingoflol20033@gmail.com>
2023-01-26 15:08:20 +0100

    updated menus

--- Diplo mod/Client_PresentMenuUI.lua
+++ Diplo mod/Client_PresentMenuUI.lua
@@ -1,17 +1,8 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, Game, close, calledFromGameRefresh)
-	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	local line = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(line).SetText("Mod Author: ").SetColor("#DDDDDD");
-	UI.CreateLabel(line).SetText("Just_A_Dutchman_").SetColor("#8EBE57");
-	line = UI.CreateHorizontalLayoutGroup(vert);
-	UI.CreateLabel(line).SetText("Version: ").SetColor("#DDDDDD");
 	if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
 		require("Client_PresentMenuUI1");
-		UI.CreateLabel(line).SetText("1.5.2").SetColor("#FF4700");
 	else
 		require("Client_PresentMenuUI2");
-		UI.CreateLabel(line).SetText("2.3").SetColor("#FF4700");
 	end
-	UI.CreateEmpty(vert).SetPreferredHeight(10);
-	Client_PresentMenuUIMain(vert, setMaxSize, setScrollable, Game, close, calledFromGameRefresh);
+	Client_PresentMenuUIMain(rootParent, setMaxSize, setScrollable, Game, close, calledFromGameRefresh);
 end
--- Diplo mod/Client_PresentMenuUI1.lua
+++ Diplo mod/Client_PresentMenuUI1.lua
@@ -1,6 +1,20 @@
 require("UI");
 function Client_PresentMenuUIMain(rootParent, setMaxSize, setScrollable, Game, close, calledFromGameRefresh)
-	init(rootParent);
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	local line = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(line).SetText("Mod Author: ").SetColor("#DDDDDD");
+	UI.CreateLabel(line).SetText("Just_A_Dutchman_").SetColor("#8EBE57");
+	line = UI.CreateHorizontalLayoutGroup(vert);
+	UI.CreateLabel(line).SetText("Version: ").SetColor("#DDDDDD");
+	if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
+		require("Client_PresentMenuUI1");
+		UI.CreateLabel(line).SetText("1.5.2").SetColor("#FF4700");
+	else
+		require("Client_PresentMenuUI2");
+		UI.CreateLabel(line).SetText("2.3").SetColor("#FF4700");
+	end
+	UI.CreateEmpty(vert).SetPreferredHeight(10);
+	init(vert);
 	game = Game;
 	if game.Us == nil then UI.Alert("You cannot use this mod since you're not playing in this game"); close(); return; end
 	if game.Us.State ~= WL.GamePlayerState.Playing then UI.Alert("You cannot use this mod anymore since you're not playing anymore"); close(); return; end
