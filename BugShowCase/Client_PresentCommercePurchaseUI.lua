7473786b08d937e2868efadfd800ed36561379c2
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2023-02-04 09:14:45 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- BugShowCase/Client_PresentCommercePurchaseUI.lua
+++ BugShowCase/Client_PresentCommercePurchaseUI.lua
@@ -1,5 +1,5 @@
 require("Client_PresentMenuUI");
 function Client_PresentCommercePurchaseUI(rootParent, game, close)
-    vert = UI.CreateVerticalLayoutGroup(rootParent);
+    local vert = UI.CreateVerticalLayoutGroup(rootParent);
     UI.CreateButton(vert).SetText("Click me").SetOnClick(function() close(); game.CreateDialog(Client_PresentMenuUI) end);
 end
