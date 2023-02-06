623904e994146d17cdaf86b7af1127532ad35875
JustMe003 <kingoflol20033@gmail.com>
2023-02-06 21:33:12 +0100

    Update Server_StartGame.lua

--- Dreadfully Hypothermia 4/Server_StartGame.lua
+++ Dreadfully Hypothermia 4/Server_StartGame.lua
@@ -3,8 +3,7 @@ function Server_StartGame(game, standing)
     local s = standing;
     for _, v in pairs(s.Territories) do
         if not v.IsNeutral and t[v.OwnerPlayerID] == nil then
-            armies = s.Territories[v.ID].NumArmies;
-            armies = WL.Armies.Create(s.Territories[v.ID].NumArmies.NumArmies, {WL.Boss3.Create(v.OwnerPlayerID, 1)})
+            local armies = s.Territories[v.ID].NumArmies.Add(WL.Armies.Create(0, {WL.Boss3.Create(v.OwnerPlayerID, 1)}))
             s.Territories[v.ID].NumArmies = armies;
             t[v.OwnerPlayerID] = true;
         end
