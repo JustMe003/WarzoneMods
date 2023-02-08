4659346ef07fe4d82acb62a71e18b199f261db5e
JustMe003 <kingoflol20033@gmail.com>
2023-02-08 15:46:15 +0100

    Update Server_AdvanceTurn.lua

--- Dragons/Server_AdvanceTurn.lua
+++ Dragons/Server_AdvanceTurn.lua
@@ -8,9 +8,8 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
             local dragonBreathDamage = 0;
             for _, sp in pairs(orderResult.ActualArmies.SpecialUnits) do
                 if sp.proxyType == "CustomSpecialUnit" then
-                    print(Mod.PublicGameData.DragonNamesIDs[sp.Name])
                     if sp.ModID ~= nil and sp.ModID == 594 and Mod.PublicGameData.DragonBreathAttack[Mod.PublicGameData.DragonNamesIDs[sp.Name]] ~= nil then
-                        dragonBreathDamage = dragonBreathDamage + 1;
+                        dragonBreathDamage = dragonBreathDamage + Mod.PublicGameData.DragonBreathAttack[Mod.PublicGameData.DragonNamesIDs[sp.Name]];
                     end
                 end
             end
