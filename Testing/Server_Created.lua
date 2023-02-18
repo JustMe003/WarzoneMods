12f921650e1ea4172215ef3d6fd39b1a3b6d649a
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2023-02-18 08:11:46 +0100

    Update Server_Created.lua

--- Testing/Server_Created.lua
+++ Testing/Server_Created.lua
@@ -1,8 +1,3 @@
 function Server_Created(game, settings)
-    local cards = {};
-    for i, v in pairs(settings.Cards) do
-        cards[i] = v;
-    end
-    cards[WL.CardID.Spy] = WL.CardGameSpy.Create(1000, 0, 0, 0, 999999, true);
-    settings.Cards = cards;
+    
 end
