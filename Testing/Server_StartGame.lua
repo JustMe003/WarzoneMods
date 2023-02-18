ba42a59833fad975ea44b45e02701227a53d397c
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2023-02-18 08:46:36 +0100

    Update Server_StartGame.lua

--- Testing/Server_StartGame.lua
+++ Testing/Server_StartGame.lua
@@ -1,4 +1,5 @@
 function Server_StartGame(game, standing)
+    print("Mod: Testing")
     local s = standing;
     if s.Territories[1] ~= nil then
         s.Territories[1].NumArmies = s.Territories[1].NumArmies.Add(WL.Armies.Create(0, {WL.Boss1.Create(s.Territories[1].OwnerPlayerID), WL.Boss2.Create(s.Territories[1].OwnerPlayerID), WL.Boss1.Create(s.Territories[1].OwnerPlayerID)}))
