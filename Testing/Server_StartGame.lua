e4c61d7fd424127208b8fbf51abb20fb3462b410
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2023-02-18 08:45:54 +0100

    Update Server_StartGame.lua

--- Testing/Server_StartGame.lua
+++ Testing/Server_StartGame.lua
@@ -4,7 +4,4 @@ function Server_StartGame(game, standing)
         s.Territories[1].NumArmies = s.Territories[1].NumArmies.Add(WL.Armies.Create(0, {WL.Boss1.Create(s.Territories[1].OwnerPlayerID), WL.Boss2.Create(s.Territories[1].OwnerPlayerID), WL.Boss1.Create(s.Territories[1].OwnerPlayerID)}))
     end
     standing = s;
-    for i, _ in pairs(game.Game.Players) do
-        print(i)
-    end
 end
