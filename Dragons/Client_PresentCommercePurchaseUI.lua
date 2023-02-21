c46a2b431f94b3e79976ba298e294295bc780715
JustMe003 <kingoflol20033@gmail.com>
2023-02-21 14:16:57 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- Dragons/Client_PresentCommercePurchaseUI.lua
+++ Dragons/Client_PresentCommercePurchaseUI.lua
@@ -24,9 +24,6 @@ function createDialog(rootParent, setMaxSize, setScrollable, game, close)
         if order.proxyType == "GameOrderCustom" and order.Payload:sub(1, #"Dragons_") == "Dragons_" then
             local info = split(order.Payload, "_");
             info[2] = tonumber(info[2]);
-            for i, v in pairs(info) do
-                print(i, v)
-            end
             dragonsOwned[info[2]] = dragonsOwned[info[2]] + 1;
         end
     end
