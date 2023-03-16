de98b3ac8113377020cf328c50801f6537cf7e10
JustMe003 <kingoflol20033@gmail.com>
2023-03-16 17:57:06 +0100

    Update Client_PresentMenuUI.lua

--- Local_Deployment_Helper/Client_PresentMenuUI.lua
+++ Local_Deployment_Helper/Client_PresentMenuUI.lua
@@ -259,7 +259,7 @@ function AddOrdersConfirmes(inputs)
 	local appendDeploys = 0;
 	local appendTransfers = 0;
 	for i, order in pairs(orderTable) do
-		print(i);
+		print(i, order.OccursInPhase);
 		if order.OccursInPhase ~= nil then
 			if appendDeploys == 0 and order.OccursInPhase > WL.TurnPhase.Deploys then
 				appendDeploys = i;
