d6e9ebfd8e46d095bc1ccbbe55e0f37ae717bd82
JustMe003 <kingoflol20033@gmail.com>
2023-03-16 11:56:49 +0100

    Update Client_GameRefresh.lua

--- LDHelper2/Client_GameRefresh.lua
+++ LDHelper2/Client_GameRefresh.lua
@@ -1,7 +1,6 @@
 require("Client_PresentMenuUI");
 require("UI");
 function Client_GameRefresh(game)
-    print(lastTurn, game.Game.TurnNumber);
     if game.Us == nil or game.Us.IsAIOrHumanTurnedIntoAI then
         return;
     end
@@ -9,10 +8,7 @@ function Client_GameRefresh(game)
         if Mod.PlayerGameData.NewTurnAction == nil then
             game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "SetAction"); end);
         else
-            print(tostring(tableIsEmpty(game.Orders)));
-            printTable(game.Orders);
             if tableIsEmpty(game.Orders) then
-                print(Mod.PlayerGameData.NewTurnAction);
                 if Mod.PlayerGameData.NewTurnAction == "AutoDeploy" then
                     game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "AutoDeploy"); end);
                 elseif Mod.PlayerGameData.NewTurnAction == "ShowWindow" then
@@ -29,10 +25,4 @@ function tableIsEmpty(t)
         return false;
     end
     return true;
-end
-
-function printTable(t)
-    for i, v in pairs(t) do
-        print(i, v);
-    end
 end
