11e34d9fa8c3f7be503c0655ead40f370f70cc74
JustMe003 <kingoflol20033@gmail.com>
2023-03-29 15:53:59 +0200

    Update Client_PresentSettingsUI.lua

--- Decoy Card/Client_PresentSettingsUI.lua
+++ Decoy Card/Client_PresentSettingsUI.lua
@@ -2,7 +2,7 @@ require("UI");
 function Client_PresentSettingsUI(rootParent)
 	init(rootParent);
 	local vert = newVerticalGroup("vert", "root");
-	newLabel("durationLabel", vert, "Maximum duration of a decoy card: " .. Mod.Settings.AmountOfTurns);
-	newLabel("cardPiecesLabel", vert, "Card pieces needed for a card: " .. Mod.Settings.CardPieces);
-	newLabel("startAmount", vert, "Card pieces each players starts with: " .. Mod.Settings.StartAmount);
+	newLabel("durationLabel", vert, "Maximum duration of a decoy card: " .. tostring(Mod.Settings.AmountOfTurns));
+	newLabel("cardPiecesLabel", vert, "Card pieces needed for a card: " .. tostring(Mod.Settings.CardPieces));
+	newLabel("startAmount", vert, "Card pieces each players starts with: " .. tostring(Mod.Settings.StartAmount));
 end
