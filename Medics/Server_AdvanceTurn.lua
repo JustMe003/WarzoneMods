0e883d0432bebe9b407cea1a8a07e14db6c1d848
JustMe003 <kingoflol20033@gmail.com>
2023-05-08 22:18:24 +0200

    Update Server_AdvanceTurn.lua

--- Medics/Server_AdvanceTurn.lua
+++ Medics/Server_AdvanceTurn.lua
@@ -28,6 +28,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
             builder.ImageFilename = "Medic.png";
             builder.IncludeABeforeName = true;
             builder.TextOverHeadOpt = "Medic";
+            builder.ModData = "UnitDescription:\"The medic does not like standing on the front lines, but rather wants to stay back to heal up any wounded soldiers. Any time the player owning this unit loses armies on a territory connected to this medic, it will recover " .. Mod.Settings.Percentage .. "% of armies lost\"";
             local mod = WL.TerritoryModification.Create(terrID);
             mod.AddSpecialUnits = {builder.Build()};
 
