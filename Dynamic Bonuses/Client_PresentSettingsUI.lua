e78417c3224d4d5366ab7e65417c9db588f224c8
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2023-06-17 08:43:00 +0200

    Update Client_PresentSettingsUI.lua

--- Dynamic Bonuses/Client_PresentSettingsUI.lua
+++ Dynamic Bonuses/Client_PresentSettingsUI.lua
@@ -2,7 +2,7 @@ function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
     UI.CreateLabel(vert).SetText("Minimum multiplier: " .. rounding(Mod.Settings.MinMultiplier, 2)).SetColor("#8EBE57");
     UI.CreateLabel(vert).SetText("Maximum multiplier: " .. rounding(Mod.Settings.MaxMultiplier, 2)).SetColor("#8EBE57");
-    UI.CreateLabel(vert).SetText("Multiplier increment each turn: " .. rounding(Mod"#8EBE57");
+    UI.CreateLabel(vert).SetText("Multiplier increment each turn: " .. rounding(Mod.Settings.LevelMultiplierIncrement, 2)).SetColor("#8EBE57");
 end
 
 function rounding(num, numDecimalPlaces)
