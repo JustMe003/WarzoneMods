a0c93b4f3261e898890f849ab21f12ef56950cf4
JustMe003 <kingoflol20033@gmail.com>
2023-06-29 16:42:24 +0200

    Update Client_PresentConfigureUI.lua

--- randomized_nonograms/Client_PresentConfigureUI.lua
+++ randomized_nonograms/Client_PresentConfigureUI.lua
@@ -11,7 +11,7 @@ function Client_PresentConfigureUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	CreateLabel(vert).SetText("This option will set the distribution to manual, everyone can see the solution of the nonogram back in the history").SetColor(textColor);
 	CreateLabel(vert).SetText("Only make the territories that will give you gold / armies pickable").SetColor(textColor);
-	setCustomDistribution = createCheckBox(vert).SetIsChecked(customDistribution).SetText(" ");
+	setCustomDistribution = CreateCheckBox(vert).SetIsChecked(customDistribution).SetText(" ");
 	CreateLabel(line).SetText("Specify the density in percentages (60% or more is recommended!)").SetColor(textColor);
 	setDensity = CreateNumberInputField(vert).SetSliderMinValue(25).SetSliderMaxValue(100).SetValue(initialValueDensity);
 	CreateLabel(vert).SetText("Check checkbox below to play with custom local deployments").SetColor(textColor);
