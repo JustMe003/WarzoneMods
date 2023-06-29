8238d02a8af89af1db0589eaa124bbc8dd2b7d1b
JustMe003 <kingoflol20033@gmail.com>
2023-06-29 16:41:24 +0200

    .

--- randomized_nonograms/Client_PresentConfigureUI.lua
+++ randomized_nonograms/Client_PresentConfigureUI.lua
@@ -1,33 +1,18 @@
 require("UI")
-local colors = initColors();
 
 function Client_PresentConfigureUI(rootParent)
-	init();
+	Init(rootParent);
+	local textColor = GetColors().TextColor;
 	local customDistribution = Mod.Settings.CustomDistribution;
-	local initialHeigth = Mod.Settings.NonogramHeigth;
-	local initialWidth = Mod.Settings.NonogramWidth;
 	local initialValueDensity = Mod.Settings.NonogramDensity;
-	local localDeploymentsValue = Mod.Settings.LocalDeployments;
 	if customDistribution == nil then customDistribution = false; end
-	if initialHeigth == nil then initialHeigth = 10; end
-	if initialWidth == nil then initialWidth = 10; end
 	if initialValueDensity == nil then initialValueDensity = 60; end
-	if localDeploymentsValue == nil then localDeploymentsValue = false; end
 	
-	-- vert is the parent of the whole layout
-    local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	createLabel(vert, "This option will set the distribution to manual, everyone can see the solution of the nonogram back in the history", colors.Ivory);
-	createLabel(vert, "Only make the territories that will give you gold / armies pickable", colors.TextColor);
-	setCustomDistribution = createCheckBox(vert, customDistribution, " ");
-	createLabel(vert, "Specify the heigth of the nonogram:", colors.TextColor);
-	setHeigth = createNumberInputField(vert, initialHeigth, 5, 20);
-	createLabel(vert, "Specify the width of the nonogram:", colors.TextColor);
-	setWidth = createNumberInputField(vert, initialWidth, 5, 20);
-	line = getNewHorz(vert)
-	createLabel(line, "Specify the density in percentages ", colors.TextColor);
-	createLabel(line, "(60% or more is recommended!)", colors.WarningNumberColor);
-	setDensity = createNumberInputField(vert, initialValueDensity, 30, 80);
-	createLabel(vert, " ", colors.TextColor);
-	createLabel(vert, "Check checkbox below to play with custom local deployments", colors.TextColor);
-	LocalDeployments = createCheckBox(vert, localDeploymentsValue, " ");
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	CreateLabel(vert).SetText("This option will set the distribution to manual, everyone can see the solution of the nonogram back in the history").SetColor(textColor);
+	CreateLabel(vert).SetText("Only make the territories that will give you gold / armies pickable").SetColor(textColor);
+	setCustomDistribution = createCheckBox(vert).SetIsChecked(customDistribution).SetText(" ");
+	CreateLabel(line).SetText("Specify the density in percentages (60% or more is recommended!)").SetColor(textColor);
+	setDensity = CreateNumberInputField(vert).SetSliderMinValue(25).SetSliderMaxValue(100).SetValue(initialValueDensity);
+	CreateLabel(vert).SetText("Check checkbox below to play with custom local deployments").SetColor(textColor);
 end
--- randomized_nonograms/Client_SaveConfigureUI.lua
+++ randomized_nonograms/Client_SaveConfigureUI.lua
@@ -1,15 +1,8 @@
 
 function Client_SaveConfigureUI(alert)
 	Mod.Settings.CustomDistribution = setCustomDistribution.GetIsChecked();
-    Mod.Settings.NonogramWidth = setWidth.GetValue();
-	Mod.Settings.NonogramHeigth = setHeigth.GetValue();
 	Mod.Settings.NonogramDensity = setDensity.GetValue();
-	Mod.Settings.LocalDeployments = LocalDeployments.GetIsChecked();
---	If the host made a mistake or tried to break the mod it checks if the given inputs are playable
-	if Mod.Settings.NonogramWidth < 5 then Mod.Settings.NonogramWidth = 5; end
-	if Mod.Settings.NonogramWidth > 20 then Mod.Settings.NonogramWidth = 20; end
-	if Mod.Settings.NonogramHeigth < 5 then Mod.Settings.NonogramHeigth = 5; end
-	if Mod.Settings.NonogramHeigth > 20 then Mod.Settings.NonogramHeigth = 20; end
+
 	if Mod.Settings.NonogramDensity < 10 then Mod.Settings.NonogramDensity = 10; end
 	if Mod.Settings.NonogramDensity > 90 then Mod.Settings.NonogramDensity = 90; end
 end
