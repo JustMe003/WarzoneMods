8bc8f8c344aa289ad11a6f5925fdd11ea4b7f3a3
JustMe003 <kingoflol20033@gmail.com>
2023-07-06 19:24:20 +0200

    .

--- BugShowCase/Client_PresentCommercePurchaseUI.lua
+++
@@ -1,5 +0,0 @@
-require("Client_PresentMenuUI");
-function Client_PresentCommercePurchaseUI(rootParent, game, close)
-    local vert = UI.CreateVerticalLayoutGroup(rootParent);
-    UI.CreateButton(vert).SetText("Click me").SetOnClick(function() close(); game.CreateDialog(Client_PresentMenuUI) end);
-end
