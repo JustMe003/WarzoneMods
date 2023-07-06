382e681668bf880a76cdb48248c9637302030a22
JustMe003 <kingoflol20033@gmail.com>
2023-07-06 19:17:48 +0200

    Update Server_Created.lua

--- randomized_nonograms/Server_Created.lua
+++ randomized_nonograms/Server_Created.lua
@@ -1,6 +1,6 @@
 
 function Server_Created(game, settings)
-	for i, v in pairs(game.Map.DistributionModes.readableKeys) do
+	for i, v in pairs(game.Map.DistributionModes) do
 		print(i, v);
 	end
 	data = Mod.PublicGameData;
