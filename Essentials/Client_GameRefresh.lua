c761c7256da0cda225d887de0f9b0c63dbbb09e0
JustMe003 <kingoflol20033@gmail.com>
2023-09-05 11:31:32 +0200

    Update Client_GameRefresh.lua

--- Essentials/Client_GameRefresh.lua
+++ Essentials/Client_GameRefresh.lua
@@ -1,5 +1,6 @@
 function Client_GameRefresh(game)
     if Mod.Settings.Links ~= nil and Mod.PlayerGameData.HasSeenLinks == nil then
         game.SendGameCustomMessage("Updating...", {}, function(t)  end);
+        UI.Alert("Test");
     end
 end
