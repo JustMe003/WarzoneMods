f94e8fb258d81b1ef7919f176eb9197675aac169
JustMe003 <kingoflol20033@gmail.com>
2023-09-05 11:59:31 +0200

    Update Client_GameRefresh.lua

--- Essentials/Client_GameRefresh.lua
+++ Essentials/Client_GameRefresh.lua
@@ -1,5 +1,8 @@
+require("Client_PresentMenuUI")
+
 function Client_GameRefresh(game)
     if Mod.Settings.Links ~= nil and Mod.PlayerGameData.HasSeenLinks == nil then
         game.SendGameCustomMessage("Updating...", {}, function(t)  end);
+        game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, showDocumentLinks); end);
     end
 end
