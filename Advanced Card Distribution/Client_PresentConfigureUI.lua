0219940e517d426cc54efd828a526e8c97e74cad
JustMe003 <kingoflol20033@gmail.com>
2023-09-25 23:30:27 +0200

    Update Client_PresentConfigureUI.lua

--- Advanced Card Distribution/Client_PresentConfigureUI.lua
+++ Advanced Card Distribution/Client_PresentConfigureUI.lua
@@ -124,7 +124,7 @@ function pickSlotToCopy(copy)
 	for i, _ in pairs(needsEntry) do
 		local t = {};
 		t.text = "Slot " .. getSlotName(i);
-		t.selected = function() copySlot(copy, i); getConfig(i); end
+		t.selected = function() copySlot(i, copy); getConfig(copy); end
 		table.insert(list, t);
 		print(i);
 	end
