28c0f8cf28e0f6882799cb93f5db6c695c9f403b
JustMe003 <kingoflol20033@gmail.com>
2023-11-06 13:21:14 +0100

    added SpecialUnitsData

---
+++ Medics/SpecialUnitsData.lua
@@ -0,0 +1,44 @@
+function dataToString(t)
+    local result = {};
+    for key, value in pairs(t) do
+        if type(value) ~= type({}) then
+            table.insert(result, string.format("%s=%s", key, value))
+        else
+            table.insert(result, string.format("%s=%s", key, getJSON(value)));
+        end
+    end
+    return "{" .. table.concat(result, ";") .. "}";
+end
+
+function stringToData(s, layer)
+    if layer == nil then layer = 1; end
+    local t = {};
+    while #s > 1 do
+        local ending = string.find(s, "=");
+        local key = string.sub(s, string.find(s, "%w+"), ending - 1);
+        s = string.sub(s, ending + 1, -1);
+        if tonumber(key) ~= nil then
+            key = tonumber(key);
+        end
+        local c = string.sub(s, 1, 1);
+        if c == "{" then
+            t[key], s = getObjectFromJSON(string.sub(s, 2, -1), layer + 1);
+            c = string.sub(s, 1, 1);
+            if c == "}" then
+                return t, string.sub(s, 2, -1);
+            end
+        else
+            local start, last = string.find(s, "[%w%s]+");
+            t[key] = string.sub(s, start, last);
+            if tonumber(t[key]) ~= nil then
+                t[key] = tonumber(t[key]);
+            end
+            c = string.sub(s, last + 1, last + 1);
+            s = string.sub(s, last + 2, -1);
+            if c == "}" then
+                return t, s;
+            end
+        end
+    end
+    return t;
+end
