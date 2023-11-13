16911e6fbf5187956dff87446cd212c81fe31bb3
JustMe003 <kingoflol20033@gmail.com>
2023-11-13 12:49:42 +0100

    Update Server_AdvanceTurn.lua

--- Meteor Strike Plus/Server_AdvanceTurn.lua
+++ Meteor Strike Plus/Server_AdvanceTurn.lua
@@ -37,6 +37,7 @@ function Server_AdvanceTurn_End(game, addNewOrder)
     local turnNumber = game.Game.TurnNumber;
     
     for i, data in ipairs(privData.Doomsdays) do
+        print(data.Turn);
         if turnNumber == data.Turn then
             local num = data.Data.NumOfMeteors + math.random(0, data.Data.RandomNumOfMeteor);
             table.insert(meteors, {Weight = num, Data = data.Data});
