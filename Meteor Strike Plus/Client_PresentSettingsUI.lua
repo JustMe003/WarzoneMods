8af502070d005a2d1ca5b282d67d15c505f971bb
JustMe003 <kingoflol20033@gmail.com>
2023-11-14 16:11:35 +0100

    Update Client_PresentSettingsUI.lua

--- Meteor Strike Plus/Client_PresentSettingsUI.lua
+++ Meteor Strike Plus/Client_PresentSettingsUI.lua
@@ -41,17 +41,17 @@ function showNormalStorm(data, b)
     if data.NotEveryTurn then
         CreateLabel(line).SetText("No").SetColor(colors.Red);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("This storm is only active on a certain interval of turns"); end);
+        CreateButton(line).SetText("?").SetColor().SetOnClick(function() UI.Alert("This storm is only active on a certain interval of turns"); end);
         
         line = CreateHorz(root).SetFlexibleWidth(1);
         CreateLabel(line).SetText("Storm active:").SetColor(colors.TextColor);
         CreateLabel(line).SetText(data.StartStorm .. " - " .. data.EndStorm).SetColor(colors.Cyan);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("THe storm will be active from turn " .. data.StartStorm .. " till (and including) " .. data.EndStorm); end);
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("THe storm will be active from turn " .. data.StartStorm .. " till (and including) " .. data.EndStorm); end);
     else
         CreateLabel(line).SetText("Yes").SetColor(colors.Green);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("This storm is active throughout the whole game"); end);
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("This storm is active throughout the whole game"); end);
     end
 
 
@@ -59,7 +59,7 @@ function showNormalStorm(data, b)
     CreateLabel(line).SetText("Chance of falling:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(showDecimal(data.ChanceofFalling, 2) .. "%").SetColor(colors.Cyan);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("Every turn this storm is active, it has a chance of actually damaging the map. 100% means that every active turn the map will be damages, 50% means half of the turns the storm is active and 1% means that on average, 1 in 100 turns the map will get damaged"); end);
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("Every turn this storm is active, it has a chance of actually damaging the map. 100% means that every active turn the map will be damages, 50% means half of the turns the storm is active and 1% means that on average, 1 in 100 turns the map will get damaged"); end);
 
     showGeneralData(data);
 
@@ -81,23 +81,23 @@ function showDoomsdayStorm(data, b)
     if data.RandomTurn then
         CreateLabel(line).SetText("Yes").SetColor(colors.Green);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("This doomsday storm will be active on a random turn"); end);
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("This doomsday storm will be active on a random turn"); end);
         
         line = CreateHorz(root).SetFlexibleWidth(1);
         CreateLabel(line).SetText("Active turn:").SetColor(colors.TextColor);
         CreateLabel(line).SetText(data.MinTurnNumber .. " - " .. data.MaxTurnNumber).SetColor(colors.Cyan);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("The doomsday storm will become active somwhere between turn " .. data.MinTurnNumber .. " and " .. data.MaxTurnNumber); end);
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("The doomsday storm will become active somwhere between turn " .. data.MinTurnNumber .. " and " .. data.MaxTurnNumber); end);
     else
         CreateLabel(line).SetText("No").SetColor(colors.Red);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("This doomsday storm will be active on a fixed turn") end);
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("This doomsday storm will be active on a fixed turn") end);
     
         line = CreateHorz(root).SetFlexibleWidth(1);
         CreateLabel(line).SetText("Fixed turn:").SetColor(colors.TextColor);
         CreateLabel(line).SetText(data.FixedTurn).SetColor(colors.Cyan);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("The doomsday will become active at turn " .. data.FixedTurn); end);    
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("The doomsday will become active at turn " .. data.FixedTurn); end);    
     end
 
     showGeneralData(data);
@@ -112,7 +112,7 @@ function showName(data)
     CreateLabel(line).SetText("Name:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(data.Name).SetColor(colors.Yellow);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("The name of the storm. Mostly used to easily identify storms from each other") end)
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("The name of the storm. Mostly used to easily identify storms from each other") end)
 end
 
 function showGeneralData(data)
@@ -120,26 +120,26 @@ function showGeneralData(data)
     CreateLabel(line).SetText("Number of meteors:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(data.NumOfMeteors .. " + " .. data.RandomNumOfMeteor .. "?").SetColor(colors.Cyan);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert(data.NumOfMeteors .. " meteors will fall, with an additional 0 to " .. data.RandomNumOfMeteor .. " more"); end)
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert(data.NumOfMeteors .. " meteors will fall, with an additional 0 to " .. data.RandomNumOfMeteor .. " more"); end)
     
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateLabel(line).SetText("Meteor damage:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(data.MeteorDamage).SetColor(colors.Cyan);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("The damage a meteor will inflict on a territory when hit. It takes special units into account, and when the territory is empty after the meteor landed it is turned to neutral"); end)
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("The damage a meteor will inflict on a territory when hit. It takes special units into account, and when the territory is empty after the meteor landed it is turned to neutral"); end)
     
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateLabel(line).SetText("Can spawn Alien:").SetColor(colors.TextColor);
     if data.CanSpawnAlien then
         CreateLabel(line).SetText("Yes").SetColor(colors.Green);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("When a meteor falls, it has a chance of spawning an Alien unit. This applies only to territories that either are neutral or turn neutral after the meteor fell"); end)
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("When a meteor falls, it has a chance of spawning an Alien unit. This applies only to territories that either are neutral or turn neutral after the meteor fell"); end)
     
         showAlienData(data);
     else
         CreateLabel(line).SetText("No").SetColor(colors.Red);
         CreateEmpty(line).SetFlexibleWidth(1);
-        CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("This storm will not spawn Alien units"); end)
+        CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("This storm will not spawn Alien units"); end)
     end
     
 end
@@ -149,13 +149,13 @@ function showAlienData(data)
     CreateLabel(line).SetText("Alien spawn chance:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(showDecimal(data.AlienSpawnChance, 2) .. "%").SetColor(colors.Cyan);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("The chance of an alien spawning when a meteor hits. This only happens if the territory is neutral or turns neutral because of the meteor") end)
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("The chance of an alien spawning when a meteor hits. This only happens if the territory is neutral or turns neutral because of the meteor") end)
 
     line = CreateHorz(root).SetFlexibleWidth(1);
     CreateLabel(line).SetText("Alien health:").SetColor(colors.TextColor);
     CreateLabel(line).SetText(data.AlienDefaultHealth .. " + " .. data.AlienRandomHealth .. "?").SetColor(colors.Cyan);
     CreateEmpty(line).SetFlexibleWidth(1);
-    CreateButton(line).SetText("?").SetColor(colors.Aqua).SetOnClick(function() UI.Alert("Aliens have an amount of hitpoints equal to " .. data.AlienDefaultHealth .. " plus a random amount between 0 and " .. data.AlienRandomHealth); end)
+    CreateButton(line).SetText("?").SetColor(colors["Light Blue"]).SetOnClick(function() UI.Alert("Aliens have an amount of hitpoints equal to " .. data.AlienDefaultHealth .. " plus a random amount between 0 and " .. data.AlienRandomHealth); end)
 end
 
 function showAll()
