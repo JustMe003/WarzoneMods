02fcee86774b84500c8479a2eb159241e277c58a
JustMe003 <kingoflol20033@gmail.com>
2024-02-08 23:03:12 +0100

    Update Client_PresentMenuUI.lua

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -1,4 +1,4 @@
-function Client_PresentMenuUI(rootParent, game, setMaxSize, setScrollable, close)
+function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
     local orders = game.Orders;
     table.insert(orders, WL.GameOrderCustom.Create(game.Us.ID, "Testing", ""));
     game.Orders = orders;
