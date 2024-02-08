5bdfea0e6c7d1480079929c6d99022beccdefc51
JustMe003 <kingoflol20033@gmail.com>
2024-02-08 01:07:19 +0100

    Update Client_PresentMenuUI.lua

--- Essentials/Client_PresentMenuUI.lua
+++ Essentials/Client_PresentMenuUI.lua
@@ -557,6 +557,7 @@ function subtitudeData(sp, data, text)
 	};
 
 	for name, f in pairs(commandMap) do
+		print("{{" .. name .. "}}", text.find("{{" .. name .. "}}"));
 		text = text.gsub("{{" .. name .. "}}", f());
 	end
 
