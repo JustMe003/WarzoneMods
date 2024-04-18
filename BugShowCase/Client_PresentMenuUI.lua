909cb6fd50f2f1b2ffcd105587d547a8b7d6d72f
JustMe003 <kingoflol20033@gmail.com>
2024-04-18 23:25:15 +0200

    Update Client_PresentMenuUI.lua

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -1,8 +1,10 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
     local list = {9, 5, 6, 2, 7};
-    print(table.concat(list, ", "));
+    print("List:", table.concat(list, ", "));
 
     local output = table.sort(list);
-    print(tostring(output), output == nil);
-    print(table.concat(list, ", "));
+    print("output: ", tostring(output));
+    print("output == nil", output == nil);
+    print("List == output", tostring(list), tostring(output), output == list);
+    print("List: ", table.concat(list, ", "));
 end
