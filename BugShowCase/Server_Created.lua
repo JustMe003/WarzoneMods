e3306d60f0d31ba5889be54a17d5848e322a1fea
JustMe003 <kingoflol20033@gmail.com>
2024-04-22 20:19:37 +0200

    Update Server_Created.lua

--- BugShowCase/Server_Created.lua
+++ BugShowCase/Server_Created.lua
@@ -1,6 +1,13 @@
 
 function Server_Created(game, settings)
-	for i, v in pairs(game.Map.DistributionModes) do
-		print(i, v);
+	local data = Mod.PlayerGameData;
+	local lastTable = data[1];		-- AI 1 has ID 1
+	for i = 1, 100 do
+		print("Level: " .. i);
+		local t = {};
+		table.insert(lastTable, t);
+		lastTable = t;
+		Mod.PlayerGameData = data;
 	end
+
 end
