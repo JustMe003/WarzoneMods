4a971f424ab5386ec6a966220f71c2efa0800625
JustMe003 <kingoflol20033@gmail.com>
2024-06-05 18:35:34 +0200

    Added new bug to the bug showcase mod

---
+++ BugShowCase/Client_PresentSettingsUI.lua
@@ -0,0 +1,48 @@
+require("Annotations");
+require("UI");
+require("Util")
+
+---Client_PresentSettingsUI hook
+---@param rootParent RootParent
+function Client_PresentSettingsUI(rootParent)
+	Init(rootParent);
+    colors = GetColors();
+    root = GetRoot();
+
+    showMain();
+end
+
+function showMain()
+    CreateLabel(root).SetText("The following slots have a modified amount of gold banked at the start of the game").SetColor(colors.TextColor).SetPreferredWidth(300);
+
+    for slot, reserve in pairs(Mod.Settings.Config) do
+        local line = CreateHorz(root).SetFlexibleWidth(1);
+        local vert1 = CreateVert(line).SetFlexibleWidth(0.475);
+        CreateEmpty(line).SetFlexibleWidth(0.05);
+        local vert2 = CreateVert(line).SetFlexibleWidth(0.475);
+        line = CreateHorz(vert1).SetFlexibleWidth(1);
+        CreateEmpty(line).SetFlexibleWidth(1);
+        CreateButton(line).SetText(getSlotName(slot)).SetColor(getSlotColor(slot));
+        line = CreateHorz(vert2).SetFlexibleWidth(1);
+        CreateLabel(line).SetText(getReserveText(reserve)).SetColor(getReserveTextColor(reserve));
+        CreateEmpty(line).SetFlexibleWidth(1);
+    end
+end
+
+function getReserveText(n)
+    if n < 0 then 
+        return "- " .. math.abs(n);
+    elseif n > 0 then
+        return "+ " .. n;
+    end
+    return n;
+end
+
+function getReserveTextColor(n)
+    if n > 0 then
+        return colors.Green;
+    elseif n < 0 then
+        return colors["Orange Red"];
+    end
+    return colors.Cyan;
+end
