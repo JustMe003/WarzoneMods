17b83cf1beab996ec2127a0355930dc5cd93091b
JustMe003 <kingoflol20033@gmail.com>
2024-07-01 07:07:44 +0200

    .

--- Gold Reserves/Server_StartGame.lua
+++ Gold Reserves/Server_StartGame.lua
@@ -5,10 +5,10 @@ require("Annotations");
 ---@param standing GameStanding
 function Server_StartGame(game, standing)
     local res = standing.Resources;
-    print(res);
     if res == nil then res = {}; end
     for _, player in pairs(game.Game.PlayingPlayers) do
         if player.Slot ~= nil and Mod.Settings.Config[player.Slot] ~= nil then
+            print(res[player.ID]);
             if res[player.ID] == nil then res[player.ID] = {}; end
             local t = {};
             for i, v in pairs(res[player.ID]) do
