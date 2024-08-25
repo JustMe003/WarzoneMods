24260e2a80e0049f684896e05cdc392432918ea5
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2024-08-25 23:01:41 +0200

    Update Server_AdvanceTurn.lua

--- BombReducesCities/Server_AdvanceTurn.lua
+++ BombReducesCities/Server_AdvanceTurn.lua
@@ -1,7 +1,9 @@
 function Server_AdvanceTurn_Order(order, orderResult, game, skipThisOrder, addNewOrder)
 	if order.proxyType == "GameOrderPlayCardBomb" then
+		print(1)
 		local structures = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritoryID].Structures;
 		if structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] > 0 then
+			print(2)
 			local mod = WL.TerritoryModifidation.Create(order.TargetTerritoryID);
 			mod.AddStructuresOpt = {
 				[WL.StructureType.City] = -Mod.Settings.NumCities
