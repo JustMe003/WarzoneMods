98aade8ef729762e85e8f114dfc9ecd4ea2fafc8
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2024-08-25 23:05:50 +0200

    Update Server_AdvanceTurn.lua

--- BombReducesCities/Server_AdvanceTurn.lua
+++ BombReducesCities/Server_AdvanceTurn.lua
@@ -4,7 +4,7 @@ function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNe
 		local structures = game.ServerGame.LatestTurnStanding.Territories[order.TargetTerritoryID].Structures;
 		if structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] > 0 then
 			print(2)
-			local mod = WL.TerritoryModifidation.Create(order.TargetTerritoryID);
+			local mod = WL.TerritoryModification.Create(order.TargetTerritoryID);
 			mod.AddStructuresOpt = {
 				[WL.StructureType.City] = -Mod.Settings.NumCities
 			};
