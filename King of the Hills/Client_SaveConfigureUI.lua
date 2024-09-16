fceac061c0410f28fc67e5b6d4283be9e24c6e18
JustMe003 <kingoflol20033@gmail.com>
2024-09-16 10:25:29 +0200

    Added anti-illegal inputs

--- King of the Hills/Client_SaveConfigureUI.lua
+++ King of the Hills/Client_SaveConfigureUI.lua
@@ -3,7 +3,7 @@ require("Annotations");
 ---Client_SaveConfigureUI hook
 ---@param alert fun(message: string) # Alert the player that something is wrong, for example, when a setting is not configured correctly. When invoked, cancels the player from saving and returning
 function Client_SaveConfigureUI(alert)
-	Mod.Settings.NumHills = numHills.GetValue();
-    Mod.Settings.NumTurns = numTurns.GetValue();
-    Mod.Settings.NumArmies = numArmies.GetValue();
+	Mod.Settings.NumHills = math.max(numHills.GetValue(), 1);
+    Mod.Settings.NumTurns = math.max(numTurns.GetValue(), 1);
+    Mod.Settings.NumArmies = math.max(numArmies.GetValue(), 0);
 end
