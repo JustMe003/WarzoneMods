43c57eb173b0bcb08e19eab52fac2ccf65c974e2
JustMe003 <kingoflol20033@gmail.com>
2024-09-23 12:02:13 +0200

    Update Client_GameRefresh.lua

--- Forced LD Card/Client_GameRefresh.lua
+++ Forced LD Card/Client_GameRefresh.lua
@@ -7,21 +7,20 @@ function Client_GameRefresh(game)
     if game.Us == nil or game.Game.TurnNumber < 1 then return; end
 	if game.Us.State == WL.GamePlayerState.Playing and cardData ~= nil then
         cardData = Mod.PublicGameData.CardData[getPlayerOrTeamID(game.Us)];
-    end
-
-    local c = 0;
-    for _, order in pairs(game.Orders) do
-        ---@diagnostic disable-next-line: undefined-field
-        if order.proxyType == "GameOrderCustom" and string.sub(order.Payload, 1, #"ForcedLD_") == "ForcedLD_" then
-            c = c + 1;
+        local c = 0;
+        for _, order in pairs(game.Orders) do
+            ---@diagnostic disable-next-line: undefined-field
+            if order.proxyType == "GameOrderCustom" and string.sub(order.Payload, 1, #"ForcedLD_") == "ForcedLD_" then
+                c = c + 1;
+            end
         end
-    end
-    if c < Mod.PublicGameData.CardsPlayedThisTurn[game.Us.ID] then
-        game.SendGameCustomMessage("Updating server...", {Action = "UpdateCardCount", Count = Mod.PublicGameData.CardsPlayedThisTurn[game.Us.ID] - c}, function(t) end);
-    end
-
-    if RefreshMainWindow and not UI.IsDestroyed(GlobalRoot) then
-        showMain();
+        if c < Mod.PublicGameData.CardsPlayedThisTurn[game.Us.ID] then
+            game.SendGameCustomMessage("Updating server...", {Action = "UpdateCardCount", Count = Mod.PublicGameData.CardsPlayedThisTurn[game.Us.ID] - c}, function(t) end);
+        end
+    
+        if RefreshMainWindow and not UI.IsDestroyed(GlobalRoot) then
+            showMain();
+        end
     end
 end
 
