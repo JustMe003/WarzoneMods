ff52684d334ac64ca62685be5e3f07446020bf16
JustMe003 <kingoflol20033@gmail.com>
2024-11-07 21:07:41 +0100

    new bug

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -8,31 +8,12 @@ require("UI");
 ---@param game GameClientHook
 ---@param close fun() # Zero parameter function that closes the dialog
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
-    Init(rootParent);
-    root = GetRoot();
-    colors = GetColors();
-    Game = game;
-
-    local orders = Game.Orders;
-    for i = 17, 32, 1 do
-        addOrder(orders, WL.GameOrderCustom.Create(game.Us.ID, "Occurs in phase: " .. i, "", {}, i));
-    end
-    for i = 44, 56, 1 do
-        addOrder(orders, WL.GameOrderCustom.Create(game.Us.ID, "Occurs in phase: " .. i, "", {}, i));
-    end
-    Game.Orders = orders;
-    close();
-end
-
-function addOrder(orders, order)
-    local index = 0;
-    for i, o in pairs(orders) do
-        if o.OccursInPhase ~= nil and o.OccursInPhase > order.OccursInPhase then
-            -- print(o.OccursInPhase, order.OccursInPhase);
-            index = i;
-            break;
-        end
-    end
-    if index == 0 then index = #orders + 1; end
-    table.insert(orders, index, order);
+    for i = 0, game.Game.TurnNumber - 1 do
+		for j = 0, game.Game.TurnNumber - 2 do
+			game.GetStanding(i, j, function(t) 
+				print("i: " .. i, "j: " .. j, t.Territories[1].NumArmies.NumArmies);
+			end);
+		end
+		print();
+	end 
 end
