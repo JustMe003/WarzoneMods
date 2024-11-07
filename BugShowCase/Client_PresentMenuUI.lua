258af8d7c6950d35321379e6276b894081348329
JustMe003 <kingoflol20033@gmail.com>
2024-11-07 21:17:57 +0100

    Update Client_PresentMenuUI.lua

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -1,6 +1,6 @@
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
     for i = 0, game.Game.TurnNumber - 1 do
-		for j = 0, game.Game.TurnNumber - 1 do
+		for j = 0, game.Game.TurnNumber - 2 do
 			game.GetStanding(i, j, function(t) 
 				print("i: " .. i, "j: " .. j, t.Territories[1].NumArmies.NumArmies);
 			end);
