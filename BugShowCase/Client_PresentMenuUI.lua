e4cfc3308355a8f90996b70ef315d74c19151f44
JustMe003 <kingoflol20033@gmail.com>
2024-11-07 21:09:06 +0100

    .

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -1,12 +1,3 @@
-require("Annotations");
-require("UI");
-
----Client_PresentMenuUI hook
----@param rootParent RootParent
----@param setMaxSize fun(width: number, height: number) # Sets the max size of the dialog
----@param setScrollable fun(horizontallyScrollable: boolean, verticallyScrollable: boolean) # Set whether the dialog is scrollable both horizontal and vertically
----@param game GameClientHook
----@param close fun() # Zero parameter function that closes the dialog
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
     for i = 0, game.Game.TurnNumber - 1 do
 		for j = 0, game.Game.TurnNumber - 2 do
