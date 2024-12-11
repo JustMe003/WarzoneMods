17c184a3713bf8f8683d9e29313a1069b0530dbb
JustMe003 <kingoflol20033@gmail.com>
2024-12-11 02:14:26 +0100

    Updated LD Helper to 3.0

--- Local_Deployment_Helper/Client_GameRefresh.lua
+++ Local_Deployment_Helper/Client_GameRefresh.lua
@@ -1,14 +1,24 @@
 require("Client_PresentMenuUI");
 require("UI");
+
+-- This is sometimes called BEFORE Warzone has finished processing the whole turn
+-- Resulting that when getting the last turn orders, the list is empty
+
 function Client_GameRefresh(game)
-    local hasAddedOrders = false;
-    if game.Us == nil or game.Us.IsAIOrHumanTurnedIntoAI then
+    local addedOrders = false;
+    if game.Us == nil or game.Us.IsAIOrHumanTurnedIntoAI or game.Us.State ~= WL.GamePlayerState.Playing then
         return;
     end
 
-	if (lastTurn == nil or lastTurn < game.Game.TurnNumber) and game.Game.TurnNumber > 1 then
-        if Mod.PlayerGameData.NewTurnAction == nil then
+    if Mod.PublicGameData.ModVersion == nil or Mod.PublicGameData.ModVersion < "3.0" then
+        game.SendGameCustomMessage("Updating mod...", { Type = "UpdateVersion" }, function(t)  end);
+        return;
+    end
+
+    if (lastTurn == nil or lastTurn < game.Game.TurnNumber) and game.Game.TurnNumber > 1 then
+        if Mod.PlayerGameData.NewTurnAction == nil and (UI.IsDestroyed == nil or UI.IsDestroyed(vert)) then
             game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "SetAction"); end);
+            game.SendGameCustomMessage("Updating mod...", {Type = "PickOption", DoNothing = true}, function(t) end);
         else
             print("orderlist is empty: " .. tostring(tableIsEmpty(game.Orders)));
             if tableIsEmpty(game.Orders) then
@@ -16,15 +26,15 @@ function Client_GameRefresh(game)
                     game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "SetDefaultOptions"); end);
                 elseif Mod.PlayerGameData.NewTurnAction == "AutoDeploy" then
                     game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "AutoDeploy"); end);
-                    hasAddedOrders = true;
+                    addedOrders = true;
                 elseif Mod.PlayerGameData.NewTurnAction == "ShowWindow" then
                     game.CreateDialog(function(a, b, c, d, e) Client_PresentMenuUI(a, b, c, d, e, "ShowWindow"); end);
-                    hasAddedOrders = true;
+                    addedOrders = true;
                 end
             end
         end
     end
-    if hasAddedOrders then
+    if addedOrders then
         lastTurn = game.Game.TurnNumber;
     end
 end
--- Local_Deployment_Helper/Server_GameCustomMessage.lua
+++ Local_Deployment_Helper/Server_GameCustomMessage.lua
@@ -5,6 +5,8 @@ function Server_GameCustomMessage(game, playerID, payload, setReturn)
         saveInputs(playerID, payload);
     elseif payload.Type == "PickOption" then
         saveDefaultOption(playerID, payload);
+    elseif payload.Type == "UpdateVersion" then
+        updateVersion(game);
     end
     Mod.PlayerGameData = playerData;
 end
@@ -22,4 +24,54 @@ function saveDefaultOption(playerID, payload)
     if payload.DoNothing then playerData[playerID].NewTurnAction = "DoNothing"; end
     if payload.ShowWindow then playerData[playerID].NewTurnAction = "ShowWindow"; end
     if payload.AutoDeploy then playerData[playerID].NewTurnAction = "AutoDeploy"; end
+end
+
+function updateVersion(game)
+    if Mod.PublicGameData.ModVersion == nil or Mod.PublicGameData.ModVersion < "3.0" then
+        local data = Mod.PublicGameData;
+        data.TerritoryToBonusMap = createTerritoryToBonusMap(game, game.Map.Territories);
+        data.ModVersion = "3.0";
+        Mod.PublicGameData = data;
+
+        for p, t in pairs(playerData) do
+            if t ~= nil then
+                print(p);
+                if t.SavedInputs ~= nil then
+                    t.SavedInputs.AddAttacks = nil;
+                    t.SavedInputs.MoveUnmovedArmies = false;
+                    t.SavedInputs.MoveSpecialUnits = false;
+                end
+            end
+        end
+    end
+end
+
+function createTerritoryToBonusMap(game, territories)
+	local t = {};
+	for terrID, terr in pairs(territories) do
+		t[terrID] = getSmallestBonusID(game, terr);
+	end
+	return t;
+end
+
+function getBonusValue(game, bonusID)
+	if game.Settings.OverriddenBonuses[bonusID] ~= nil then
+		return game.Settings.OverriddenBonuses[bonusID];
+	else
+		return game.Map.Bonuses[bonusID].Amount;
+	end
+end
+
+function getSmallestBonusID(game, terr)
+	local terrCount = 10000;
+	local smallestBonusID = 0;
+	for _, bonusID in pairs(terr.PartOfBonuses) do
+		if getBonusValue(game, bonusID) ~= 0 then
+			if #game.Map.Bonuses[bonusID].Territories < terrCount then
+				terrCount = #game.Map.Bonuses[bonusID].Territories;
+				smallestBonusID = bonusID;
+			end
+		end
+	end
+	return smallestBonusID;
 end
