30a4f7b55b061edb302ca8d26e35a75cf777c163
JustMe003 <kingoflol20033@gmail.com>
2024-12-19 20:35:22 +0100

    Update Client_PresentSettingsUI.lua

--- Airbases/Client_PresentSettingsUI.lua
+++ Airbases/Client_PresentSettingsUI.lua
@@ -1,6 +1,4 @@
 function Client_PresentSettingsUI(rootParent)
     local vert = UI.CreateVerticalLayoutGroup(rootParent);
     UI.CreateLabel(vert).SetText("Number of cities needed to airlift: " .. tostring(Mod.Settings.RequiredCities)).SetColor("#DDDDDD");
-    UI.CreateEmpty(vert).SetPreferredHeight(5);
-    UI.CreateLabel(vert).SetText("Note that both sending and receiving territiries need to have this amount of cities. When either or both territories has not enough cities, the order is cancelled").SetColor("#DDDDDD");
 end
