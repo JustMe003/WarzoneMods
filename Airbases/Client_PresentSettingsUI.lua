8597e87fb2f58a209bc6faa6b5d18d3bc1df7747
JustMe003 <kingoflol20033@gmail.com>
2024-12-19 20:36:00 +0100

    Update Client_PresentSettingsUI.lua

--- Airbases/Client_PresentSettingsUI.lua
+++ Airbases/Client_PresentSettingsUI.lua
@@ -1,4 +1,4 @@
 function Client_PresentSettingsUI(rootParent)
     local vert = UI.CreateVerticalLayoutGroup(rootParent);
-    UI.CreateLabel(vert).SetText("Number of cities needed to airlift: " .. tostring(Mod.Settings.RequiredCities)).SetColor("#DDDDDD");
+    UI.CreateLabel(vert).SetText("Number of cities required to airlift: " .. tostring(Mod.Settings.RequiredCities)).SetColor("#DDDDDD");
 end
