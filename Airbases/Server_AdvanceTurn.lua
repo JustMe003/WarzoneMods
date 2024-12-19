a920bdf3f552019fbed7264edfde5a7fd02dabb8
JustMe003 <kingoflol20033@gmail.com>
2024-12-19 20:29:47 +0100

    Added present settings hook and added custom info order

--- Airbases/Server_AdvanceTurn.lua
+++ Airbases/Server_AdvanceTurn.lua
@@ -1,7 +1,8 @@
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
     if order.proxyType == "GameOrderPlayCardAirlift" then
         if not hasRequiredCities(game.ServerGame.LatestTurnStanding.Territories[order.ToTerritoryID]) or not hasRequiredCities(game.ServerGame.LatestTurnStanding.Territories[order.FromTerritoryID]) then
-            skipThisOrder(WL.ModOrderControl.Skip);
+            skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+            addNewOrder(WL.GameOrderCustom.Create(order.PlayerID, "Your airlift order was skipped because " .. game.Map.Territories[order.FromTerritoryID].Name .. " or " .. game.Map.Territories[order.ToTerritoryID].Name .. " has less than " .. tostring(Mod.Settings.RequiredCities) .. " cities"))
         end
     end
 end
