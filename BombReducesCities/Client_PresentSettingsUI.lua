aeae9e1b5c68f598807639213c3fdc127a43de8e
JustMe003 <kingoflol20033@gmail.com>
2025-01-06 20:55:19 +0100

    Update Client_PresentSettingsUI.lua

--- BombReducesCities/Client_PresentSettingsUI.lua
+++ BombReducesCities/Client_PresentSettingsUI.lua
@@ -1,16 +1,6 @@
 function Client_PresentSettingsUI(rootParent)
     local par = UI.CreateHorizontalLayoutGroup(rootParent);
     UI.CreateLabel(par)
-        .SetText("The number of cities that are removed when a territory is hit with a bomb")
+        .SetText("When a territory is hit by a bomb card, " .. Mod.Settings.NumCities .. " cities will be removed from that territory")
         .SetColor("#DDDDDD");
-
-    UI.CreateEmpty(par)
-        .SetPreferredWidth(10);
-        
-        UI.CreateLabel(par)
-        .SetText(Mod.Settings.NumCities)
-        .SetColor("#0077FF");
-
-    UI.CreateEmpty(par)
-        .SetPreferredWidth(40);
 end
