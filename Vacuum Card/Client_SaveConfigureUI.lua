2ee7dc0792ee4bdd404bf998a02bc1a2e2757fe0
JustMe003 <kingoflol20033@gmail.com>
2025-01-20 19:33:14 +0100

    Update Client_SaveConfigureUI.lua

--- Vacuum Card/Client_SaveConfigureUI.lua
+++ Vacuum Card/Client_SaveConfigureUI.lua
@@ -1,5 +1,5 @@
 function Client_SaveConfigureUI(alert, customCard)
     customCard("Vacuum", "Play on a territory. Every connected territory will make an attack/transfer order to this territory, regardless of the players controlling them", "Annexation130x180.png", 1, 1, 3, 1);
-    customCard("Second vacuum", "Same card as the first, this is for testing purposes.\n\nEvery connected territory will make an attack/transfer order to this territory, regardless of the players controlling them", "Annexation130x180.png", 1, 1, 3, 1);
+    -- customCard("Second vacuum", "Same card as the first, this is for testing purposes.\n\nEvery connected territory will make an attack/transfer order to this territory, regardless of the players controlling them", "Annexation130x180.png", 1, 1, 3, 1);
 end
 
