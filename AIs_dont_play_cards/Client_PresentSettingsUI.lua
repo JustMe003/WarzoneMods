17a86c52c36807bda8f07cbfafad675b6bd6b7dc
JustMe003 <kingoflol20033@gmail.com>
2025-01-30 15:07:54 +0100

    Update Client_PresentSettingsUI.lua

--- AIs_dont_play_cards/Client_PresentSettingsUI.lua
+++ AIs_dont_play_cards/Client_PresentSettingsUI.lua
@@ -1,9 +1,39 @@
 
 function Client_PresentSettingsUI(rootParent)
-	UI.CreateLabel(rootParent).SetText("AI's can play emergency blockade cards: " .. tostring(Mod.Settings.CanPlayEMB));
-	UI.CreateLabel(rootParent).SetText("AI's can play diplomacy cards: " .. tostring(Mod.Settings.CanPlayDiplomacy));
-	UI.CreateLabel(rootParent).SetText("AI's can play sanctions cards: " .. tostring(Mod.Settings.CanPlaySanctions));
-	UI.CreateLabel(rootParent).SetText("AI's can play blockade cards: " .. tostring(Mod.Settings.CanPlayBlockade));
-	UI.CreateLabel(rootParent).SetText("AI's can play bomb cards: " .. tostring(Mod.Settings.CanPlayBomb));
+	local root = UI.CreateVerticalLayoutGroup(rootParent);
+	local line;
+
+	line = UI.CreateHorizontalLayoutGroup(root).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("AI's can play emergency blockade cards: ");
+	createBoolLabel(line, Mod.Settings.CanPlayEMB);
+	
+	line = UI.CreateHorizontalLayoutGroup(root).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("AI's can play diplomacy cards: ");
+	createBoolLabel(line, Mod.Settings.CanPlayDiplomacy);
+	
+	line = UI.CreateHorizontalLayoutGroup(root).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("AI's can play sanctions cards: ");
+	createBoolLabel(line, Mod.Settings.CanPlaySanctions);
+	
+	line = UI.CreateHorizontalLayoutGroup(root).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("AI's can play blockade cards: ");
+	createBoolLabel(line, Mod.Settings.CanPlayBlockade);
+	
+	line = UI.CreateHorizontalLayoutGroup(root).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("AI's can play bomb cards: ");
+	createBoolLabel(line, Mod.Settings.CanPlayBomb);
 end
 
+function createBoolLabel(line, b)
+	UI.CreateLabel(line).SetText(getYesOrNo(b)).SetColor(getBoolColor(b));
+end
+
+function getYesOrNo(b)
+	if b then return "Yes"; end
+	return "No";
+end
+
+function getBoolColor(b)
+	if b then return "#00FF00"; end
+	return "#FF0000";
+end
