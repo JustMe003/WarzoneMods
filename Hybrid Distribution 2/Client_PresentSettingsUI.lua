150da92fd7fae07ae701f8325c16f1b8fcf5217d
JustMe003 <kingoflol20033@gmail.com>
2025-02-04 16:19:49 +0100

    Update Client_PresentSettingsUI.lua

--- Hybrid Distribution 2/Client_PresentSettingsUI.lua
+++ Hybrid Distribution 2/Client_PresentSettingsUI.lua
@@ -1,7 +1,33 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 
-	UI.CreateLabel(vert).SetText("Number of territories to auto distribute to each player: " .. Mod.Settings.NumTerritories);
-	UI.CreateLabel(vert).SetText("Auto distributed territories are chosen from territories in distribution: " .. tostring(Mod.Settings.takeDistributionTerr))
-	UI.CreateLabel(vert).SetText("Number of armies on the auto distributed territories is the same as those in the manual distribution: " .. tostring(Mod.Settings.setArmiesToInDistribution))
+	local line = UI.CreateHorizontalLayoutGroup(vert).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("Number of distributed territories: ").SetColor("#DDDDDD");
+	UI.CreateLabel(line).SetText(Mod.Settings.NumTerritories).SetColor("#FFE5B4");
+	
+	line = UI.CreateHorizontalLayoutGroup(vert).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("Distribute territories in distribution: ").SetColor("#DDDDDD");
+	createBoolLabel(line, Mod.Settings.takeDistributionTerr);
+	
+	line = UI.CreateHorizontalLayoutGroup(vert).SetFlexibleWidth(1);
+	UI.CreateLabel(line).SetText("Number of armies on the auto distributed territories is the same as those in the manual distribution: ")
+	createBoolLabel(line, Mod.Settings.setArmiesToInDistribution);
+end
+
+function createBoolLabel(line, b)
+	UI.CreateLabel(line).SetText(getBoolText(b)).SetColor(getBoolColor(b));
+end
+
+function getBoolText(b)
+	if b then 
+		return "Yes";
+	end
+	return "No";
+end
+
+function getBoolColor(b)
+	if b then
+		return "#00FF00";
+	end
+	return "#FF0000";
 end
