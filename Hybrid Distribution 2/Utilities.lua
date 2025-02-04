6e765e769bed6fdb5674f2f36e63ddd836d2dde6
JustMe003 <kingoflol20033@gmail.com>
2025-02-04 15:48:51 +0100

    Update Utilities.lua

--- Hybrid Distribution 2/Utilities.lua
+++ Hybrid Distribution 2/Utilities.lua
@@ -6,6 +6,7 @@ function NewIdentity()
 	Mod.PublicGameData = data;
 	return ret;
 end
+
 function Dump(obj)
 	if obj.proxyType ~= nil then
 		DumpProxy(obj);
@@ -15,11 +16,13 @@ function Dump(obj)
 		print('Dump ' .. type(obj));
 	end
 end
+
 function DumpTable(tbl)
     for k,v in pairs(tbl) do
         print('k = ' .. tostring(k) .. ' (' .. type(k) .. ') ' .. ' v = ' .. tostring(v) .. ' (' .. type(v) .. ')');
     end
 end
+
 function DumpProxy(obj)
 
     print('type=' .. obj.proxyType .. ' readOnly=' .. tostring(obj.readonly) .. ' readableKeys=' .. table.concat(obj.readableKeys, ',') .. ' writableKeys=' .. table.concat(obj.writableKeys, ','));
