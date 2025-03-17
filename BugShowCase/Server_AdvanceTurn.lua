a1f6ea4688a49020f74b41f86752c8d6dd995a56
JustMe003 <kingoflol20033@gmail.com>
2025-03-17 12:55:42 +0100

    Update Server_AdvanceTurn.lua

--- BugShowCase/Server_AdvanceTurn.lua
+++ BugShowCase/Server_AdvanceTurn.lua
@@ -6,13 +6,13 @@ local WL = WL;
 ---@param addNewOrder fun(order: GameOrder) # Adds a game order, will be processed before any of the rest of the orders 
 function Server_AdvanceTurn_End(game, addNewOrder)
     for pID, _ in pairs(game.Game.PlayingPlayers) do
-        local custom = WL.GameOrderCustom.Create(pID, "Adding resources (custom)", "", {
+        local custom = WL.GameOrderCustom.Create(pID, "Adding +10 resources (custom order)", "", {
             [2] = 10,
             [5] = 10
         });
         addNewOrder(custom);
 
-        local event = WL.GameOrderEvent.Create(pID, "Adding resources (event)");
+        local event = WL.GameOrderEvent.Create(pID, "Adding +1 resource (event order)", {});
         event.AddResourceOpt = {
             [pID] = {
                 [2] = 1,
