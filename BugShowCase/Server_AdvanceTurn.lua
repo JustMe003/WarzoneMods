fee1d25cee7df9f0eb4b4d013b7e801386587ad4
JustMe003 <kingoflol20033@gmail.com>
2025-03-17 12:53:55 +0100

    .

--- BugShowCase/Server_AdvanceTurn.lua
+++ BugShowCase/Server_AdvanceTurn.lua
@@ -7,15 +7,15 @@ local WL = WL;
 function Server_AdvanceTurn_End(game, addNewOrder)
     for pID, _ in pairs(game.Game.PlayingPlayers) do
         local custom = WL.GameOrderCustom.Create(pID, "Adding resources (custom)", "", {
-            [2] = -1,
-            [5] = -3
+            [2] = 10,
+            [5] = 10
         });
         addNewOrder(custom);
 
         local event = WL.GameOrderEvent.Create(pID, "Adding resources (event)");
         event.AddResourceOpt = {
             [pID] = {
-                [2] = -2,
+                [2] = 1,
                 [5] = 1
             }
         }
--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -5,13 +5,8 @@ function Server_StartGame(game, standing)
     local res = standing.Resources or {};
 	for pID, _ in pairs(game.Game.PlayingPlayers) do
         res[pID] = res[pID] or {};
-        res[pID][2] = 10;
+        res[pID][2] = 100;
         res[pID][5] = 100;
     end
     standing.Resources = res;
-
-    for pID, _ in pairs(game.Game.PlayingPlayers) do
-        print(2, standing.NumResources(pID, 2));
-        print(5, standing.NumResources(pID, 5));
-    end
 end
