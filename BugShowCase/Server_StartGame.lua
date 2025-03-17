4a4cf683e165c16b2b4311f7b639fd9a7e74b335
JustMe003 <kingoflol20033@gmail.com>
2025-03-17 12:51:42 +0100

    Update Server_StartGame.lua

--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -2,7 +2,7 @@
 ---@param game GameServerHook
 ---@param standing GameStanding
 function Server_StartGame(game, standing)
-    local res = standing.Resources;
+    local res = standing.Resources or {};
 	for pID, _ in pairs(game.Game.PlayingPlayers) do
         res[pID] = res[pID] or {};
         res[pID][2] = 10;
