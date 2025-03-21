d1ed27e16ce116e19e7e8774d3fe864fb538f9e7
JustMe003 <kingoflol20033@gmail.com>
2025-03-21 14:22:01 +0100

    Update Server_AdvanceTurn.lua

--- BugShowCase/Server_AdvanceTurn.lua
+++ BugShowCase/Server_AdvanceTurn.lua
@@ -7,16 +7,16 @@ local WL = WL;
 function Server_AdvanceTurn_End(game, addNewOrder)
     for pID, _ in pairs(game.Game.PlayingPlayers) do
         local custom = WL.GameOrderCustom.Create(pID, "Adding +10 resources (custom order)", "", {
-            [2] = 10,
-            [5] = 10
+            [2] = -1,
+            [5] = -1
         });
         addNewOrder(custom);
 
         local event = WL.GameOrderEvent.Create(pID, "Adding +1 resource (event order)", {});
         event.AddResourceOpt = {
             [pID] = {
-                [2] = 1,
-                [5] = 1
+                [2] = 10,
+                [5] = 10
             }
         }
         addNewOrder(event);
