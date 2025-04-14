eb166ca8099af4959aac66d0ed7c1e78464b146b
JustMe003 <kingoflol20033@gmail.com>
2025-04-14 19:54:14 +0200

    Added version label to settings page

--- Factions 2/Client_PresentSettingsUI.lua
+++ Factions 2/Client_PresentSettingsUI.lua
@@ -3,13 +3,20 @@ require("utilities")
 
 function Client_PresentSettingsUI(rootParent)
 	Init();
-	GlobalRoot = CreateVert(rootParent).SetFlexibleWidth(1).SetCenter(true);
 	colors = GetColors();
-
-    if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
+	local vert = CreateVert(rootParent).SetCenter(true).SetFlexibleWidth(1);
+	GlobalRoot = CreateVert(vert).SetCenter(true).SetFlexibleWidth(1);
+	
+	local line = CreateHorz(vert).SetFlexibleWidth(1);
+	CreateEmpty(line).SetFlexibleWidth(1);
+	CreateLabel(line).SetText("Version: ");
+	if Mod.Settings.VersionNumber == nil or Mod.Settings.VersionNumber == 1 then
 		require("Client_PresentSettingsUI1");
+		CreateLabel(line).SetText(MOD_VERSION_1).SetColor(colors.TextColor);
 	else
 		require("Client_PresentSettingsUI2");
+		CreateLabel(line).SetText(MOD_VERSION_2).SetColor(colors.TextColor);
 	end
+
     Client_PresentSettingsUIMain();
 end
