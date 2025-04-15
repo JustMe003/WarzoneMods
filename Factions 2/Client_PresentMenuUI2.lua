a61007a00eaa59137312af2f67d7244eb4a7ba30
JustMe003 <kingoflol20033@gmail.com>
2025-04-15 22:27:35 +0200

    Update Client_PresentMenuUI2.lua

--- Factions 2/Client_PresentMenuUI2.lua
+++ Factions 2/Client_PresentMenuUI2.lua
@@ -442,8 +442,11 @@ function showFactionDetails(factionName)
 								showFactionDetails(factionName); 
 							end)
 						end).SetInteractable(not (faction.Offers and faction.Offers[otherFaction]));
-						print(faction.Offers)
+						print(faction.Offers, otherFaction);
 						print(faction.Offers[otherFaction]);
+						for i, v in pairs(faction.Offers) do
+							print(i, v);
+						end
 					else
 						but.SetText("Declare war").SetColor(colors.Red).SetOnClick(function()
 							confirmChoice("Are you sure you want to declare war on " .. name .. "? All your faction members will be forced to declare war on all of the players in " .. name, function() 
