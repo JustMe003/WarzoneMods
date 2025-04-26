0c5f6f931130858d8aba438bceedda8d9560b5cf
JustMe003 <kingoflol20033@gmail.com>
2025-04-26 23:24:57 +0200

    Create Client_GameOrderCreated.lua

---
+++ Local_Deployment_Helper/Client_GameOrderCreated.lua
@@ -0,0 +1,15 @@
+local payload = "[LDH_V3]";
+
+function Client_GameOrderCreated(game, order, skipOrder)
+    local orders = game.Orders;
+    local index = 0;
+    while index <= #orders do
+        print(index, #orders);
+        local o = orders[index];
+        if o.proxyType == "GameOrderCustom" and o.Payload == payload then
+            table.remove(orders, index);
+        else
+            index = index + 1;
+        end
+    end
+end
