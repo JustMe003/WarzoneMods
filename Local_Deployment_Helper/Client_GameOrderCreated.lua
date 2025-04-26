e0c3ac1bc4ee236ed3843624a88fe3268b4b7adc
JustMe003 <kingoflol20033@gmail.com>
2025-04-26 23:26:19 +0200

    Update Client_GameOrderCreated.lua

--- Local_Deployment_Helper/Client_GameOrderCreated.lua
+++ Local_Deployment_Helper/Client_GameOrderCreated.lua
@@ -6,10 +6,12 @@ function Client_GameOrderCreated(game, order, skipOrder)
     while index <= #orders do
         print(index, #orders);
         local o = orders[index];
+        if o.OccursInPhase and o.OccursInPhase > WL.TurnPhase.Purchase then break; end 
         if o.proxyType == "GameOrderCustom" and o.Payload == payload then
             table.remove(orders, index);
         else
             index = index + 1;
         end
     end
+    game.Orders = orders;
 end
