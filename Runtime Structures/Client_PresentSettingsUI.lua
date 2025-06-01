0519ca8d15af4c5d342e515a850f0f4a7e8331db
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2025-06-01 11:43:28 +0200

    Update Client_PresentSettingsUI.lua

--- Runtime Structures/Client_PresentSettingsUI.lua
+++ Runtime Structures/Client_PresentSettingsUI.lua
@@ -7,7 +7,7 @@ function Client_PresentSettingsUI(rootParent)
     Init();
     local textColor = GetColors().TextColor;
     local root = CreateVert(rootParent).SetCenter(true);
-    CreateLabel(root).SetText("Note that structures can only be added to neutral territories").SetColor(textColor);
+    CreateLabel(root).SetText("Note that structures will only be added to neutral a random neutral territory").SetColor(textColor);
     for _, group in ipairs(Mod.Settings.Config) do
         CreateLabel(root).SetText(getGroupText(group)).SetColor(textColor);
     end
