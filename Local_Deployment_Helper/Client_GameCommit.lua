090ad63bb9aa0842c7d31d353680f21debac848a
JustMe003 <kingoflol20033@gmail.com>
2025-07-16 01:05:11 +0200

    Updated new hooks

--- Local_Deployment_Helper/Client_GameCommit.lua
+++ Local_Deployment_Helper/Client_GameCommit.lua
@@ -1,6 +1,6 @@
 local payload = "[LDH_V3]";
 
-function Client_GameOrderCreated(game, order, skipOrder)
+function Client_GameCommit(game, skipCommit)
     local orders = game.Orders;
     local index = #orders;
     while index > 0 do
@@ -8,8 +8,9 @@ function Client_GameOrderCreated(game, order, skipOrder)
         if o.OccursInPhase and o.OccursInPhase < WL.TurnPhase.ReceiveCards then break; end 
         if o.proxyType == "GameOrderCustom" and o.Payload == payload then
             table.remove(orders, index);
+            break;
         end
         index = index - 1;
     end
-    game.Orders = orders;
+    if #game.Orders ~= #orders then game.Orders = orders; end
 end
