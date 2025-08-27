38c5cd9175518741f4795dc14f61656e3b3d9835
JustMe003 <kingoflol20033@gmail.com>
2025-08-27 12:00:37 +0200

    Added support for just builded cities

--- Airbases/Client_GameOrderCreated.lua
+++ Airbases/Client_GameOrderCreated.lua
@@ -1,19 +1,29 @@
 function Client_GameOrderCreated(game, order, skipOrder)
-    for i, v in pairs(order) do
-        print(i, v);
-    end
     if order.proxyType == "GameOrderPlayCardAirlift" and Mod.Settings.RequiredCities > 0 then
-        if testForNumberOfCities(game, order, game.LatestStanding.Territories[order.FromTerritoryID].Structures, skipOrder) then
-            testForNumberOfCities(game, order, game.LatestStanding.Territories[order.ToTerritoryID].Structures, skipOrder)
+        if testForNumberOfCities(game, order, order.FromTerritoryID, skipOrder) then
+            testForNumberOfCities(game, order, order.ToTerritoryID, skipOrder)
         end
     end
 end
 
-function testForNumberOfCities(game, order, structures, skipOrder)
-    if structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] >= Mod.Settings.RequiredCities then
+function testForNumberOfCities(game, order, terrID, skipOrder)
+    local structures = game.LatestStanding.Territories[terrID].Structures;
+    if structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] + getPurchasedCities(game.Orders, terrID) >= Mod.Settings.RequiredCities then
         UI.Alert("You cannot play this airlift because '" .. game.Map.Territories[order.FromTerritoryID].Name .. "' and/or '" .. game.Map.Territories[order.ToTerritoryID].Name .. "' don't have the number of required cities. When playing an airlift card, both territories need to have at least " .. Mod.Settings.RequiredCities .. " cities");
         skipOrder();
         return false;
     end
     return true;
+end
+
+function getPurchasedCities(orders, terrID)
+    for _, order in pairs(orders) do
+        if order.proxyType == "GameOrderPurchase" then
+            print(order.BuildCities[terrID] or 0);
+            return order.BuildCities[terrID] or 0;
+        elseif order.OccursInPhase > WL.TurnPhase.Purchase then
+            return 0;
+        end
+    end
+    return 0;
 end
