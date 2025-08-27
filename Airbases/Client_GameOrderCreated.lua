d62f9d9c0756a0bcb338159e64d9992129798457
JustMe003 <kingoflol20033@gmail.com>
2025-08-27 12:05:12 +0200

    Update Client_GameOrderCreated.lua

--- Airbases/Client_GameOrderCreated.lua
+++ Airbases/Client_GameOrderCreated.lua
@@ -8,8 +8,7 @@ end
 
 function testForNumberOfCities(game, order, terrID, skipOrder)
     local structures = game.LatestStanding.Territories[terrID].Structures;
-    if structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] + getPurchasedCities(game.Orders, terrID) >= Mod.Settings.RequiredCities then
-        print(structures[WL.StructureType.City] + getPurchasedCities(game.Orders, terrID));
+    if not (structures and structures[WL.StructureType.City] and structures[WL.StructureType.City] + getPurchasedCities(game.Orders, terrID) >= Mod.Settings.RequiredCities) then
         UI.Alert("You cannot play this airlift because '" .. game.Map.Territories[order.FromTerritoryID].Name .. "' and/or '" .. game.Map.Territories[order.ToTerritoryID].Name .. "' don't have the number of required cities. When playing an airlift card, both territories need to have at least " .. Mod.Settings.RequiredCities .. " cities");
         skipOrder();
         return false;
