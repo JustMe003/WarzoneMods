cab280554d558c84014a59a19d14bba86a0d46d2
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-09-04 00:02:06 +0200

    Update RemoveArmies.lua

--- Meteor Strike Plus/RemoveArmies.lua
+++ Meteor Strike Plus/RemoveArmies.lua
@@ -91,13 +91,13 @@ end
 function getRemainingHealth(armies, damage)
 	damage = damage - armies.NumArmies;
 	for _, sp in ipairs(armies.SpecialUnits) do
-		if damage < 0 then return damage; end
+		if damage < 0 then return -damage; end
 		damage = damage - getHealth(sp);
 		if sp.proxyType == "CustomSpecialUnit" and not unitHasHealth(sp) and sp.DamageAbsorbedWhenAttacked ~= nil then
 			damage = damage - sp.DamageAbsorbedWhenAttacked;
 		end
 	end
-	return damage;
+	return -damage;
 end
 
 --	Clone the passed special unit (only if it has [Health] instead of [DamageAbsorbedWhenAttacked])
