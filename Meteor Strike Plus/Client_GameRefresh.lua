8ddefb649873f8fa79bfb39d236c1504562db5ac
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-09-18 12:55:52 +0200

    Update Client_GameRefresh.lua

--- Meteor Strike Plus/Client_GameRefresh.lua
+++ Meteor Strike Plus/Client_GameRefresh.lua
@@ -2,7 +2,7 @@ require("Client_PresentMenuUI")
 
 function Client_GameRefresh(game)
 	if game.Us ~= nil then
-        if UI == nil then print("UI == nil!"); return; end
+        if UI == nil or UI.IsDestroyed() == nil then print("UI.IsDestroyed == nil!"); return; end
         if TurnNumber ~= nil and TurnNumber ~= game.Game.TurnNumber and not UI.IsDestroyed(root) then
             Close();
             game.CreateDialog(Client_PresentMenuUI);
