16199dd98b8fa966a6aae699b22488b4ab213a00
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-09-18 11:47:23 +0200

    Update Client_GameOrderCreated.lua

--- BugShowCase/Client_GameOrderCreated.lua
+++ BugShowCase/Client_GameOrderCreated.lua
@@ -1,4 +1,7 @@
 function Client_GameOrderCreated(game, order, skipOrder)
     print("UI table: " .. tostring(UI));
+    for i, v in pairs(UI) do
+        print(i, v);
+    end
     UI.IsDestroyed(SomeVar);
 end
