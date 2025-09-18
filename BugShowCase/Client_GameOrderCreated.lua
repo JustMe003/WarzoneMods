210290e59d006436cad3f26bb8fed4b79de2ea09
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-09-18 12:54:58 +0200

    Update Client_GameOrderCreated.lua

--- BugShowCase/Client_GameOrderCreated.lua
+++ BugShowCase/Client_GameOrderCreated.lua
@@ -1,7 +1,3 @@
 function Client_GameOrderCreated(game, order, skipOrder)
-    print("UI table: " .. tostring(UI));
-    for i, v in pairs(UI) do
-        print(i, v);
-    end
-    UI.Alert("This is annoying isn't it?");
+
 end
