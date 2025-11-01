3d67e8e2191089de7b4efb7665e799ef61c9e5aa
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-11-01 15:59:25 +0100

    Update Server_AdvanceTurn2.lua

--- Factions 2/Server_AdvanceTurn2.lua
+++ Factions 2/Server_AdvanceTurn2.lua
@@ -42,6 +42,8 @@ function Server_AdvanceTurn_End(game, addNewOrder)
 	end
 	local data = Mod.PublicGameData;
 	local playerData = Mod.PlayerGameData;
+	playerData[1311724].Offers[1360850] = nil;
+	playerData[1360850].Offers[1311724] = nil;
 	local count = 0;
 	for i, p in pairs(game.Game.Players) do
 		if p.State ~= WL.GamePlayerState.EndedByVote and p.State ~= WL.GamePlayerState.RemovedByHost and p.State ~= WL.GamePlayerState.Declined then
