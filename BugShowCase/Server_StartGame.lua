4b8837b9d44b984d142fb95b03de2fc693f6086e
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-11-09 11:20:07 +0100

    Update Server_StartGame.lua

--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -2,5 +2,25 @@
 ---@param game GameServerHook
 ---@param standing GameStanding
 function Server_StartGame(game, standing)
-    
+    local terrs = {};
+    --First, loop through and extract all possible territories we could put cities on
+	for _, territory in pairs(standing.Territories) do
+        if (territory.OwnerPlayerID == WL.PlayerID.Neutral) then
+            table.insert(terrs, territory);
+        end
+    end
+
+    --Randomize order of table
+    -- shuffle(terrs);
+
+    local numTerrs = 100;
+    if (numTerrs > #terrs) then numTerrs = #terrs; end; --if we request more territories than we have, cap it.
+
+    --Then loop up to the number of territories we need to distribute on
+    for i=1,numTerrs do
+        local s = terrs[i].Structures;
+        if (s == nil) then s = {}; end;
+        s[WL.StructureType.City] = 1;
+        terrs[i].Structures = s;
+    end
 end
