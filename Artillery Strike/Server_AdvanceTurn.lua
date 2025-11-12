de29781cb8d4608ae674d172f0a8ee5193836f2f
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2025-11-12 17:52:41 +0100

    Updated server files

--- Artillery Strike/Server_AdvanceTurn.lua
+++ Artillery Strike/Server_AdvanceTurn.lua
@@ -2,39 +2,13 @@ require("Utilities");
 
 function Server_AdvanceTurn_Start(game, addNewOrder)
 	data = Mod.PublicGameData;
-	lastDeployorder = {};
-	customOrders = {};
-	for _, player in pairs(game.ServerGame.Game.PlayingPlayers) do
-		for _, order in pairs(game.ServerGame.ActiveTurnOrders[player.ID]) do
-			if order.proxyType == "GameOrderDeploy" then
-				lastDeployorder[player.ID] = order.DeployOn;
-			elseif order.proxyType == "GameOrderCustom" then
-				if string.find(order.Payload, "Artillery Strike") ~= nil then
-					table.insert(customOrders, order.Payload);
-					data.TotalArtilleryShots[order.PlayerID] = data.TotalArtilleryShots[order.PlayerID] + 1;
-				end
-			end
-		end
-	end
 end
 
 function Server_AdvanceTurn_Order(game, order, orderResult, skipThisOrder, addNewOrder)
-	if order.proxyType == "GameOrderDeploy" then
-		if order.DeployOn == lastDeployorder[order.PlayerID] then
-			lastDeployorder[order.PlayerID] = nil;
-			if getTableLength(lastDeployorder) == 0 then
-				armyCountChanges = {};
-				armyCountChanges[order.DeployOn] = game.ServerGame.LatestTurnStanding.Territories[order.DeployOn].NumArmies.NumArmies + order.NumArmies;
-				for _, custom in pairs(customOrders) do
-					if string.find(custom, "Artillery Strike") ~= nil then
-						local info = split(custom, "_");
-						artilleryStrike(game, addNewOrder, info[2], tonumber(info[3]), tonumber(info[4]), tonumber(info[5]));
-					end
-				end
-			end
-		end
-	elseif order.proxyType == "GameOrderCustom" then
-		if string.find(order.Payload, "Artillery Strike") ~= nil then
+	if order.proxyType == "GameOrderCustom" then
+		if string.find(order.Payload, "Artillery Strike_") ~= nil then
+			local info = split(order.Payload, "_");
+			artilleryStrike(game, addNewOrder, info[2], tonumber(info[3]), tonumber(info[4]), tonumber(info[5]));
 			skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 		end
 	end
@@ -54,39 +28,47 @@ function artilleryStrike(game, addNewOrder, artillery, terrID, from, per)
 	if artillery == "Cannon" then
 		local mod = WL.TerritoryModification.Create(terrID);
 		local armies = getNumArmies(game, terrID);
-		mod.SetArmiesTo = armies - (armies * (per / 100));
-		armyCountChanges[terrID] = mod.SetArmiesTo;
+		mod.AddArmies = -(armies * (per / 100));
 		local event = WL.GameOrderEvent.Create(game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID, game.Map.Territories[terrID].Name .. " was attacked by a cannon at " .. game.Map.Territories[from].Name .. " for " .. per .. "% damage", {}, {mod});
 		if game.Settings.CommerceGame then
 			event.AddResourceOpt = {[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID] = {[WL.ResourceType.Gold] = -(Mod.Settings.GoldCost or 0)}};
 		end
+		event.JumpToActionSpotOpt = createRectangleVM(game, terrID);
+		event.TerritoryAnnotationsOpt = {
+			[terrID] = WL.TerritoryAnnotation.Create("Artillery Strike (" .. mod.AddArmies .. ")", 5, game.Game.Players[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID].Color.IntColor);
+		};
 		addNewOrder(event);
 	elseif artillery == "Mortar" then
 		local mods = {};
+		local annotations = {};
+		local annotationColor = game.Game.Players[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID].Color.IntColor;
 		local modTarget = WL.TerritoryModification.Create(terrID);
-		modTarget.SetArmiesTo = getNumArmies(game, terrID) - round(getNumArmies(game, terrID) * ((100 - Mod.Settings.MortarDamage + per) / 100));
-		armyCountChanges[terrID] = modTarget.SetArmiesTo;
+		modTarget.AddArmies = -round(getNumArmies(game, terrID) * ((Mod.Settings.MortarDamage - per) / 100));
 		table.insert(mods, modTarget);
+		annotations[terrID] = WL.TerritoryAnnotation.Create("Artillery Strike (" .. modTarget.AddArmies .. ")", 5, annotationColor)
 		local perForCon = per / getTableLength(game.Map.Territories[terrID].ConnectedTo);
 		for i, _ in pairs(game.Map.Territories[terrID].ConnectedTo) do
 			local mod = WL.TerritoryModification.Create(i);
 			local armies = getNumArmies(game, i);
-			mod.SetArmiesTo = armies * ((100 - perForCon) / 100);
-			armyCountChanges[i] = mod.SetArmiesTo;
+			mod.AddArmies = -(armies * ((perForCon) / 100));
 			table.insert(mods, mod);
+			annotations[i] = WL.TerritoryAnnotation.Create(mod.AddArmies, 5, annotationColor);
 		end
 		local event = WL.GameOrderEvent.Create(game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID, game.Map.Territories[terrID].Name .. " was attacked by a mortar at " .. game.Map.Territories[from].Name, {}, mods);
 		if game.Settings.CommerceGame then
 			event.AddResourceOpt = {[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID] = {[WL.ResourceType.Gold] = -(Mod.Settings.GoldCost or 0)}};
 		end
+		event.TerritoryAnnotationsOpt = annotations;
+		event.JumpToActionSpotOpt = createRectangleVM(game, terrID);
 		addNewOrder(event);
 	end
+	data.TotalArtilleryShots[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID] = data.TotalArtilleryShots[game.ServerGame.LatestTurnStanding.Territories[from].OwnerPlayerID] + 1;
 end
 
 function getNumArmies(game, terrID)
-	if armyCountChanges[terrID] ~= nil then
-		return armyCountChanges[terrID];
-	else
-		return game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies;
-	end
+	return game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies;
+end
+
+function createRectangleVM(game, terrID)
+	return WL.RectangleVM.Create(game.Map.Territories[terrID].MiddlePointX, game.Map.Territories[terrID].MiddlePointY, game.Map.Territories[terrID].MiddlePointX, game.Map.Territories[terrID].MiddlePointY)
 end
