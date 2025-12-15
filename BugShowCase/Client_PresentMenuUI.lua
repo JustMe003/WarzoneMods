df555041034b6f1eb9cb32406de2df938047608b
Just_A_Dutchman_ <justadutchman123@gmail.com>
2025-12-15 16:33:23 +0100

    Update Client_PresentMenuUI.lua

--- BugShowCase/Client_PresentMenuUI.lua
+++ BugShowCase/Client_PresentMenuUI.lua
@@ -5,5 +5,12 @@
 ---@param game GameClientHook
 ---@param close fun()
 function Client_PresentMenuUI(rootParent, setMaxSize, setScrollable, game, close)
+    local root = UI.CreateVerticalLayoutGroup(rootParent).SetFlexibleWidth(1);
     
+    for turn, orders in pairs(Mod.PublicGameData.Logs) do
+        UI.CreateLabel(root).SetText("Turn " .. turn).SetColor("#DDDDDD");
+        for _, order in pairs(orders) do
+            UI.CreateLabel(root).SetText(order);
+        end
+    end
 end
