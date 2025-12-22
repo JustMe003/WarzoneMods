7e62d5d23b768f0de6862bf3661d932a3f9c148e
Just_A_Dutchman_ <justadutchman123@gmail.com>
2025-12-22 23:05:39 +0100

    Update Server_AdvanceTurn.lua

--- BugShowCase/Server_AdvanceTurn.lua
+++ BugShowCase/Server_AdvanceTurn.lua
@@ -7,7 +7,7 @@ function Server_AdvanceTurn_Start(game, addNewOrder)
         table.insert(terrs, id);
     end
 
-    local fogMod = WL.FogMod.Create("Giving you vision!", WL.StandingFogLevel.Visible, terrs, Mod.PublicGameData.Players or {});
+    local fogMod = WL.FogMod.Create("Giving you vision!", 10000, WL.StandingFogLevel.Visible, terrs, Mod.PublicGameData.Players or {});
     local event = WL.GameOrderEvent.Create(WL.PlayerID.Neutral, "Vision!", nil, {});
     event.FogModsOpt = {fogMod};
     addNewOrder(fogMod);
