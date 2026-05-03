3edfd0172b196d43d2e4aa37043acaf370e44a28
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2026-05-03 23:21:48 +0200

    Update Server_StartGame.lua

--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -4,5 +4,17 @@
 function Server_StartGame(game, standing)
     local cards = standing.Cards;
 
+    for pID, playerCards in pairs(cards) do
+        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
+    end
+
     standing.Cards = cards;
+end
+
+function tableSize(t)
+    local c = 0;
+    for _, _ in pairs(t) do
+        c = c + 1;
+    end
+    return c;
 end
