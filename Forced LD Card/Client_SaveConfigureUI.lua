5b3ccb3bfec27b5d0470e0a8fc067fbae5bbdd9c
Just_A_Dutchman_ <51970740+JustMe003@users.noreply.github.com>
2026-05-16 10:10:46 +0200

    Add LD Card prefix to alert messages

--- Forced LD Card/Client_SaveConfigureUI.lua
+++ Forced LD Card/Client_SaveConfigureUI.lua
@@ -5,15 +5,15 @@ require("VersionNumber");
 ---@param addCard fun(name: string, desc: string, filename: string, numPieces: integer, minNumPieces: integer, initPieces: integer, weight: number, duration: integer | nil, expireBehaviour: ActiveCardExpireBehaviorOptions): CardID # Creates a custom card and returns the ID of that card
 function Client_SaveConfigureUI(alert, addCard)
 	Mod.Settings.NumPieces = numPieces.GetValue();
-    if Mod.Settings.NumPieces < 1 then alert("The number of pieces the card is divided into must be bigger or equal to 1"); end
+    if Mod.Settings.NumPieces < 1 then alert("LD Card: The number of pieces the card is divided into must be bigger or equal to 1"); end
     Mod.Settings.PiecesPerTurn = piecesPerTurn.GetValue();
-    if Mod.Settings.PiecesPerTurn < 1 then alert("The number of pieces awarded per turn must be bigger or equal to 1"); end
+    if Mod.Settings.PiecesPerTurn < 0 then alert("LD Card: The number of pieces awarded per turn must be bigger or equal to 1"); end
     Mod.Settings.StartingPieces = startingPieces.GetValue();
-    if Mod.Settings.StartingPieces < 0 then alert("The number of pieces given to each player at the start of the game must be a positive number"); end
+    if Mod.Settings.StartingPieces < 0 then alert("LD Card: The number of pieces given to each player at the start of the game must be a positive number"); end
     Mod.Settings.Weight = weight.GetValue();
-    if Mod.Settings.Weight < 0 then alert("The weight of the card must be a positive number"); end
+    if Mod.Settings.Weight < 0 then alert("LD Card: The weight of the card must be a positive number"); end
     Mod.Settings.Duration = duration.GetValue();
-    if Mod.Settings.Duration < 1 then alert("The card duration must be bigger or equal to 1"); end
+    if Mod.Settings.Duration < 1 then alert("LD Card: The card duration must be bigger or equal to 1"); end
     -- Mod.Settings.AIAutoplayCards = AIAutoplayCards.GetIsChecked();
     Mod.Settings.CanPlayOnTeammates = canPlayOnTeammates.GetIsChecked();
 
