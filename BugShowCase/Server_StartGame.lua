761a83ec8641f79a3f4243e9cb6487f6f95d61dd
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2026-06-12 01:51:37 +0200

    Update Server_StartGame.lua

--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -7,22 +7,21 @@ function Server_StartGame(game, standing)
     print(tostring(cards), tostring(standing.Cards));
     for pID, playerCards in pairs(cards) do
         local whole = playerCards.WholeCards;
-        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(whole.WholeCards)));
+        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
         
         -- Removing a card
-        local cardID = getFirstCard(whole.WholeCards);
-        print(string.format("Removing card with ID %s, indexing gives %s", cardID, tostring(whole.WholeCards[cardID])));
-        whole.WholeCards[cardID] = nil;
-        cards[pID] = whole;
-        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(whole.WholeCards)));
+        local cardID = getFirstCard(playerCards.WholeCards);
+        print(string.format("Removing card with ID %s, indexing gives %s", cardID, tostring(playerCards.WholeCards[cardID])));
+        playerCards.WholeCards[cardID] = nil;
+        cards[pID] = playerCards;
+        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
         
         -- Adding a card
         local new = WL.ReinforcementCardInstance.Create(1);
-        print(string.format("Adding card with ID %s, indexing gives %s", new.ID, tostring(whole.WholeCards[new.ID])));
-        whole.WholeCards[new.ID] = new;
-        cards[pID] = whole;
-        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(whole.WholeCards)));
-        playerCards.WholeCards = whole;
+        print(string.format("Adding card with ID %s, indexing gives %s", new.ID, tostring(playerCards.WholeCards[new.ID])));
+        playerCards.WholeCards[new.ID] = new;
+        cards[pID] = playerCards;
+        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
     end
 
     standing.Cards = cards;
