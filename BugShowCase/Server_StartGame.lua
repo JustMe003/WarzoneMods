de7badb85bdb91e5ebd071faa940ebdf89cf84a2
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2026-06-12 01:44:05 +0200

    Update Server_StartGame.lua

--- BugShowCase/Server_StartGame.lua
+++ BugShowCase/Server_StartGame.lua
@@ -7,9 +7,18 @@ function Server_StartGame(game, standing)
     print(tostring(cards), tostring(standing.Cards));
     for pID, playerCards in pairs(cards) do
         print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
+        
+        -- Removing a card
         local cardID = getFirstCard(playerCards.WholeCards);
         print(string.format("Removing card with ID %s, indexing gives %s", cardID, tostring(playerCards.WholeCards[cardID])));
         playerCards.WholeCards[cardID] = nil;
+        cards[pID] = playerCards;
+        print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
+        
+        -- Adding a card
+        local new = WL.ReinforcementCardInstance.Create(1);
+        playerCards.WholeCards[new.ID] = new;
+        print(string.format("Adding card with ID %s, indexing gives %s", cardID, tostring(playerCards.WholeCards[cardID])));
         cards[pID] = playerCards;
         print(string.format("Player: %d\tNum cards: %d", pID, tableSize(playerCards.WholeCards)));
     end
