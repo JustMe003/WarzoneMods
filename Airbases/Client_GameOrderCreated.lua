947c408c5369e4a62fa94f383a4784d6a2005b1f
Just_A_Dutchman_ <kingoflol20033@gmail.com>
2026-06-22 00:08:23 +0200

    Update Client_GameOrderCreated.lua

--- Airbases/Client_GameOrderCreated.lua
+++ Airbases/Client_GameOrderCreated.lua
@@ -8,7 +8,13 @@ end
 
 function testForNumberOfCities(game, order, terrID, skipOrder)
     local structures = game.LatestStanding.Territories[terrID].Structures;
-    if not (structures ~= nil and structures[WL.StructureType.City] ~= nil and structures[WL.StructureType.City] + getPurchasedCities(game.Orders, terrID) >= Mod.Settings.RequiredCities) then
+    local cities = 0;
+    if structures ~= nil and structures[WL.StructureType.City] ~= nil then
+        cities = structures[WL.StructureType.City];
+    end
+    cities = cities + getPurchasedCities(game.Orders, terrID);
+    print(cities);
+    if cities >= Mod.Settings.RequiredCities then
         if UI ~= nil and UI.Alert ~= nil then
             UI.Alert("You cannot play this airlift because '" .. game.Map.Territories[order.FromTerritoryID].Name .. "' and/or '" .. game.Map.Territories[order.ToTerritoryID].Name .. "' don't have the number of required cities. When playing an airlift card, both territories need to have at least " .. Mod.Settings.RequiredCities .. " cities");
         end
@@ -22,7 +28,7 @@ function getPurchasedCities(orders, terrID)
     for _, order in pairs(orders) do
         if order.proxyType == "GameOrderPurchase" then
             return order.BuildCities[terrID] or 0;
-        elseif order.OccursInPhase > WL.TurnPhase.Purchase then
+        elseif order.OccursInPhase ~= nil and order.OccursInPhase > WL.TurnPhase.Purchase then
             return 0;
         end
     end
